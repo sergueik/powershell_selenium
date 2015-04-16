ede3b3e644b6d1abc9e8597cf5fb178933b243ac
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-04-16 11:37:30 -0400

    initial revision

---
+++ powershell/carnival_octopus_logon.ps1
@@ -0,0 +1,237 @@
+# http://www.codeproject.com/Articles/856325/Mouse-Hover-Action-using-selenium-WebDriver
+
+#Copyright (c) 2014 Serguei Kouzmine
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
+param(
+  [string]$hub_host = '127.0.0.1',
+  [string]$browser,
+  [switch]$grid,
+  [switch]$pause,
+  [string]$username_text = '',
+  [string]$url = 'https://haldev.service-now.com/api/now/table/change_request',
+  [switch]$use_proxy,
+  [string]$password_text = ''
+
+)
+
+function set_timeouts {
+  param(
+    [System.Management.Automation.PSReference]$selenium_ref,
+    [int]$explicit = 120,
+    [int]$page_load = 600,
+    [int]$script = 30000
+    #    [int]$script = 3000
+  )
+
+  [void]($selenium_ref.Value.Manage().Timeouts().ImplicitlyWait([System.TimeSpan]::FromSeconds($explicit)))
+  [void]($selenium_ref.Value.Manage().Timeouts().SetPageLoadTimeout([System.TimeSpan]::FromSeconds($pageload)))
+  [void]($selenium_ref.Value.Manage().Timeouts().SetScriptTimeout([System.TimeSpan]::FromSeconds($script)))
+
+}
+
+
+# http://stackoverflow.com/questions/8343767/how-to-get-the-current-directory-of-the-cmdlet-being-executed
+function Get-ScriptDirectory
+{
+  $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+  if ($Invocation.PSScriptRoot) {
+    $Invocation.PSScriptRoot
+  }
+  elseif ($Invocation.MyCommand.Path) {
+    Split-Path $Invocation.MyCommand.Path
+  } else {
+    $Invocation.InvocationName.Substring(0,$Invocation.InvocationName.LastIndexOf(""))
+  }
+}
+
+function cleanup
+{
+  param(
+    [System.Management.Automation.PSReference]$selenium_ref
+  )
+  try {
+    $selenium_ref.Value.Quit()
+  } catch [exception]{
+    # Ignore errors if unable to close the browser
+    Write-Output (($_.Exception.Message) -split "`n")[0]
+
+  }
+}
+
+
+$shared_assemblies = @{
+  'WebDriver.dll' = 2.44;
+  'WebDriver.Support.dll' = '2.44';
+  'nunit.core.dll' = $null;
+  'nunit.framework.dll' = $null;
+}
+
+
+$shared_assemblies_path = 'c:\developer\sergueik\csharp\SharedAssemblies'
+
+if (($env:SHARED_ASSEMBLIES_PATH -ne $null) -and ($env:SHARED_ASSEMBLIES_PATH -ne '')) {
+  $shared_assemblies_path = $env:SHARED_ASSEMBLIES_PATH
+}
+
+pushd $shared_assemblies_path
+$shared_assemblies.Keys | ForEach-Object {
+  # http://all-things-pure.blogspot.com/2009/09/assembly-version-file-version-product.html
+  $assembly = $_
+  $assembly_path = [System.IO.Path]::Combine($shared_assemblies_path,$assembly)
+  $assembly_version = [Reflection.AssemblyName]::GetAssemblyName($assembly_path).Version
+  $assembly_version_string = ('{0}.{1}' -f $assembly_version.Major,$assembly_version.Minor)
+  if ($shared_assemblies[$assembly] -ne $null) {
+    # http://stackoverflow.com/questions/26999510/selenium-webdriver-2-44-firefox-33
+    if (-not ($shared_assemblies[$assembly] -match $assembly_version_string)) {
+      Write-Output ('Need {0} {1}, got {2}' -f $assembly,$shared_assemblies[$assembly],$assembly_path)
+      Write-Output $assembly_version
+      throw ('invalid version :{0}' -f $assembly)
+    }
+  }
+
+  if ($host.Version.Major -gt 2) {
+    Unblock-File -Path $_;
+  }
+  Write-Debug $_
+  Add-Type -Path $_
+}
+popd
+
+
+$verificationErrors = New-Object System.Text.StringBuilder
+
+# use Default Web Site to host the page. Enable Directory Browsing.
+
+$hub_port = '4444'
+$uri = [System.Uri](('http://{0}:{1}/wd/hub' -f $hub_host,$hub_port))
+
+
+try {
+  $connection = (New-Object Net.Sockets.TcpClient)
+  $connection.Connect($hub_host,[int]$hub_port)
+  $connection.Close()
+} catch {
+  if ($PSBoundParameters['grid']) {
+
+    Start-Process -FilePath "C:\Windows\System32\cmd.exe" -ArgumentList "start cmd.exe /c c:\java\selenium\hub.cmd"
+    Start-Process -FilePath "C:\Windows\System32\cmd.exe" -ArgumentList "start cmd.exe /c c:\java\selenium\node.cmd"
+
+  } else {
+    Start-Process -FilePath "C:\Windows\System32\cmd.exe" -ArgumentList "start cmd.exe /c c:\java\selenium\selenium.cmd"
+  }
+  Start-Sleep -Millisecond 5000
+}
+
+
+
+Write-Debug "Running on ${browser}"
+if ($browser -match 'firefox') {
+  $capability = [OpenQA.Selenium.Remote.DesiredCapabilities]::Firefox()
+
+}
+elseif ($browser -match 'chrome') {
+  $capability = [OpenQA.Selenium.Remote.DesiredCapabilities]::Chrome()
+}
+elseif ($browser -match 'ie') {
+  $capability = [OpenQA.Selenium.Remote.DesiredCapabilities]::InternetExplorer()
+  if ($version -ne $null -and $version -ne 0) {
+    $capability.SetCapability("version",$version.ToString());
+  }
+
+}
+elseif ($browser -match 'safari') {
+  $capability = [OpenQA.Selenium.Remote.DesiredCapabilities]::Safari()
+}
+else {
+  throw "unknown browser choice:${browser}"
+}
+$selenium = New-Object OpenQA.Selenium.Remote.RemoteWebDriver ($uri,$capability)
+
+
+$DebugPreference = 'Continue'
+$base_url = 'http://octopus.carnival.com:81/app#/'
+
+$selenium.Navigate().GoToUrl($base_url)
+$selenium.Manage().Window.Maximize()
+set_timeouts ([ref]$selenium)
+if ($PSBoundParameters['pause']) {
+  Write-Output 'pause'
+  try {
+    [void]$host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+  } catch [exception]{}
+} else {
+  Start-Sleep -Millisecond 1000
+}
+
+function find_page_element_by_css_selector {
+
+  param(
+    [System.Management.Automation.PSReference]$selenium_driver_ref,
+    [System.Management.Automation.PSReference]$element_ref,
+    [string]$css_selector,
+    [int]$wait_seconds = 10
+
+  )
+
+  if ($css_selector -eq '' -or $css_selector -eq $null) {
+    return
+  }
+  $local:element = $null
+  [OpenQA.Selenium.Remote.RemoteWebDriver]$local:selenum_driver = $selenium_driver_ref.Value
+  [OpenQA.Selenium.Support.UI.WebDriverWait]$wait = New-Object OpenQA.Selenium.Support.UI.WebDriverWait ($local:selenum_driver,[System.TimeSpan]::FromSeconds($wait_seconds))
+  $wait.PollingInterval = 50
+
+  try {
+    [void]$wait.Until([OpenQA.Selenium.Support.UI.ExpectedConditions]::ElementExists([OpenQA.Selenium.By]::CssSelector($css_selector)))
+  } catch [exception]{
+    Write-Debug ("Exception : {0} ...`ncss_selector={1}" -f (($_.Exception.Message) -split "`n")[0],$css_selector)
+  }
+
+  $local:element = $local:selenum_driver.FindElement([OpenQA.Selenium.By]::CssSelector($css_selector))
+  $element_ref.Value = $local:element
+
+}
+
+$username = $null
+find_page_element_by_css_selector ([ref]$selenium) ([ref]$username) 'input#inputUsername'
+# $username_text = ''
+[void]$username.SendKeys($username_text)
+$password = $null
+find_page_element_by_css_selector ([ref]$selenium) ([ref]$password) 'input#inputPassword'
+# $password_text = ''
+[void]$password.SendKeys($password_text)
+
+$button = $null
+find_page_element_by_css_selector ([ref]$selenium) ([ref]$button) 'button[type=submit]'
+
+[OpenQA.Selenium.Interactions.Actions]$actions = New-Object OpenQA.Selenium.Interactions.Actions ($selenium)
+[void]$actions.MoveToElement([OpenQA.Selenium.IWebElement]$button).Click().Build().Perform()
+
+# assert
+Start-Sleep 10
+# Cleanup
+cleanup ([ref]$selenium)
+
+
+<#
+Exception calling "FindElement" with "1" argument(s): "Session
+[08a0a650-d83a-4eef-ad83-90551fbb6148] was terminated due to TIMEOUT"
+#>
