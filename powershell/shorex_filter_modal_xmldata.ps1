de8b79c97db1616a55db222196d314ec0648407b
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-05-06 12:37:46 -0400

    initial revision

---
+++ powershell/shorex_filter_modal_xmldata.ps1
@@ -0,0 +1,304 @@
+
+$shoreex_destinations_modal_raw_xmldata = @'
+<?xml version="1.0"?>
+<dummy>
+  <!-- # Load the destinations modal contents and grab all of it, one entry at a time .  Visualize the process -->
+  <!-- Modal Dialog : Destination -->
+  <div class="modal fade" id="destinationModal">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-body">
+          <div id="destinations" class="row dest">
+            <div class="ca-home-modal-list-container ca-home-modal-list-destination col-sm-3">
+              <ul>
+                <li class="ca-home-destination-title">
+                  <span>Departure Ports</span>
+                </li>
+                <li>
+                  <a href="/shore-excursions/athens-greece" data-val="ATH">Athens, Greece</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/baltimore-md" data-val="BWI">Baltimore, MD</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/barbados" data-val="BDS">Barbados</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/barcelona-spain" data-val="BCN">Barcelona, Spain</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/charleston-sc" data-val="CHS">Charleston, SC</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/galveston-tx" data-val="GAL">Galveston, TX</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/honolulu-hi" data-val="HNL">Honolulu, HI</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/jacksonville-fl" data-val="JAX">Jacksonville, FL</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/miami-fl" data-val="MIA">Miami, FL</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/new-orleans-la" data-val="MSY">New Orleans, LA</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/new-york-ny" data-val="NYC">New York, NY</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/port-canaveral-orlando-fl" data-val="PCV">Port Canaveral (Orlando), FL</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/san-juan-puerto-rico" data-val="SJU">San Juan, Puerto Rico</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/seattle-wa" data-val="SEA">Seattle, WA</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/tampa-fl" data-val="TPA">Tampa, FL</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/vancouver-bc-canada" data-val="YVR">Vancouver, Bc, Canada</a>
+                </li>
+                <li class="ca-home-destination-title ca-home-destination-list-row">
+                  <a href="/shore-excursions/alaska" data-val="A">Alaska</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/juneau-ak" data-val="JNU">Juneau</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/ketchikan-ak" data-val="KTN">Ketchikan</a>
+                </li>
+              </ul>
+            </div>
+            <div class="ca-home-modal-list-container ca-home-modal-list-destination col-sm-3">
+              <ul>
+                <li>
+                  <a href="/shore-excursions/skagway-ak" data-val="SKY">Skagway</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/victoria-bc-canada" data-val="YYJ">Victoria</a>
+                </li>
+                <li class="ca-home-destination-title ca-home-destination-list-row">
+                  <a href="/shore-excursions/bahamas" data-val="BH">Bahamas</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/freeport-the-bahamas" data-val="FPO">Freeport</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/grand-turk" data-val="GDT">Grand Turk</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/half-moon-cay-the-bahamas" data-val="HMC">Half Moon Cay</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/nassau-the-bahamas" data-val="NAS">Nassau</a>
+                </li>
+                <li class="ca-home-destination-title ca-home-destination-list-row">
+                  <a href="/shore-excursions/bermuda" data-val="BM">Bermuda</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/bermuda" data-val="WRF">Bermuda</a>
+                </li>
+                <li class="ca-home-destination-title ca-home-destination-list-row">
+                  <a href="/shore-excursions/caribbean" data-val="C">Caribbean</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/amber-cove-dominican-republic" data-val="DOP">Amber Cove</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/antigua" data-val="ATG">Antigua</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/aruba" data-val="ARB">Aruba</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/belize" data-val="BZE">Belize</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/bonaire" data-val="BON">Bonaire                       </a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/colon-panama" data-val="CLN">Colon</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/curacao" data-val="CUR">Curacao</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/dominica" data-val="DOM">Dominica</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/falmouth-jamaica" data-val="FJM">Falmouth</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/grand-cayman-cayman-islands" data-val="CAY">Grand Cayman</a>
+                </li>
+              </ul>
+            </div>
+            <div class="ca-home-modal-list-container ca-home-modal-list-destination col-sm-3">
+              <ul>
+                <li>
+                  <a href="/shore-excursions/grenada" data-val="JGU">Grenada</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/la-romana-dominican-republic" data-val="LRM">La Romana</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/limon-costa-rica" data-val="LMO">Limon</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/mahogany-bay-isla-roatan" data-val="RTN">Mahogany Bay</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/martinique-fwi" data-val="MTK">Martinique</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/montego-bay-jamaica" data-val="MTB">Montego Bay</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/ocho-rios-jamaica" data-val="OCJ">Ocho Rios</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/key-west-fl" data-val="KEY">Key West</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/st-croix-usvi" data-val="STX">St. Croix</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/st-thomas-usvi" data-val="STT">St. Thomas</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/st-kitts-wi" data-val="STK">St Kitts</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/st-maarten-na" data-val="SXM">St. Maarten</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/tortola-british-virgin-islands" data-val="TOR">Tortola</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/ft-lauderdale-pt-evrglds-fl" data-val="EGL">Ft Lauderdale (Pt Evrglds)</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/st-lucia" data-val="SLC">St. Lucia</a>
+                </li>
+                <li class="ca-home-destination-title ca-home-destination-list-row">
+                  <a href="/shore-excursions/europe" data-val="E">Europe</a>
+                </li>
+                <li class="ca-home-destination-title ca-home-destination-list-row">
+                  <a href="/shore-excursions/hawaii" data-val="H">Hawaii</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/hilo-hi" data-val="ITO">Hilo</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/kona-hi" data-val="KOA">Kona</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/kauai-nawiliwili-hi" data-val="LIH">Kauai (Nawiliwili)</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/maui-kahului-hi" data-val="OGG">Maui (Kahului)</a>
+                </li>
+              </ul>
+            </div>
+            <div class="ca-home-modal-list-container ca-home-modal-list-destination col-sm-3">
+              <ul>
+                <li class="ca-home-destination-title">
+                  <a href="/shore-excursions/mexico" data-val="M">Mexico</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/cabo-san-lucas-mexico" data-val="CSL">Cabo San Lucas</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/puerto-vallarta-mexico" data-val="PVR">Puerto Vallarta</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/catalina-island-ca" data-val="CAT">Catalina Island</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/mazatlan-mexico" data-val="MZT">Mazatlan</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/los-angeles-long-beach-ca" data-val="LGB">Los Angeles (Long Beach)</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/manzanillo-mexico" data-val="MAN">Manzanillo</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/ensenada-mexico" data-val="ENS">Ensenada</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/cozumel-mexico" data-val="CZM">Cozumel</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/costa-maya-mexico" data-val="CMZ">Costa Maya</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/yucatan-progreso-mexico" data-val="PGR">Yucatan (Progreso)</a>
+                </li>
+                <li class="ca-home-destination-title ca-home-destination-list-row">
+                  <a href="/shore-excursions/canada-new-england" data-val="NN">Canada / New England</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/boston-ma" data-val="BOS">Boston</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/portland-me" data-val="PWM">Portland</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/halifax-ns-canada" data-val="YHZ">Halifax</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/saint-john-nb-canada" data-val="YSJ">Saint John</a>
+                </li>
+                <li class="ca-home-destination-title ca-home-destination-list-row">
+                  <a href="/shore-excursions/south-america" data-val="S">South America</a>
+                </li>
+                <li>
+                  <a href="/shore-excursions/santa-marta-colombia" data-val="SRT">Santa Marta</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="ca-top-icon-button close pull-left" data-dismiss="modal" aria-hidden="true">&#xF057;</a>
+    </div>
+  </div>
+</dummy>
+'@
+
+#  http://www.powershellmagazine.com/2014/06/30/pstip-using-xpath-in-powershell-part-4/
+$s1 = [xml]$shoreex_destinations_modal_raw_xmldata
+$cnt = 0
+
+$data = @( @{
+    'code' = $null;
+    'url' = $null;
+    'description' = $null;
+
+  })
+Select-Xml -Xml $s1 -XPath '//div[@id="destinationModal"]//ul//li//a' | ForEach-Object {
+  $node = $_;
+  <#
+  if ($cnt -eq 0) {
+    $cnt = 1
+    $o = $node.Node;
+    Write-Output ($o | Get-Member | Format-List)
+
+  }
+  #>
+  $data += @{
+    'code' = $o.'data-val';
+    'url' = $o.href;
+    'description' = $o.'#text';
+  }
+  <#
+write-output $o.'data-val'
+write-output $o.href
+write-output $o.'#text'
+#>
+}
