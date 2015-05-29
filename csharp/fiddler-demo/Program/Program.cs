ef963fb122a9fe401c065051a5583ebd2daa4eec
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-05-28 21:05:17 -0400

    intermediate revision

--- csharp/fiddler-demo/Program/Program.cs
+++ csharp/fiddler-demo/Program/Program.cs
@@ -11,6 +11,7 @@ namespace WebTester
 {
     public class Monitor
     {
+        bool IgnoreResources;
         public Monitor()
         {
             #region AttachEventListeners
@@ -25,32 +26,87 @@ namespace WebTester
             // by default, we must handle notifying the user ourselves.
             FiddlerApplication.OnNotification += delegate(object sender, NotificationEventArgs oNEA) { Console.WriteLine("** NotifyUser: " + oNEA.NotifyString); };
             FiddlerApplication.Log.OnLogString += delegate(object sender, LogEventArgs oLEA) { Console.WriteLine("** LogString: " + oLEA.LogString); };
-
-            FiddlerApplication.BeforeRequest += delegate(Session oS)
+            IgnoreResources = false;
+            FiddlerApplication.BeforeRequest += (s) =>
             {
-                Console.WriteLine("Before request for:\t" + oS.fullUrl);
+                Console.WriteLine("Before request for:\t" + s.fullUrl);
                 // In order to enable response tampering, buffering mode must
                 // be enabled; this allows FiddlerCore to permit modification of
                 // the response in the BeforeResponse handler rather than streaming
                 // the response to the client as the response comes in.
-                oS.bBufferResponse = true;
+                s.bBufferResponse = true;
             };
 
-            FiddlerApplication.BeforeResponse += delegate(Session oS)
+            FiddlerApplication.BeforeResponse += (s) =>
             {
-                Console.WriteLine("{0}:HTTP {1} for {2}", oS.id, oS.responseCode, oS.fullUrl);
+                Console.WriteLine("{0}:HTTP {1} for {2}", s.id, s.responseCode, s.fullUrl);
 
-                // Uncomment the following two statements to decompress/unchunk the
-                // HTTP response and subsequently modify any HTTP responses to replace 
-                // instances of the word "Microsoft" with "Bayden"
-                //oS.utilDecodeResponse(); oS.utilReplaceInResponse("Microsoft", "Bayden");
+                // Uncomment the following to decompress/unchunk the HTTP response 
+                // s.utilDecodeResponse(); 
             };
 
-            FiddlerApplication.AfterSessionComplete += delegate(Session oS) { Console.WriteLine("Finished session:\t" + oS.fullUrl); };
 
+            FiddlerApplication.AfterSessionComplete += (s) => Console.WriteLine("Finished session:\t" + s.fullUrl);
+            FiddlerApplication.AfterSessionComplete += FiddlerApplication_AfterSessionComplete;
             #endregion AttachEventListeners
         }
 
+
+        private void FiddlerApplication_AfterSessionComplete(Session sess)
+        {
+            // Ignore HTTPS connect requests
+            if (sess.RequestMethod == "CONNECT")
+                return;
+            /*
+                if (!string.IsNullOrEmpty(CaptureConfiguration.CaptureDomain))
+                {
+                    if (sess.hostname.ToLower() != CaptureConfiguration.CaptureDomain.Trim().ToLower())
+                        return;
+                }
+            */
+
+            if (IgnoreResources)
+            {
+                string url = sess.fullUrl.ToLower();
+                /*
+                        var extensions = CaptureConfiguration.ExtensionFilterExclusions;
+                        foreach (var ext in extensions)
+                        {
+                            if (url.Contains(ext))
+                                return;
+                        }
+
+                        foreach (var urlFilter in filters)
+                        {
+                            if (url.Contains(urlFilter))
+                                return;
+                        }
+                */
+            }
+
+            if (sess == null || sess.oRequest == null || sess.oRequest.headers == null)
+                return;
+
+            string headers = sess.oRequest.headers.ToString();
+            var reqBody = sess.GetRequestBodyAsString();
+
+            // to capture the response
+            // string respHeaders = session.oResponse.headers.ToString();
+            // var respBody = session.GetResponseBodyAsString();
+
+            // replace the HTTP line to inject full URL
+            string firstLine = sess.RequestMethod + " " + sess.fullUrl + " " + sess.oRequest.headers.HTTPVersion;
+            int at = headers.IndexOf("\r\n");
+            if (at < 0)
+                return;
+            headers = firstLine + "\r\n" + headers.Substring(at + 1);
+
+            string output = headers + "\r\n" +
+                            (!string.IsNullOrEmpty(reqBody) ? reqBody + "\r\n" : string.Empty) +
+                             "\r\n\r\n";
+            Console.WriteLine(output);
+
+        }
         public void Start()
         {
             Console.WriteLine("Starting FiddlerCore...");
@@ -68,14 +124,15 @@ namespace WebTester
         public void Stop()
         {
             // TODO: raise event
-            Console.WriteLine("Shutting down...");
+            Console.WriteLine("Shutdown.");
             FiddlerApplication.Shutdown();
             System.Threading.Thread.Sleep(1);
         }
-
+        public static Monitor m;
+        // No longer necessary 
         public static void Main(string[] args)
         {
-            Monitor m = new Monitor();
+            m = new Monitor();
             #region AttachEventListeners
             // Tell the system console to handle CTRL+C by calling our method that
             // gracefully shuts down the FiddlerCore.
@@ -87,12 +144,13 @@ namespace WebTester
             {
                 System.Threading.Monitor.Wait(forever);
             }
-            m.Stop();
+
         }
 
         static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
         {
-            Console.WriteLine("Stop...");
+            Console.WriteLine("Stop.");
+            m.Stop();
             System.Threading.Thread.Sleep(1);
         }
     }
