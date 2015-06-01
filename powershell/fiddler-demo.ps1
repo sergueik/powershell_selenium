776a5b90f451b6e585e58cce328667613a3c1723
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-06-01 17:45:07 -0400

    intermediate revision

--- powershell/fiddler-demo.ps1
+++ powershell/fiddler-demo.ps1
@@ -22,7 +22,7 @@
 param(
   [string]$browser = '',
   [int]$version,
-  [string]$base_url = 'http://www.google.com/',
+  [string]$base_url = 'http://www.travelzoo.com/',
   [switch]$debug,
   [switch]$pause
 )
@@ -75,6 +75,8 @@ $shared_assemblies = @(
   'nunit.framework.dll'
 )
 
+# version: 
+
 $shared_assemblies_path = 'c:\developer\sergueik\csharp\SharedAssemblies'
 
 if (($env:SHARED_ASSEMBLIES_PATH -ne $null) -and ($env:SHARED_ASSEMBLIES_PATH -ne '')) {
@@ -86,8 +88,9 @@ pushd $shared_assemblies_path
 $shared_assemblies | ForEach-Object { Unblock-File -Path $_; Add-Type -Path $_ }
 popd
 
-# refactored http://fiddler.wikidot.com/fiddlercore-demo
-# copied  from  http://weblog.west-wind.com/posts/2014/Jul/29/Using-FiddlerCore-to-capture-HTTP-Requests-with-NET
+# http://fiddler.wikidot.com/fiddlercore-demo
+# http://fiddler.wikidot.com/fiddlercore-api
+
 Add-Type @"
 
 using System;
@@ -97,22 +100,15 @@ namespace WebTester
 {
     public class Monitor
     {
-        bool IgnoreResources;
         public Monitor()
         {
             #region AttachEventListeners
-            //
-            // It is important to understand that FiddlerCore calls event handlers on the
-            // session-handling thread.  If you need to properly synchronize to the UI-thread
-            // (say, because you're adding the sessions to a list view) you must call .Invoke
-            // on a delegate on the window handle.
-            //
 
             // Simply echo notifications to the console.  Because CONFIG.QuietMode=true 
             // by default, we must handle notifying the user ourselves.
             FiddlerApplication.OnNotification += delegate(object sender, NotificationEventArgs oNEA) { Console.WriteLine("** NotifyUser: " + oNEA.NotifyString); };
             FiddlerApplication.Log.OnLogString += delegate(object sender, LogEventArgs oLEA) { Console.WriteLine("** LogString: " + oLEA.LogString); };
-            IgnoreResources = false;
+            // IgnoreResources = false;
             FiddlerApplication.BeforeRequest += (s) =>
             {
                 Console.WriteLine("Before request for:\t" + s.fullUrl);
@@ -138,61 +134,32 @@ namespace WebTester
         }
 
 
-        private void FiddlerApplication_AfterSessionComplete(Session sess)
+        private void FiddlerApplication_AfterSessionComplete(Session fiddler_session)
         {
+
             // Ignore HTTPS connect requests
-            if (sess.RequestMethod == "CONNECT")
-                return;
-            /*
-                if (!string.IsNullOrEmpty(CaptureConfiguration.CaptureDomain))
-                {
-                    if (sess.hostname.ToLower() != CaptureConfiguration.CaptureDomain.Trim().ToLower())
-                        return;
-                }
-            */
-
-            if (IgnoreResources)
-            {
-                string url = sess.fullUrl.ToLower();
-                /*
-                        var extensions = CaptureConfiguration.ExtensionFilterExclusions;
-                        foreach (var ext in extensions)
-                        {
-                            if (url.Contains(ext))
-                                return;
-                        }
-
-                        foreach (var urlFilter in filters)
-                        {
-                            if (url.Contains(urlFilter))
-                                return;
-                        }
-                */
-            }
-
-            if (sess == null || sess.oRequest == null || sess.oRequest.headers == null)
+            if (fiddler_session.RequestMethod == "CONNECT")
                 return;
 
-            string headers = sess.oRequest.headers.ToString();
-            var reqBody = sess.GetRequestBodyAsString();
-
-            // to capture the response
-            // string respHeaders = session.oResponse.headers.ToString();
-            // var respBody = session.GetResponseBodyAsString();
-
-            // replace the HTTP line to inject full URL
-            string firstLine = sess.RequestMethod + " " + sess.fullUrl + " " + sess.oRequest.headers.HTTPVersion;
-            int at = headers.IndexOf("\r\n");
-            if (at < 0)
+            if (fiddler_session == null || fiddler_session.oRequest == null || fiddler_session.oRequest.headers == null)
                 return;
-            headers = firstLine + "\r\n" + headers.Substring(at + 1);
 
-            string output = headers + "\r\n" +
-                            (!string.IsNullOrEmpty(reqBody) ? reqBody + "\r\n" : string.Empty) +
-                             "\r\n\r\n";
-            Console.WriteLine(output);
-
+        	 var full_url = fiddler_session.fullUrl;
+        	 Console.WriteLine("URL: " + full_url);
+        	         	 
+        	 HTTPResponseHeaders response_headers = fiddler_session.ResponseHeaders;
+        	 Console.WriteLine("HTTP Response: " + response_headers.HTTPResponseCode.ToString());
+        	 
+        	 foreach (HTTPHeaderItem header_item in response_headers){
+        	 	Console.WriteLine(header_item.Name + " " + header_item.Value);
+        	 }
+        
+        	 // http://fiddler.wikidot.com/timers
+			 var timers  =  fiddler_session.Timers;
+			 var duration = timers.ClientDoneResponse - timers.ClientBeginRequest;
+			 Console.WriteLine(String.Format("Duration: {0:F10}", duration.Milliseconds));
         }
+
         public void Start()
         {
             Console.WriteLine("Starting FiddlerCore...");
@@ -215,23 +182,6 @@ namespace WebTester
             System.Threading.Thread.Sleep(1);
         }
         public static Monitor m;
-        // No longer necessary 
-        public static void Main(string[] args)
-        {
-            m = new Monitor();
-            #region AttachEventListeners
-            // Tell the system console to handle CTRL+C by calling our method that
-            // gracefully shuts down the FiddlerCore.
-            Console.CancelKeyPress += new ConsoleCancelEventHandler(Console_CancelKeyPress);
-            #endregion AttachEventListeners
-            m.Start();
-            Object forever = new Object();
-            lock (forever)
-            {
-                System.Threading.Monitor.Wait(forever);
-            }
-
-        }
 
         static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
         {
@@ -244,53 +194,8 @@ namespace WebTester
 "@ -ReferencedAssemblies 'System.dll','System.Data.dll',"${shared_assemblies_path}\FiddlerCore4.dll"
 # TODO - randomise namespace.
 $o = New-Object -TypeName 'WebTester.Monitor'
-# http://fiddler.wikidot.com/fiddlercore-api
 $o.Start()
 
-<#
-TODO: extract $req, $res.
-
-----------------------------------------
-POST /CMS/javascript/tealeaf/TealeafTarget.aspx HTTP/1.1
-Accept: */*
-X-TeaLeaf: ClientEvent
-Content-Type: text/xml
-X-TeaLeafType: PERFORMANCE; GUI;
-X-TeaLeafSubType: INIT; BEFOREUNLOAD
-X-TeaLeaf-Page-Url: /bookingengine/cruise-search/caribbean
-X-TeaLeaf-Screen-Res: 2
-X-TeaLeaf-Browser-Res: 2
-X-TeaLeaf-Page-Render: 2370
-X-TeaLeaf-Page-Img-Fail: 2
-X-TeaLeaf-Page-Cui-Events: 3
-X-TeaLeaf-Page-Cui-Bytes: 1241
-X-TeaLeaf-Page-Dwell: 13642
-Referer: ...
-Accept-Language: en-us
-Accept-Encoding: gzip, deflate
-User-Agent: Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; KTXN B579989732A48120T2783790P5)
-Host: www.carnival.com
-Content-Length: 1241
-Connection: Keep-Alive
-Cache-Control: no-cache
-Cookie: ...
-----------------------------------------
-HTTP/1.1 200 OK
-Cache-Control: private
-Content-Type: text/html; charset=utf-8
-Server: Microsoft-IIS/7.0
-X-AspNet-Version: 4.0.30319
-X-Powered-By: ASP.NET
-Content-Length: 238
-Date: Tue, 19 May 2015 20:16:33 GMT
-Connection: keep-alive
-Set-Cookie: ...
-Set-Cookie: ...
-
-#>
-
-
-# TODO : $o.IsStarted()
 
 $headless = $false
 if ($browser -ne $null -and $browser -ne '') {
