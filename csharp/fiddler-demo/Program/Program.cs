5022d12b5c198f191219bb7ff0b998f800fc2e8f
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-06-13 14:52:51 -0400

    intermediate revision

--- csharp/fiddler-demo/Program/Program.cs
+++ csharp/fiddler-demo/Program/Program.cs
@@ -1,20 +1,22 @@
-using System;
-using System.Text.RegularExpressions;
 using Fiddler;
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Data.SQLite;
 using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace WebTester
 {
     public class Monitor
     {
         private string tableName = "";
-        private bool IgnoreResources;
         private string dataFolderPath;
         private string database;
         private string dataSource;
@@ -24,13 +26,10 @@ namespace WebTester
             database = String.Format("{0}\\data.db", dataFolderPath);
             dataSource = "data source=" + database;
             tableName = "product";
-
-            // Simply echo notifications to the console.  Because CONFIG.QuietMode=true 
-            // by default, we must handle notifying the user ourselves.
+            /* handle all events is not really necessary  
             FiddlerApplication.OnNotification += delegate(object sender, NotificationEventArgs e) { Console.WriteLine("** NotifyUser: " + e.NotifyString); };
             FiddlerApplication.Log.OnLogString += delegate(object sender, Fiddler.LogEventArgs e) { Console.WriteLine("** LogString: " + e.LogString); };
 
-            IgnoreResources = false;
             FiddlerApplication.BeforeRequest += (s) =>
             {
                 Console.WriteLine("Before request for:\t" + s.fullUrl);
@@ -48,12 +47,11 @@ namespace WebTester
                 // Uncomment the following to decompress/unchunk the HTTP response 
                 // s.utilDecodeResponse(); 
             };
-
+            */
             FiddlerApplication.AfterSessionComplete += FiddlerApplication_AfterSessionComplete;
-
         }
 
-        private void FiddlerApplication_AfterSessionComplete(Session fiddler_session)
+        private void FiddlerApplication_AfterSessionComplete(Fiddler.Session fiddler_session)
         {
             // Ignore HTTPS connect requests
             if (fiddler_session.RequestMethod == "CONNECT")
@@ -77,12 +75,12 @@ namespace WebTester
             {
                 referer = request_headers_dictionary["Referer"].Value;
             }
-            
+
             //foreach (HTTPHeaderItem header_item in response_headers)
             //{
             //    Console.Error.WriteLine(header_item.Name + " " + header_item.Value);
             //}
-            
+
             //foreach (HTTPHeaderItem header_item in request_headers)
             //{
             //    Console.Error.WriteLine(header_item.Name + " " + header_item.Value);
@@ -92,7 +90,7 @@ namespace WebTester
 
             // http://fiddler.wikidot.com/timers
             var timers = fiddler_session.Timers;
-            TimeSpan duration = timers.ClientDoneResponse - timers.ClientBeginRequest;
+            TimeSpan duration = (TimeSpan)(timers.ClientDoneResponse - timers.ClientBeginRequest);
             Console.Error.WriteLine(String.Format("Duration: {0:F10}", duration.Milliseconds));
             var dic = new Dictionary<string, object>(){
                 	{"url" ,full_url}, {"status", http_response_code},
@@ -100,6 +98,16 @@ namespace WebTester
                 	{"referer", referer }
                 };
             insert(dic);
+
+            // https://groups.google.com/forum/#!msg/httpfiddler/RuFf5VzKCg0/wcgq-WeUnCoJ 
+            //// the following code does not work as intended: request body is always blank
+            //string request_body = fiddler_session.GetRequestBodyAsString();
+
+            //if (!string.IsNullOrEmpty(request_body))
+            //{
+            //    // TODO: UrlDecode 
+            //    Console.Error.WriteLine(string.Join(Environment.NewLine, request_body.Split(new char[] { '&' })));
+            //}
         }
 
         bool TestConnection()
@@ -133,8 +141,6 @@ namespace WebTester
                         cmd.Connection = conn;
                         conn.Open();
                         SQLiteHelper sh = new SQLiteHelper(cmd);
-                        int count = sh.ExecuteScalar<int>(String.Format("select count(*) from {0};", this.tableName)) + 1;
-
                         sh.Insert(tableName, dic);
                         conn.Close();
                         return true;
@@ -178,21 +184,69 @@ namespace WebTester
             database = String.Format("{0}\\fiddler-data.db", dataFolderPath);
             dataSource = "data source=" + database;
             tableName = "product";
+
             // http://stackoverflow.com/questions/24969198/how-do-i-get-fiddlercore-programmatic-certificate-installation-to-stick
             // http://weblog.west-wind.com/posts/2014/Jul/29/Using-FiddlerCore-to-capture-HTTP-Requests-with-NET
             TestConnection();
+            // TODO: 
+            // http://dynamicjson.codeplex.com/
             createTable();
-            // For the purposes of this demo, we'll forbid connections to HTTPS 
-            // sites that use invalid certificates
-            CONFIG.IgnoreServerCertErrors = false;
-            // Because we've chosen to decrypt HTTPS traffic, makecert.exe must
-            // be present in the Application folder.
-            FiddlerApplication.Startup(8877, true, true);
+            int open_port = get_open_port();
+            // allow connections to HTTPS sites w/invalid certificates
+            Fiddler.CONFIG.IgnoreServerCertErrors = true;
+
+            // To decrypt HTTPS traffic, makecert.exe mus be present
+            // TODO: read HKEY_CURRENT_USER\Software\Telerik\FiddlerCoreAPI
+            // or rely on nuget to install
+            // or define the MakeCert.exe path manually.
+            // FiddlerApplication.Prefs.SetStringPref("fiddler.config.path.makecert", @"c:\tools\FiddlerCoreAPI\Makecert.exe");
+            // FiddlerApplication.Prefs.SetBoolPref("fiddler.network.streaming.abortifclientaborts", true);//Abort session when client abort
+
+            // https://github.com/rkprajapat/webtester/blob/master/FiddlerCoreAPI/FiddlerCore.chm
+            // discourages the following            
+            // FiddlerApplication.Startup(open_port, /* Register As System Proxy */ true, /* Decrypt SSL */ true);
+            // in favour of 
+            FiddlerApplication.Startup(open_port,
+                                       FiddlerCoreStartupFlags.CaptureLocalhostTraffic |
+                                       FiddlerCoreStartupFlags.RegisterAsSystemProxy |
+                                       FiddlerCoreStartupFlags.MonitorAllConnections |
+                                       FiddlerCoreStartupFlags.ChainToUpstreamGateway |
+            (FiddlerCoreStartupFlags.Default & ~FiddlerCoreStartupFlags.DecryptSSL));
+
+            int fiddler_listen_port = Fiddler.FiddlerApplication.oProxy.ListenPort;
+
+            Console.WriteLine("Fiddler starting on port " + fiddler_listen_port);
+            // Usage:
+            // var profile = new FirefoxProfile();
+            // profile.SetProxyPreferences(new Proxy { HttpProxy = String.Format("localhost:{0}",fiddler_listen_port), SslProxy = String.Format("localhost:{0}",fiddler_listen_port)});
+            // var selenium = new FirefoxDriver(profile);
+            // .SetProxyInProcess("127.0.0.1:" + port.ToString(), "<-loopback>");
+
+            // Wait  for the user to stop
             Console.WriteLine("Hit CTRL+C to end session.");
-            // Wait  for the user to hit CTRL+C.  
         }
+
+        private int get_open_port()
+        {
+            try
+            {
+                Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                sock.Bind(new IPEndPoint(IPAddress.Any, 0));
+                int port = ((IPEndPoint)sock.LocalEndPoint).Port;
+                sock.Dispose();
+                return port;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+                return 0;
+            }
+        }
+
+
+
         // TODO : extract cookies
-        public void extract_headers(string raw_text)
+        private void extract_headers_basic(string raw_text)
         {
 
             string header_name_regexp = @"(?<header_name>[^ ]+):";
@@ -211,22 +265,22 @@ namespace WebTester
         public void Stop()
         {
             Console.WriteLine("Shutdown.");
-
             FiddlerApplication.AfterSessionComplete -= FiddlerApplication_AfterSessionComplete;
             if (FiddlerApplication.IsStarted())
                 FiddlerApplication.Shutdown();
             System.Threading.Thread.Sleep(1);
         }
 
-        public static Monitor m;
+        public static Monitor proxy;
+
         // Not necessary if embedded in Powershell  
         public static void Main(string[] args)
         {
-            m = new Monitor();
+            proxy = new Monitor();
             #region AttachEventListeners
             Console.CancelKeyPress += new ConsoleCancelEventHandler(Console_CancelKeyPress);
             #endregion AttachEventListeners
-            m.Start();
+            proxy.Start();
             Object forever = new Object();
             lock (forever)
             {
@@ -237,7 +291,7 @@ namespace WebTester
         static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
         {
             Console.WriteLine("Stop.");
-            m.Stop();
+            proxy.Stop();
             System.Threading.Thread.Sleep(1);
         }
     }
