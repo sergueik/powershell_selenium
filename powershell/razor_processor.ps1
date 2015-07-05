7e291dbacb0bf1491c63905c8b34b3f3d0320ea8
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-07-05 15:01:48 -0400

    intermediate revision

--- powershell/razor_processor.ps1
+++ powershell/razor_processor.ps1
@@ -3,7 +3,7 @@
 # This is a vanilla script to bootstrap code requird to invoke RazorEngine.
 # it has some minor modifications compared to
 # https://github.com/knutkj/misc/blob/master/PowerShell/Razor/Render-RazorTemplate.ps1
-# it depends on System.Web.Razor.dll which is already in the GAC, but not loaded by defauls, or can be restored by nuget :
+# it depends on System.Web.Razor.dll which is already in the GAC, but not loaded by defauls, or can be restored by nuget:
 
 
 $assembly_name = 'System.Web.Razor'
@@ -26,31 +26,91 @@ if ($assembly_in_the_GAC -eq $null) {
     $shared_assemblies_path = $env:SHARED_ASSEMBLIES_PATH
   }
 
-
   pushd $shared_assemblies_path
 
+  $shared_assemblies | ForEach-Object {
+    if ($host.Version.Major -gt 2) {
+      Unblock-File -Path $_;
+    }
+    Write-Debug $_
+    Add-Type -Path $_
+  }
 
   popd
 
-  # example of using nuget packages
-  $nuget_base_dir = $PWD
-  $razorSearchPath = Join-Path -Path $nuget_base_dir -ChildPath "packages\AspNetRazor.Core.*\lib\net40\${assembly_name}.dll"
+  # Example of finding assembly downloaded by nuget package restore
 
-  $assembly_nuget_package_path = Get-ChildItem -Path $razorSearchPath | Select-Object -First 1 -ExpandProperty FullName
+  $nuget_packages_base_path = $PWD
+  $assembly_path = Join-Path -Path $nuget_packages_base_path -ChildPath "packages\AspNetRazor.Core.*\lib\net40\${assembly_name}.dll"
+
+  $assembly_nuget_package_path = Get-ChildItem -Path $assembly_path | Select-Object -First 1 -ExpandProperty FullName
 
   if ($assembly_nuget_package_path -ne $null) {
-    Add-Type -Path $razorPath
+    Add-Type -Path $assembly_nuget_package_path
   } else {
-    throw "The System.Web.Razor assembly must be loaded."
+    throw ('The System.Web.Razor assembly cannot be found. Nothing was found in nuget package restore of "{0}"' -f $nuget_packages_base_path  )
   }
 }
 
+$imports = @'
+
+# SCRIPT PARAMETERS.
+param(
+  [string]$browser = '',
+  [switch]$grid,
+  [int]$browser_version,
+  [string]$base_url,
+  [switch]$pause
+)
+# END OF PARAMETERS.
+
+<# 
+OPTIONAL OVERRIDE $shared_assemblies:
+
+$shared_assemblies = @@(
+  'WebDriver.dll',
+  'WebDriver.Support.dll',
+  'System.Data.SQLite.dll',
+  'nunit.framework.dll'
+)
+
+#> 
 
 
-$template1 = "<ul>@foreach(var i in Model){<li>@i</li>}</ul>"
-$template2 = "<ul>@Model.PageObject.Name</ul>"
+$module_name = 'selenium_utils.psd1'
+$module_path = '.'
+  # MODULE_PATH environment overrides parameter, e.g. for Team City
+  if (($env:MODULE_PATH -ne $null) -and ($env:MODULE_PATH -ne '')) {
+    $module_path = $env:MODULE_PATH
+  }
 
-$template3 = @"
+Import-Module -Name ('{0}/{1}' -f $module_path, $module_name )
+if ([bool]$PSBoundParameters['grid'].IsPresent) {
+  $selenium = launch_selenium -browser $browser -grid
+} else {
+  $selenium = launch_selenium -browser $browser
+}
+
+# TEST FIXTURE
+if (-not $base_url) { return }
+$selenium.Navigate().GoToUrl($base_url)
+$selenium.Manage().Window.Maximize()
+
+# $element = find_page_element -xpath $xpath
+# $element = find_page_element -css_selector $css_selector
+# [OpenQA.Selenium.Interactions.Actions]$actions = New-Object OpenQA.Selenium.Interactions.Actions ($selenium)
+# $actions.MoveToElement([OpenQA.Selenium.IWebElement]$element).Click().Build().Perform()
+# $expected_text
+# [NUnit.Framework.Assert]::IsTrue(($element.Text -match $expected_text),('expected: {0} got:{1}' -f $expected_text,$element.Text))
+
+# TEST CLEANUP
+# $actions = $null
+cleanup ([ref]$selenium)
+# END OF THE TEST
+
+'@
+
+$functions = @'
 
 
 @functions{
@@ -152,27 +212,9 @@ $template3 = @"
 }
 
 
+'@
 
-@{
-
-<text>@@Model.PageObject.Name = '@Model.PageObject.Name'</text>
-<text>@@Model.PageObject.HtmlTag='@Model.PageObject.HtmlTag'</text> 
-<text>@@Model.PageObject.Type='@Model.PageObject.Type'</text> 
-<text>@@Model.PageObject.How='@Model.PageObject.How'</text>
-<text>@@Model.PageObject.Locator='@Model.PageObject.Locator'</text>
-<text>(@Model.PageObject.Name, @Model.PageObject.HtmlTag, @Model.PageObject.Type, @Model.How, @Model.PageObject.Locator)</text>
-      
-<text>@RubyWebElement(@Model.PageObject.Name, @Model.PageObject.HtmlTag, @Model.PageObject.Type, @Model.PageObject.How, @Model.PageObject.Locator);</text>
-    
-}
-
-
-end
-"@
-
-[object]$Model1 = (0..3)
-
-$Model2 = New-Object PSObject
+$model2 = New-Object PSObject
 
 $element = New-Object PSObject
 $element | Add-Member Noteproperty 'Name' 'element_name'
@@ -181,14 +223,92 @@ $element | Add-Member Noteproperty 'How' 'CssSelector'
 $element | Add-Member Noteproperty 'HtmlTag' 'element_html_tag'
 $element | Add-Member Noteproperty 'Locator' 'CssSelector'
 
-$Model2 | Add-Member Noteproperty 'PageObject' $element
+$model2 | Add-Member Noteproperty 'PageObject' $element
 
-$ModelType = "dynamic"
-$TemplateClassName = ("t{0}" -f ([System.IO.Path]::GetRandomFileName() -replace "\.",""))
-$TemplateBaseClassName = ("t{0}" -f ([System.IO.Path]::GetRandomFileName() -replace "\.",""))
-$TemplateNamespace = "Kkj.Templates"
 
-$templateBaseCode = @"
+[object]$model3 = New-Object PSObject
+
+$items = @()
+
+(0..2) | ForEach-Object {
+
+  $cnt = $_
+  $element = New-Object PSObject
+  $element | Add-Member Noteproperty 'Name' ('element_name_{0}' -f $cnt)
+  $element | Add-Member Noteproperty 'Type' 'ordered_list'
+  $element | Add-Member Noteproperty 'How' 'CssSelector'
+  $element | Add-Member Noteproperty 'HtmlTag' ('element_html_tag_{0}' -f $cnt)
+  $element | Add-Member Noteproperty 'Locator' ('div > :nth-of-type({0})' -f $cnt)
+
+
+  $items += $element
+}
+[object]$pageobject_items = New-Object PSObject
+$pageobject_items | Add-Member Noteproperty 'Items' $items
+
+$model3 | Add-Member Noteproperty 'PageObject' $pageobject_items
+
+$models = @(
+@{ 
+'input' =  '<ul>@foreach(var i in Model){<li>@i</li>}</ul>';
+[object]'model' = @( 0..3);
+},
+@{ 
+'input' =   @"
+
+@{
+
+    <text>###</text>
+    <text>@@Model.PageObject.Name = '@Model.PageObject.Name'</text>
+    <text>@@Model.PageObject.HtmlTag = '@Model.PageObject.HtmlTag'</text> 
+    <text>@@Model.PageObject.Type = '@Model.PageObject.Type'</text> 
+    <text>@@Model.PageObject.How = '@Model.PageObject.How'</text>
+    <text>@@Model.PageObject.Locator = '@Model.PageObject.Locator'</text>
+    <text>@RubyWebElement(@Model.PageObject.Name, @Model.PageObject.HtmlTag, @Model.PageObject.Type, @Model.PageObject.How, @Model.PageObject.Locator);</text>
+    <text>###
+    </text>
+}
+
+"@;
+[object]'model' = $model2;
+},
+@{ 
+'input' =  @"
+
+@foreach (var element in @Model.PageObject.Items ) 
+{
+      
+    <text>###</text>
+    <text>@@element.Name = '@element.Name'</text>
+    <text>@@element.HtmlTag = '@element.HtmlTag'</text> 
+    <text>@@element.Type = '@element.Type'</text> 
+    <text>@@element.How = '@element.How'</text>
+    <text>@@element.Locator = '@element.Locator'</text>
+    <text>@RubyWebElement(@element.Name, @element.HtmlTag, @element.Type, @element.How, @element.Locator)</text>
+    <text>### 
+    </text>
+}
+
+"@ ;
+[object]'model' = $model3;
+
+
+}
+)
+
+$input = $models[2].input
+$model = $models[2].model
+ 
+$template = @($imports, $functions, $input) -join "`r`n"
+
+$modelType = 'dynamic'
+$template_class_name = ('t_{0}' -f (Get-Random -Maximum 5000))
+$TemplateBaseClassName = ('t_{0}' -f (Get-Random -Maximum 5000))
+$template_namespace = 'razor.templates'
+
+# generate unique template base class
+$template_base_class = @"
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -221,44 +341,28 @@ namespace {2} {{
         }}
     }}
 }}
-"@ -f $ModelType,$TemplateBaseClassName,$TemplateNamespace
+"@ -f $modelType,$TemplateBaseClassName,$template_namespace
 
-#
-# A Razor template.
-#
-
-$razor_code_language = New-Object -TypeName 'System.Web.Razor.CSharpRazorCodeLanguage'
+# instantiate a razor template
+[System.Web.Razor.CSharpRazorCodeLanguage]$razor_code_language = New-Object System.Web.Razor.CSharpRazorCodeLanguage
 $razor_engine_host = New-Object -TypeName 'System.Web.Razor.RazorEngineHost' -ArgumentList $razor_code_language -Property @{
-  DefaultBaseClass = ("{0}.{1}" -f $TemplateNamespace,$TemplateBaseClassName);
-  DefaultClassName = $TemplateClassName;
-  DefaultNamespace = $TemplateNamespace;
+  DefaultBaseClass = ('{0}.{1}' -f $template_namespace,$TemplateBaseClassName);
+  DefaultClassName = $template_class_name;
+  DefaultNamespace = $template_namespace;
 }
-$razor_template_engine = New-Object -TypeName System.Web.Razor.RazorTemplateEngine -ArgumentList $razor_engine_host
+$razor_template_engine = New-Object System.Web.Razor.RazorTemplateEngine($razor_engine_host)
 
-$stringReader = New-Object -TypeName 'System.IO.StringReader' -ArgumentList $Template3
-$code = $razor_template_engine.GenerateCode($stringReader)
+$sr = New-Object System.IO.StringReader($template)
+$code = $razor_template_engine.GenerateCode($sr)
 
-#
-# Template compilation.
-#
-$stringWriter = New-Object -TypeName System.IO.StringWriter
-$compiler = New-Object -TypeName Microsoft.CSharp.CSharpCodeProvider
-$compilerResult = $compiler.GenerateCodeFromCompileUnit(
-  $code.GeneratedCode,$stringWriter,$null
-)
-$templateCode =
-$templateBaseCode + "`n" + $stringWriter.ToString()
-Add-Type `
-   -TypeDefinition $templateCode `
-   -ReferencedAssemblies System.Core,Microsoft.CSharp
+# do template compilation
+[System.IO.StringWriter]$sw = New-Object System.IO.StringWriter
+[Microsoft.CSharp.CSharpCodeProvider]$compiler = New-Object Microsoft.CSharp.CSharpCodeProvider
+[void]$compiler.GenerateCodeFromCompileUnit( $code.GeneratedCode, $sw, $null)
 
-#
-# Template execution.
-#
-$templateInstance = New-Object -TypeName ("{0}.{1}" -f $TemplateNamespace,$TemplateClassName)
+# do template execution
+Add-Type -TypeDefinition ($template_base_class + "`n" + $sw.ToString()) -ReferencedAssemblies 'System.Core','Microsoft.CSharp'
 
-$x = $templateInstance
-Write-Output $x.ToString()
-Write-Output ($templateInstance | Get-Member)
-$Model2.PageObject | Get-Member
-$templateInstance.Render($Model2)
+$instance = New-Object -TypeName ('{0}.{1}' -f $template_namespace, $template_class_name)
+
+$instance.Render($model)
