714fec430a842a3f720b9bf6a0b91349b2a171ea
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-07-07 12:27:22 -0400

    intermediate revision

--- powershell/razor_processor.ps1
+++ powershell/razor_processor.ps1
@@ -59,43 +59,203 @@ $imports = @'
 
 # SCRIPT PARAMETERS.
 param(
-  [string]$browser = '',
-  [switch]$grid,
-  [int]$browser_version,
-  [string]$base_url,
+  [string]$browser = 'firefox',
+  [int]$browser_version,  # unused
+  [switch]$grid, # unused
+  [string]$base_url =  'https://github.com/dzharii/swd-recorder',
   [switch]$pause
 )
 # END OF PARAMETERS.
 
-$module_name = 'selenium_utils.psd1'
-$module_path = '.'
-  # MODULE_PATH environment overrides parameter, e.g. for Team City
-  if (($env:MODULE_PATH -ne $null) -and ($env:MODULE_PATH -ne '')) {
-    $module_path = $env:MODULE_PATH
+function launch_selenium {
+  param(
+    $browser = ''
+  )
+  $driver_folder_path = 'C:\java\selenium' 
+  $shared_assemblies_path = 'c:\developer\sergueik\csharp\SharedAssemblies'
+  $shared_assemblies = @@(
+    'WebDriver.dll',
+    'WebDriver.Support.dll',
+    'nunit.framework.dll'
+  )
+
+  pushd $shared_assemblies_path
+
+  $shared_assemblies | ForEach-Object {
+    if ($host.Version.Major -gt 2) {
+      Unblock-File -Path $_
+    }
+    Write-Debug $_
+    Add-Type -Path $_
+  }
+  popd
+  # adding driver folder to the path environment
+  if (-not (Test-Path $driver_folder_path))
+  {
+    throw "Folder ${driver_folder_path} does not Exist, cannot be added to $env:PATH"
+  }
+  # See if the new folder is already in the path.
+  if ($env:PATH | Select-String -SimpleMatch $driver_folder_path)
+  { Write-Debug "Folder ${driver_folder_path} already within `$env:PATH"
+
   }
 
-Import-Module -Name ('{0}/{1}' -f $module_path, $module_name )
-if ([bool]$PSBoundParameters['grid'].IsPresent) {
-  $selenium = launch_selenium -browser $browser -grid
-} else {
-  $selenium = launch_selenium -browser $browser
+  # Set the new PATH environment
+  $env:PATH = $env:PATH + ';' + $driver_folder_path
+
+
+  # launch browser
+  switch ($browser)
+  {
+    'firefox' {
+
+
+      $selenium = New-Object OpenQA.Selenium.Firefox.FirefoxDriver
+    }
+    'chrome' {
+      $selenium = New-Object OpenQA.Selenium.Chrome.ChromeDriver
+
+      $selenium = New-Object OpenQA.Selenium.Firefox.FirefoxDriver
+    }
+    'ie' {
+
+
+      $selenium = New-Object OpenQA.Selenium.IE.InternetExplorerDriver ($driver_folder_path)
+    }
+    default {
+      Write-Host 'Running on phantomjs'
+      $headless = $true
+      $phantomjs_executable_folder = 'C:\tools\phantomjs-2.0.0\bin'
+      $selenium = New-Object OpenQA.Selenium.PhantomJS.PhantomJSDriver ($phantomjs_executable_folder)
+      $selenium.Capabilities.setCapability('ssl-protocol','any')
+      $selenium.Capabilities.setCapability('ignore-ssl-errors',$true)
+      $selenium.Capabilities.setCapability('takesScreenshot',$true)
+      $selenium.Capabilities.setCapability('userAgent',$phantomjs_useragent)
+      $options = New-Object OpenQA.Selenium.PhantomJS.PhantomJSOptions
+      $options.AddAdditionalCapability('phantomjs.executable.path',$phantomjs_executable_folder)
+    }
+
+  }
+
+  return $selenium
 }
 
-# TEST FIXTURE
-if (-not $base_url) { return }
-$selenium.Navigate().GoToUrl($base_url)
-$selenium.Manage().Window.Maximize()
 
-# $element = find_page_element -xpath $xpath
-# $element = find_page_element -css_selector $css_selector
-# [OpenQA.Selenium.Interactions.Actions]$actions = New-Object OpenQA.Selenium.Interactions.Actions ($selenium)
-# $actions.MoveToElement([OpenQA.Selenium.IWebElement]$element).Click().Build().Perform()
-# $expected_text
+function find_element {
+  param(
+    [Parameter(ParameterSetName = 'set_xpath')] $xpath,
+    [Parameter(ParameterSetName = 'set_css_selector')] $css,
+    [Parameter(ParameterSetName = 'set_id')] $id,
+    [Parameter(ParameterSetName = 'set_linktext')] $linktext,
+    [Parameter(ParameterSetName = 'set_partial_link_text')] $partial_link_text,
+    [Parameter(ParameterSetName = 'set_css_tagname')] $tagname
+  )
+
+
+  # guard
+  $implemented_options = @@{
+    'xpath' = $true;
+    'css' = $true;
+    'id' = $false;
+    'linktext' = $false;
+    'partial_link_text' = $false;
+    'tagname' = $false;
+  }
+
+  $implemented.Keys | ForEach-Object { $option = $_
+    if ($psBoundParameters.ContainsKey($option)) {
+
+      if (-not $implemented_options[$option]) {
+
+        Write-Output ('Option {0} i not implemented' -f $option)
+
+
+
+      } else {
+
+      }
+    }
+  }
+  if ($false) {
+    Write-Output @@psBoundParameters | Format-Table -AutoSize
+  }
+  $element = $null
+  $wait_seconds = 5
+  $wait_polling_interval = 50
+
+  if ($css -ne $null) {
+
+    [OpenQA.Selenium.Support.UI.WebDriverWait]$wait = New-Object OpenQA.Selenium.Support.UI.WebDriverWait ($selenium,[System.TimeSpan]::FromSeconds($wait_seconds))
+    $wait.PollingInterval = $wait_polling_interval
+
+    try {
+      [void]$wait.Until([OpenQA.Selenium.Support.UI.ExpectedConditions]::ElementExists([OpenQA.Selenium.By]::CssSelector($css)))
+    } catch [exception]{
+      Write-Debug ("Exception : {0} ...`ncss = '{1}'" -f (($_.Exception.Message) -split "`n")[0],$css)
+    }
+    $element = $selenium.FindElement([OpenQA.Selenium.By]::CssSelector($css))
+
+
+  }
+
+
+  if ($xpath -ne $null) {
+
+    [OpenQA.Selenium.Support.UI.WebDriverWait]$wait = New-Object OpenQA.Selenium.Support.UI.WebDriverWait ($selenum,[System.TimeSpan]::FromSeconds($wait_seconds))
+    $wait.PollingInterval = $wait_polling_interval
+
+    try {
+      [void]$wait.Until([OpenQA.Selenium.Support.UI.ExpectedConditions]::ElementExists([OpenQA.Selenium.By]::XPath($xpath)))
+    } catch [exception]{
+      Write-Debug ("Exception : {0} ...`nxpath={1}" -f (($_.Exception.Message) -split "`n")[0],$xpath)
+    }
+
+    $element = $local:selenum_driver.FindElement([OpenQA.Selenium.By]::XPath($xpath))
+
+
+  }
+
+  return $element
+}
+
+function highlight {
+  param(
+    [object]$element,
+    [int]$delay = 300
+  )
+  # https://selenium.googlecode.com/git/docs/api/java/org/openqa/selenium/JavascriptExecutor.html
+  [OpenQA.Selenium.IJavaScriptExecutor]$selenium.ExecuteScript("arguments[0].setAttribute('style', arguments[1]);",$element,'color: yellow; border: 4px solid yellow;')
+  Start-Sleep -Millisecond $delay
+  [OpenQA.Selenium.IJavaScriptExecutor]$selenium.ExecuteScript("arguments[0].setAttribute('style', arguments[1]);",$element,'')
+}
+
+
+
+function cleanup
+{
+  param(
+    [object]$selenium
+  )
+  try {
+    $selenium.Quit()
+  } catch [exception]{
+    Write-Output (($_.Exception.Message) -split "`n")[0]
+    # Ignore errors if unable to close the browser
+  }
+}
+
+
+$selenium = launch_selenium -browser 
+
+
+[void]$selenium.Manage().Timeouts().ImplicitlyWait([System.TimeSpan]::FromSeconds(10))
+$selenium.Navigate().GoToUrl($base_url)
+
+
 # [NUnit.Framework.Assert]::IsTrue(($element.Text -match $expected_text),('expected: {0} got:{1}' -f $expected_text,$element.Text))
 
 # TEST CLEANUP
-# $actions = $null
-cleanup ([ref]$selenium)
+cleanup $selenium
 # END OF THE TEST
 
 '@
@@ -203,16 +363,16 @@ $models = @(
     'model_generator' = [scriptblock]{
 
       $model = New-Object PSObject
-
+      $how_choices = @('CssSelector', 'XPath', 'Id', 'LinkText' , 'ClassName')
       $items = @()
 
-      (0..2) | ForEach-Object {
+      (0..($how_choices.Count-1)) | ForEach-Object {
 
         $cnt = $_
         $element = New-Object PSObject
         $element | Add-Member Noteproperty 'Name' ('element_{0}' -f $cnt)
         $element | Add-Member Noteproperty 'Type' 'unused'
-        $element | Add-Member Noteproperty 'How' 'CssSelector'
+        $element | Add-Member Noteproperty 'How' $how_choices[$_]
         $element | Add-Member Noteproperty 'HtmlTag' ('element_html_tag_{0}' -f $cnt)
         $element | Add-Member Noteproperty 'Locator' ('div > :nth-of-type({0})' -f $cnt)
         $element | Add-Member Noteproperty 'Xpath' 'id("js-repo-pjax-container")/div[1]/table[1]/tbody[1]/tr[1]/td[1]/span[1]/a[1]'
