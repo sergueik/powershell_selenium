2c46ab7ddce9eedec2c8710441a94a4abf3ca887
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-07-26 15:37:33 -0400

    refactored version, ported method

--- java/hal/src/main/java/com/mycompany/app/App.java
+++ java/hal/src/main/java/com/mycompany/app/App.java
@@ -4,9 +4,14 @@ import java.io.File;
 import java.io.InputStream;
 import java.io.StringWriter;
 import java.lang.StringBuilder;
+import java.lang.RuntimeException;
+
+
 import java.util.concurrent.TimeUnit;
 import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.Hashtable;
+
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
@@ -94,7 +99,10 @@ public static void main(String[] args) throws InterruptedException,java.io.IOExc
 	capabilities =   new DesiredCapabilities("firefox", "", Platform.ANY);
 	FirefoxProfile profile = new ProfilesIni().getProfile("default");
 	capabilities.setCapability("firefox_profile", profile);
-	driver = new RemoteWebDriver(capabilities);
+	// run embedded
+	driver = new FirefoxDriver(capabilities);
+	// run remote
+	// driver = new RemoteWebDriver(capabilities);
 
 	// Wait For Page To Load
 	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -110,8 +118,9 @@ public static void main(String[] args) throws InterruptedException,java.io.IOExc
 
 
 	String value0 = "pnav-destinations";
-	String title0  = "Destinations & Excursions - opens submenu";
-
+	String title0  = null;
+	title0  = "Destinations & Excursions - opens submenu";
+	title0  = "Destinations - opens submenu";
 	String css_selector0 = String.format("a#%s", value0);
 	// Hover over menu
 	WebElement element0 =  driver.findElement(By.cssSelector(css_selector0));
@@ -121,22 +130,23 @@ public static void main(String[] args) throws InterruptedException,java.io.IOExc
 	// assertEquals(200, response.getStatusLine().getStatusCode());
 	assertTrue(String.format("Unexpected title '%s'", element0.getAttribute("title")), element0.getAttribute("title").matches(title0) );
 /*
-	String value1  = "/cruise-destinations/alaska?WT.ac=pnav_DestMap_Alaska";
-	String text1   = "Alaska & Yukon";
-	String title1  = "Alaska Cruise Vacations";
+        String value1  = "/cruise-destinations/alaska?WT.ac=pnav_DestMap_Alaska";
+        String text1   = "Alaska & Yukon";
+        String title1  = "Alaska Cruise Vacations";
 
-	String css_selector1 = String.format("a[href='%s']", value1);
-	WebDriverWait wait = new WebDriverWait(driver, 1);
-	wait.withTimeout(1, TimeUnit.SECONDS).pollingEvery(150, TimeUnit.MICROSECONDS ); 
-	wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(css_selector1)));
-	WebElement element1 = driver.findElement(By.cssSelector(css_selector1));
-        assertTrue(String.format("Unexpected text '%s'" , element1.getText()), element1.getText().matches(text1) ); 
-	System.out.println(String.format("Click on '%s'", element1.getText()));
-	new Actions(driver).moveToElement(element1).click().build().perform();
-	Thread.sleep(3000L);
+        String css_selector1 = String.format("a[href='%s']", value1);
+        WebDriverWait wait = new WebDriverWait(driver, 1);
+        wait.withTimeout(1, TimeUnit.SECONDS).pollingEvery(150, TimeUnit.MICROSECONDS );
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(css_selector1)));
+        // WebElement element1 = driver.findElement(By.cssSelector(css_selector1));
+        WebElement element1 = find_element("css_selector", css_selector1) ;
+        assertTrue(String.format("Unexpected text '%s'" , element1.getText()), element1.getText().matches(text1) );
+        System.out.println(String.format("Click on '%s'", element1.getText()));
+        new Actions(driver).moveToElement(element1).click().build().perform();
+        Thread.sleep(3000L);
         assertTrue(driver.getTitle(),driver.getTitle().contains(title1));
-	driver.navigate().back();
-*/
+        driver.navigate().back();
+ */
 	//take a screenshot
 	File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 	String currentDir = System.getProperty("user.dir");
@@ -146,6 +156,86 @@ public static void main(String[] args) throws InterruptedException,java.io.IOExc
 	//closing current driver window
 	driver.close();
 }
-       // http://selenium-interview-questions.blogspot.in/2014/02/how-to-take-screenshot-of-webpage-using.html
+// http://selenium-interview-questions.blogspot.in/2014/02/how-to-take-screenshot-of-webpage-using.html
+
+
+// http://www.adam-bien.com/roller/abien/entry/named_parameters_in_java_8
+// does nothing exist prior to java 8 ?
+
+private WebElement find_element(String selector_type, String selector_value){
+	int flexible_wait_interval = 5;
+	long wait_polling_interval = 500;
+	WebDriverWait wait = new WebDriverWait(driver, flexible_wait_interval );
+	wait.pollingEvery(wait_polling_interval,TimeUnit.MILLISECONDS);
+	WebElement element = null;
+
+	Hashtable<String, Boolean> supported_selectors = new Hashtable<String, Boolean>();
+	supported_selectors.put("id", true);
+	supported_selectors.put("css_selector", true);
+	supported_selectors.put("xpath", true);
+	supported_selectors.put("partial_link_text", false);
+	supported_selectors.put("link_text", true);
+	supported_selectors.put("classname", false);
+
+	if (selector_type == null || !supported_selectors.containsKey(selector_type) || !supported_selectors.get(selector_type)) {
+		return null;
+	}
+	if (selector_type == "id") {
+
+		try {
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(selector_value)));
+		} catch (RuntimeException timeoutException) {
+		return null;
+		}
+		element = driver.findElement(By.id(selector_value));
+	}
+
+	if (selector_type == "id") {
+
+		try {
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(selector_value)));
+		} catch (RuntimeException timeoutException) {
+			return null;
+		}
+		element = driver.findElement(By.id(selector_value));
+	}
+
+
+	if (selector_type == "link_text") {
+
+		try {
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText(selector_value)));
+		} catch (RuntimeException timeoutException) { 
+			return null;
+		}
+		element = driver.findElement(By.linkText(selector_value));
+	}
+
+
+	if (selector_type == "css_selector") {
+
+		try {
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(selector_value)));
+		} catch (RuntimeException timeoutException) {
+			return null;
+		}
+		element = driver.findElement(By.cssSelector(selector_value));
+	}
+
+
+	if (selector_type == "xpath") {
+
+		try {
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(selector_value)));
+		} catch (RuntimeException timeoutException) {
+			return null;
+		}
+		element = driver.findElement(By.xpath(selector_value));
+	}
+
+	return element;
 }
+
+}
+
 
