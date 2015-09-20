cef2b77779ad8c4d056f056771fed59990499a2d
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-09-19 20:07:25 -0400

    intermediate revision - started searches

--- powershell/freelancer_search.ps1
+++ powershell/freelancer_search.ps1
@@ -42,7 +42,7 @@ load_shared_assemblies
 
 if ([bool]$PSBoundParameters['grid'].IsPresent) {
   $selenium = launch_selenium -browser $browser -grid
-  start-sleep -millisecond 500
+  Start-Sleep -Millisecond 500
 } else {
   $selenium = launch_selenium -browser $browser
 }
@@ -83,34 +83,105 @@ $login_password_element.SendKeys($login_password_data)
 
 
 
-[string]$login_submit_selector =  "form[id='login-form'] button[id='login-bt']"
+[string]$login_submit_selector = "form[id='login-form'] button[id='login-bt']"
 [object]$login_submit_element = find_element_new -css_selector $login_submit_selector
 highlight ([ref]$selenium) ([ref]$login_submit_element)
 [void]$actions.MoveToElement([OpenQA.Selenium.IWebElement]$login_submit_element).Click().Build().Perform()
 
-$wait_seconds =  10
-$wait_polling_interval = 300  
-  [OpenQA.Selenium.Support.UI.WebDriverWait]$wait = New-Object OpenQA.Selenium.Support.UI.WebDriverWait ($selenium,[System.TimeSpan]::FromSeconds($wait_seconds))
-  $wait.PollingInterval = $wait_polling_interval
+$wait_seconds = 10
+$wait_polling_interval = 300
+[OpenQA.Selenium.Support.UI.WebDriverWait]$wait = New-Object OpenQA.Selenium.Support.UI.WebDriverWait ($selenium,[System.TimeSpan]::FromSeconds($wait_seconds))
+$wait.PollingInterval = $wait_polling_interval
 
 [string]$profile_figure_selector = "figure[id='profile-figure'][class='profile-img']"
-    try {
-      [void]$wait.Until([OpenQA.Selenium.Support.UI.ExpectedConditions]::ElementExists([OpenQA.Selenium.By]::CssSelector($profile_figure_selector)))
-    } catch [exception]{
-      Write-Debug ("Exception : {0} ...`ncss = '{1}'" -f (($_.Exception.Message) -split "`n")[0],$profile_figure_selector)
-    }
+try {
+  [void]$wait.Until([OpenQA.Selenium.Support.UI.ExpectedConditions]::ElementExists([OpenQA.Selenium.By]::CssSelector($profile_figure_selector)))
+} catch [exception]{
+  Write-Debug ("Exception : {0} ...`ncss = '{1}'" -f (($_.Exception.Message) -split "`n")[0],$profile_figure_selector)
+}
+[object]$profile_figure_element = find_element_new -css_selector $profile_figure_selector
+
+highlight ([ref]$selenium) ([ref]$profile_figure_element)
+[NUnit.Framework.StringAssert]::Contains('www.freelancer.com/dashboard/',$selenium.url,{})
+Start-Sleep -Millisecond 1000
+
+1..2 | ForEach-Object {
+  $page_count = $_
+  Write-Host "Page count: ${page_count}"
+
+  $selenium.Navigate().GoToUrl(('{0}/jobs/myskills/{1}/' -f $base_url,$page_count))
+
+  [NUnit.Framework.StringAssert]::Contains(('{0}/jobs/myskills/{1}/' -f $base_url,$page_count),$selenium.url,{})
+
+  [string]$project_table_selector = "table[id=project_table]"
+  [object]$project_table_element = find_element_new -css_selector $project_table_selector
+  [void]$actions.MoveToElement([OpenQA.Selenium.IWebElement]$project_table_element).Build().Perform()
+
+  highlight ([ref]$selenium) ([ref]$project_table_element)
+
+
+
+  [string]$project_selector = "tr[class='project-description']"
+
+  $project_elements = $project_table_element.FindElements([OpenQA.Selenium.By]::CssSelector($project_selector))
+
+
+  $project_elements.Count
+  # $project_elements[0].getAttribute('innerHTML')
+  # $project_elements[0]
+  $project_elements | ForEach-Object {
+    $project_element = $_
+    [string]$project_synopsis_selector = 'div[class="project-synopsis"]'
+    $project_synopsis_element = $project_element.FindElement([OpenQA.Selenium.By]::CssSelector($project_synopsis_selector))
+    $project_synopsis_text = ($project_synopsis_element.getAttribute('innerHTML') -join '')
+    $project_synopsis_text = $project_synopsis_text -replace '<p>','' -replace '</p>','' -replace '<p class=".*" style=".*">','' -replace '\r?\n',' ' -replace ' +',' ' -replace '^ +',''
+    Write-Host -ForegroundColor 'yellow' $project_synopsis_text
+    # makes browser unstable
+    #  [void]$actions.MoveToElement([OpenQA.Selenium.IWebElement]$project_synopsis_element).Build().Perform()
+
+    [string]$project_actions_selector = 'div[class="project-actions"] a'
+    $project_actions_element = $project_element.FindElement([OpenQA.Selenium.By]::CssSelector($project_actions_selector))
+    Write-Host -ForegroundColor 'green' $project_actions_element.getAttribute('href')
+
+  }
+
+  # next page
+
+  [string]$pagination_selector = "div[class*='dataTables_paginate']"
+  [object]$pagination_element = find_element_new -css_selector $pagination_selector
+  highlight ([ref]$selenium) ([ref]$pagination_element)
+  Start-Sleep -Millisecond 1000
+
+  [string]$project_next_page_selector = "div[class*='dataTables_paginate'] li[class*='next']"
+  [object]$project_next_page_element = find_element_new -css_selector $project_next_page_selector
+  highlight ([ref]$selenium) ([ref]$project_next_page_element)
+  [void]$actions.MoveToElement([OpenQA.Selenium.IWebElement]$project_next_page_element).Click().Build().Perform()
+  Start-Sleep -Millisecond 1000
+
+
+}
+
+Start-Sleep -Millisecond 1000
+[string]$profile_figure_selector = "figure[id='profile-figure'][class='profile-img']"
+try {
+  [void]$wait.Until([OpenQA.Selenium.Support.UI.ExpectedConditions]::ElementExists([OpenQA.Selenium.By]::CssSelector($profile_figure_selector)))
+} catch [exception]{
+  Write-Debug ("Exception : {0} ...`ncss = '{1}'" -f (($_.Exception.Message) -split "`n")[0],$profile_figure_selector)
+}
 [object]$profile_figure_element = find_element_new -css_selector $profile_figure_selector
 
 highlight ([ref]$selenium) ([ref]$profile_figure_element)
 
-Start-Sleep -millisecond 1000
 [void]$actions.MoveToElement([OpenQA.Selenium.IWebElement]$profile_figure_element).Click().Build().Perform()
+# TODO - click on logut
 
 
-[NUnit.Framework.StringAssert]::Contains('www.freelancer.com/dashboard/',$selenium.url,{})
 
 $selenium.Navigate().GoToUrl("{0}/users/onsignout.php" -f $base_url)
-Start-Sleep -millisecond 1000
+Start-Sleep -Millisecond 1000
 
-
+# a class="primary-navigation-link is-active" ng-mouseenter="trackHover(navItem)" fl-analytics="MySkillsProjects" target="" ng-class="{ 'is-active': navItem.isCurrent }" ng-href="/jobs/myskills/1/" href="/jobs/myskills/1/"
+# g-include class="ng-scope" src="templateDir + '/' + subNavItem.id + '.html'">
+# <span class="ng-scope" i18n-id="3e50c7ab20ebe01229fc00e47250ca32" i18n-msg="Browse Projects">Browse Projects</span>
+# https://www.freelancer.com/jobs/myskills/1/
 cleanup ([ref]$selenium)
