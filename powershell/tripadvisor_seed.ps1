23e83e39eea4e99ef1aaf62ac8fcadaef5df4313
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-09-21 17:57:04 -0400

    intermediate revision - added search result collection

--- powershell/tripadvisor_seed.ps1
+++ powershell/tripadvisor_seed.ps1
@@ -46,27 +46,25 @@ $selenium.Navigate().GoToUrl($base_url)
 
 write-output 'Input main search'
 $password = 'restaurant'
-[string]$login_password_selector = "input#mainSearch"
-[string]$login_password_data = $password
-[object]$login_password_element = find_element -css_selector $login_password_selector
-highlight ([ref]$selenium) ([ref]$login_password_element)
-$login_password_element.Clear()
+[string]$mainsearch_selector = "input#mainSearch"
+[string]$mainsearch_data = $password
+[object]$mainsearch_element = find_element -css_selector $mainsearch_selector
+highlight ([ref]$selenium) ([ref]$mainsearch_element)
+$mainsearch_element.Clear()
 
-$login_password_element.SendKeys($login_password_data)
+$mainsearch_element.SendKeys(($mainsearch_data + [OpenQA.Selenium.Keys]::Enter))
 
 
 write-output 'Input search of interest'
 $username = 'Prague, Czech Republic, Europe'
-[string]$login_username_selector = "input[id='GEO_SCOPED_SEARCH_INPUT']"
-[string]$login_username_data = $username + [OpenQA.Selenium.Keys]::Enter
-# [OpenQA.Selenium.Keys]::Tab +
-[object]$login_username_element = find_element -css_selector $login_username_selector
-highlight ([ref]$selenium) ([ref]$login_username_element)
-$login_username_element.Clear()
-$login_username_element.SendKeys($login_username_data)
+[string]$point_of_interest_city_country_selector = "input[id='GEO_SCOPED_SEARCH_INPUT']"
+[string]$point_of_interest_city_country_data = $username + [OpenQA.Selenium.Keys]::Enter
+[object]$point_of_interest_city_country_element = find_element -css_selector $point_of_interest_city_country_selector
+highlight ([ref]$selenium) ([ref]$point_of_interest_city_country_element)
+$point_of_interest_city_country_element.Clear()
+$point_of_interest_city_country_element.SendKeys($point_of_interest_city_country_data)
 
 Start-Sleep -Millisecond 100
-
 
 [string]$div_geoscoped_selector = "div[id='GEO_SCOPE_CONTAINER'] > div[class *='geoScopeDisplay']"
 [object]$div_geoscoped_element = find_element -css_selector $div_geoscoped_selector
@@ -74,72 +72,6 @@ Start-Sleep -Millisecond 100
 # highlight ([ref]$selenium) ([ref]$div_geoscoped_element)
 
 
-# $div_geoscoped_element.GetAttribute('innerHTML') 
-<#
-<ul class="resultContainer">
-  <li class="displayItem result selected">
-    <div class="sprite-image leftIcon sprite-typeahead-destination"/>
-    <span class="poi-name primaryText">Prague</span>
-    <span class="geo-name secondaryText">Czech Republic, Europe</span>
-  </li>
-  <li class="displayItem result">
-    <div class="sprite-image leftIcon sprite-typeahead-destination"/>
-    <span class="poi-name primaryText">Bohemia</span>
-    <span class="geo-name secondaryText">Czech Republic, Europe</span>
-  </li>
-  <li class="displayItem result">
-    <div class="sprite-image leftIcon sprite-typeahead-destination"/>
-    <span class="poi-name primaryText">Czech Republic</span>
-    <span class="geo-name secondaryText">Europe</span>
-  </li>
-  <li class="displayItem result">
-    <div class="sprite-image leftIcon sprite-typeahead-destination"/>
-    <span class="poi-name primaryText">Republic of Macedonia</span>
-    <span class="geo-name secondaryText">Europe</span>
-  </li>
-  <li class="displayItem result">
-    <div class="sprite-image leftIcon sprite-typeahead-destination"/>
-    <span class="poi-name primaryText">Republic of Tatarstan</span>
-    <span class="geo-name secondaryText">Russia, Europe</span>
-  </li>
-  <li class="displayItem result">
-    <div class="sprite-image leftIcon sprite-typeahead-destination"/>
-    <span class="poi-name primaryText">Republic of Bashkortostan</span>
-    <span class="geo-name secondaryText">Russia, Europe</span>
-  </li>
-  <li class="displayItem result">
-    <div class="sprite-image leftIcon sprite-typeahead-destination"/>
-    <span class="poi-name primaryText">Republic of Buryatia</span>
-    <span class="geo-name secondaryText">Russia, Europe</span>
-  </li>
-  <li class="displayItem result">
-    <div class="sprite-image leftIcon sprite-typeahead-destination"/>
-    <span class="poi-name primaryText">Republic of Karelia</span>
-    <span class="geo-name secondaryText">Russia, Europe</span>
-  </li>
-  <li class="displayItem result">
-    <div class="sprite-image leftIcon sprite-typeahead-destination"/>
-    <span class="poi-name primaryText">Udmurt Republic</span>
-    <span class="geo-name secondaryText">Russia, Europe</span>
-  </li>
-  <li class="displayItem result">
-    <div class="sprite-image leftIcon sprite-typeahead-destination"/>
-    <span class="poi-name primaryText">Chuvash Republic</span>
-    <span class="geo-name secondaryText">Russia, Europe</span>
-  </li>
-  <li class="displayItem result">
-    <div class="sprite-image leftIcon sprite-typeahead-destination"/>
-    <span class="poi-name primaryText">Sakha (Yakutia) Republic</span>
-    <span class="geo-name secondaryText">Russia, Europe</span>
-  </li>
-  <li class="displayItem result">
-    <div class="sprite-image leftIcon sprite-typeahead-destination"/>
-    <span class="poi-name primaryText">Republic of Altai</span>
-    <span class="geo-name secondaryText">Russia, Europe</span>
-  </li>
-</ul>
-
-#>
 Start-Sleep -Millisecond 1000
 
 
@@ -171,16 +103,34 @@ $elements | ForEach-Object {
 }
 
 write-output 'Search'
-[string]$login_submit_selector = "button[id='SEARCH_BUTTON']"
-[object]$login_submit_element = find_element -css_selector $login_submit_selector
-highlight ([ref]$selenium) ([ref]$login_submit_element)
-[void]$actions.MoveToElement([OpenQA.Selenium.IWebElement]$login_submit_element).Click().Build().Perform()
+[string]$search_submit_selector = "button[id='SEARCH_BUTTON']"
+[object]$search_submit_element = find_element -css_selector $search_submit_selector
+highlight ([ref]$selenium) ([ref]$search_submit_element)
+[void]$actions.MoveToElement([OpenQA.Selenium.IWebElement]$search_submit_element).Click().Build().Perform()
 Start-Sleep -Millisecond 3000
 write-output 'Search results'
 [NUnit.Framework.StringAssert]::Contains('/Restaurants',$selenium.url,{})
 [NUnit.Framework.StringAssert]::Contains('Bohemia',$selenium.url,{})
 
-# div#EATERY_SEARCH_RESULTS a.property_title
+$search_results_css_selector = "div[id='EATERY_SEARCH_RESULTS'] a.property_title"
+
+$search_results_elements = $selenium.FindElements([OpenQA.Selenium.By]::CssSelector($search_results_css_selector))
+
+
+$search_results_elements.count
+$max_count = 10
+$element_count = 0
+$search_results_elements | ForEach-Object {
+  $element_count++
+  if ($element_count -gt $max_count) {
+    return
+  }
+
+  $search_results_element = $_
+  [void]$actions.MoveToElement([OpenQA.Selenium.IWebElement]$search_results_element).Build().Perform()
+  $search_results_element.Text
+  highlight_new -element $search_results_element
+}
 custom_pause -fullstop $fullstop
 
 Start-Sleep -Millisecond 10000
