5e7dc131d9147fd25b39850320ae510524acafda
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-09-21 19:13:28 -0400

    intermediate revision - added page forward

--- powershell/tripadvisor_seed.ps1
+++ powershell/tripadvisor_seed.ps1
@@ -39,15 +39,15 @@ if ([bool]$PSBoundParameters['grid'].IsPresent) {
 } else {
   $selenium = launch_selenium -browser $browser
 }
-
+$selenium.Manage().Window.Maximize()
 $selenium.Navigate().GoToUrl($base_url)
 
 [OpenQA.Selenium.Interactions.Actions]$actions = New-Object OpenQA.Selenium.Interactions.Actions ($selenium)
 
-write-output 'Input main search'
-$password = 'restaurant'
+Write-Output 'Input main search'
+$main_search_word = 'restaurant'
 [string]$mainsearch_selector = "input#mainSearch"
-[string]$mainsearch_data = $password
+[string]$mainsearch_data = $main_search_word
 [object]$mainsearch_element = find_element -css_selector $mainsearch_selector
 highlight ([ref]$selenium) ([ref]$mainsearch_element)
 $mainsearch_element.Clear()
@@ -55,10 +55,10 @@ $mainsearch_element.Clear()
 $mainsearch_element.SendKeys(($mainsearch_data + [OpenQA.Selenium.Keys]::Enter))
 
 
-write-output 'Input search of interest'
-$username = 'Prague, Czech Republic, Europe'
+Write-Output 'Input search of interest'
+$city_country = 'Prague, Czech Republic, Europe'
 [string]$point_of_interest_city_country_selector = "input[id='GEO_SCOPED_SEARCH_INPUT']"
-[string]$point_of_interest_city_country_data = $username + [OpenQA.Selenium.Keys]::Enter
+[string]$point_of_interest_city_country_data = $city_country + [OpenQA.Selenium.Keys]::Enter
 [object]$point_of_interest_city_country_element = find_element -css_selector $point_of_interest_city_country_selector
 highlight ([ref]$selenium) ([ref]$point_of_interest_city_country_element)
 $point_of_interest_city_country_element.Clear()
@@ -96,43 +96,61 @@ $elements | ForEach-Object {
   $element.Text
   if ($element.Text -match 'Bohemia') {
     [void]$actions.MoveToElement([OpenQA.Selenium.IWebElement]$element).Click().Build().Perform()
-    write-output 'Selected point ot interest'
+    Write-Output 'Selected point ot interest'
     $element_found = $true
     return
   }
 }
 
-write-output 'Search'
+Write-Output 'Search'
 [string]$search_submit_selector = "button[id='SEARCH_BUTTON']"
 [object]$search_submit_element = find_element -css_selector $search_submit_selector
 highlight ([ref]$selenium) ([ref]$search_submit_element)
 [void]$actions.MoveToElement([OpenQA.Selenium.IWebElement]$search_submit_element).Click().Build().Perform()
 Start-Sleep -Millisecond 3000
-write-output 'Search results'
+Write-Output 'Search results'
 [NUnit.Framework.StringAssert]::Contains('/Restaurants',$selenium.url,{})
 [NUnit.Framework.StringAssert]::Contains('Bohemia',$selenium.url,{})
 
-$search_results_css_selector = "div[id='EATERY_SEARCH_RESULTS'] a.property_title"
 
-$search_results_elements = $selenium.FindElements([OpenQA.Selenium.By]::CssSelector($search_results_css_selector))
+0..2 | ForEach-Object {
+  $page_count = $_
+
+  $search_results_css_selector = "div[id='EATERY_SEARCH_RESULTS'] a.property_title"
+
+  $search_results_elements = $selenium.FindElements([OpenQA.Selenium.By]::CssSelector($search_results_css_selector))
 
 
-$search_results_elements.count
-$max_count = 10
-$element_count = 0
-$search_results_elements | ForEach-Object {
-  $element_count++
-  if ($element_count -gt $max_count) {
-    return
+  $search_results_elements.count
+  $max_count = 100
+  $element_count = 0
+  $search_results_elements | ForEach-Object {
+    $element_count++
+    if ($element_count -gt $max_count) {
+      return
+    }
+
+    $search_results_element = $_
+    [void]$actions.MoveToElement([OpenQA.Selenium.IWebElement]$search_results_element).Build().Perform()
+    $search_results_element.Text
+    highlight_new -element $search_results_element
   }
 
-  $search_results_element = $_
-  [void]$actions.MoveToElement([OpenQA.Selenium.IWebElement]$search_results_element).Build().Perform()
-  $search_results_element.Text
-  highlight_new -element $search_results_element
-}
-custom_pause -fullstop $fullstop
+  custom_pause -fullstop $fullstop
 
-Start-Sleep -Millisecond 10000
+  [string]$pagination_selector = "div[id = 'EATERY_LIST_CONTENTS'] div[class *='pagination']"
+  [object]$pagination_element = find_element -css_selector $pagination_selector
+  highlight ([ref]$selenium) ([ref]$pagination_element)
+
+  custom_pause -fullstop $fullstop
+
+  [string]$results_next_page_selector = ("{0} a.nav.next" -f $pagination_selector)
+  [object]$results_next_page_element = find_element -css_selector $results_next_page_selector
+  highlight ([ref]$selenium) ([ref]$results_next_page_element)
+
+  [void]$actions.MoveToElement([OpenQA.Selenium.IWebElement]$results_next_page_element).Click().Build().Perform()
+
+  Start-Sleep -Millisecond 10000
+}
 
 cleanup ([ref]$selenium)
