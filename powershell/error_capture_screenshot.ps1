51a7e36fcfbde2d9d02a8efbddf089dde3da5bfe
sergueik <kouzmine_serguei@yahoo.com>
2015-10-11 15:29:16 -0400

    refactoring common module

--- powershell/error_capture_screenshot.ps1
+++ powershell/error_capture_screenshot.ps1
@@ -20,108 +20,33 @@
 
 
 # http://seleniumeasy.com/selenium-tutorials/set-browser-width-and-height-in-selenium-webdriver
+
 param(
   [string]$browser = 'firefox',
+  [string]$base_url = 'http://stackoverflow.com',
   [int]$event_delay = 250,
+  [switch]$grid,
+  [switch]$debug,
   [switch]$pause
-
 )
 
+[bool]$fullstop = [bool]$PSBoundParameters['pause'].IsPresent
 
-function netstat_check
-{
-  param(
-    [string]$selenium_http_port = 4444
-  )
+$MODULE_NAME = 'selenium_utils.psd1'
+Import-Module -Name ('{0}/{1}' -f '.',$MODULE_NAME)
+load_shared_assemblies
 
-  $results = Invoke-Expression -Command "netsh interface ipv4 show tcpconnections"
 
-  $t = $results -split "`r`n" | Where-Object { ($_ -match "\s$selenium_http_port\s") }
-  (($t -ne '') -and $t -ne $null)
-
+if ([bool]$PSBoundParameters['grid'].IsPresent) {
+  $selenium = launch_selenium -browser $browser -grid
+  Start-Sleep -Millisecond 500
+} else {
+  $selenium = launch_selenium -browser $browser
 }
 
-function cleanup
-{
-  param(
-    [System.Management.Automation.PSReference]$selenium_ref
-  )
-  try {
-    $selenium_ref.Value.Quit()
-  } catch [exception]{
-    Write-Output (($_.Exception.Message) -split "`n")[0]
-    # Ignore errors if unable to close the browser
-  }
-}
 
-$shared_assemblies = @(
-  'WebDriver.dll',
-  'WebDriver.Support.dll',# for Events
-  'nunit.core.dll',
-  'nunit.framework.dll'
-)
-
-$shared_assemblies_path = 'c:\developer\sergueik\csharp\SharedAssemblies'
-
-if (($env:SHARED_ASSEMBLIES_PATH -ne $null) -and ($env:SHARED_ASSEMBLIES_PATH -ne '')) {
-  $shared_assemblies_path = $env:SHARED_ASSEMBLIES_PATH
-}
-pushd $shared_assemblies_path
-$shared_assemblies | ForEach-Object {
-  # Unblock-File -Path $_; 
-  Add-Type -Path $_
-}
-popd
 
 [void][System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
-$verificationErrors = New-Object System.Text.StringBuilder
-$phantomjs_executable_folder = "C:\tools\phantomjs"
-if ($browser -ne $null -and $browser -ne '') {
-  try {
-    $connection = (New-Object Net.Sockets.TcpClient)
-    $connection.Connect("127.0.0.1",4444)
-    $connection.Close()
-  } catch {
-    Start-Process -FilePath "C:\Windows\System32\cmd.exe" -ArgumentList "start cmd.exe /c c:\java\selenium\hub.cmd"
-    Start-Process -FilePath "C:\Windows\System32\cmd.exe" -ArgumentList "start cmd.exe /c c:\java\selenium\node.cmd"
-    Start-Sleep -Seconds 10
-  }
-  Write-Host "Running on ${browser}" -foreground 'Yellow'
-  if ($browser -match 'firefox') {
-    $capability = [OpenQA.Selenium.Remote.DesiredCapabilities]::Firefox()
-
-  }
-  elseif ($browser -match 'chrome') {
-    $capability = [OpenQA.Selenium.Remote.DesiredCapabilities]::Chrome()
-  }
-  elseif ($browser -match 'ie') {
-    $capability = [OpenQA.Selenium.Remote.DesiredCapabilities]::InternetExplorer()
-    if ($version -ne $null -and $version -ne 0) {
-      $capability.SetCapability("version",$version.ToString());
-    }
-
-  }
-  elseif ($browser -match 'safari') {
-    $capability = [OpenQA.Selenium.Remote.DesiredCapabilities]::Safari()
-  }
-  else {
-    throw "unknown browser choice:${browser}"
-  }
-  $uri = [System.Uri]("http://127.0.0.1:4444/wd/hub")
-  $selenium = New-Object OpenQA.Selenium.Remote.RemoteWebDriver ($uri,$capability)
-} else {
-  Write-Host 'Running on phantomjs' -foreground 'Yellow'
-  $phantomjs_executable_folder = "C:\tools\phantomjs"
-  $selenium = New-Object OpenQA.Selenium.PhantomJS.PhantomJSDriver ($phantomjs_executable_folder)
-  $selenium.Capabilities.SetCapability("ssl-protocol","any")
-  $selenium.Capabilities.SetCapability("ignore-ssl-errors",$true)
-  $selenium.Capabilities.SetCapability("takesScreenshot",$true)
-  $selenium.Capabilities.SetCapability("userAgent","Mozilla/5.0 (Windows NT 6.1) AppleWebKit/534.34 (KHTML, like Gecko) PhantomJS/1.9.7 Safari/534.34")
-  $options = New-Object OpenQA.Selenium.PhantomJS.PhantomJSOptions
-  $options.AddAdditionalCapability("phantomjs.executable.path",$phantomjs_executable_folder)
-}
-
-
 if ($host.Version.Major -le 2) {
   [void][System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
   $selenium.Manage().Window.Size = New-Object System.Drawing.Size (600,400)
@@ -133,23 +58,6 @@ if ($host.Version.Major -le 2) {
 
 $window_position = $selenium.Manage().Window.Position
 $window_size = $selenium.Manage().Window.Size
-
-$base_url = 'http://stackoverflow.com'
-
-
-# http://stackoverflow.com/questions/8343767/how-to-get-the-current-directory-of-the-cmdlet-being-executed
-function Get-ScriptDirectory {
-  $Invocation = (Get-Variable MyInvocation -Scope 1).Value
-  if ($Invocation.PSScriptRoot) {
-    $Invocation.PSScriptRoot
-  }
-  elseif ($Invocation.MyCommand.Path) {
-    Split-Path $Invocation.MyCommand.Path
-  } else {
-    $Invocation.InvocationName.Substring(0,$Invocation.InvocationName.LastIndexOf(""))
-  }
-}
-
 
 # https://github.com/yizeng/EventFiringWebDriverExamples
 $event_firing_selenium = New-Object -Type 'OpenQA.Selenium.Support.Events.EventFiringWebDriver' -ArgumentList @( $selenium)
@@ -175,9 +83,6 @@ $event_firing_selenium.FindElement([OpenQA.Selenium.By]::CssSelector("#hlogo > a
 Start-Sleep -Millisecond 3000
 $event_firing_selenium.FindElement([OpenQA.Selenium.By]::CssSelector("#hlogo > a > b > c")).Displayed
 
-# Cleanup
-cleanup ([ref]$event_firing_selenium)
-
-
-
+custom_pause -fullstop $fullstop
+cleanup ([ref]$selenium)
 
