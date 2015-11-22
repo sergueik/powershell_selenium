6c5ca0f08c0c3a8289c7177e6705ce64824229f0
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-11-22 16:35:06 -0500

    intermediate revision - continue refactoring

--- csharp/protractor-net/Program/ClientSideScripts.cs
+++ csharp/protractor-net/Program/ClientSideScripts.cs
@@ -109,68 +109,49 @@ TestForAngular(attempts);";
          *
          * arguments[0] {Element} The scope of the search.
          * arguments[1] {string} The binding, e.g. {{cat.name}}.
-         *
-         * @return {Array.WebElement} The elements containing the binding.
-         */
-        public const string FindBindings = @"
-var using = arguments[0] || document;
-var binding = arguments[1];
-var bindings = using.getElementsByClassName('ng-binding');
-var matches = [];
-for (var i = 0; i < bindings.length; ++i) {
-    var bindingName = angular.element(bindings[i]).data().$binding[0].exp ||
-        angular.element(bindings[i]).data().$binding;
-    if (bindingName.indexOf(binding) != -1) {
-        matches.push(bindings[i]);
-    }
-}
-return matches;";
-
-        /**
-         * Find a list of elements in the page by their angular binding.
-         *
-         * arguments[0] {string} The binding, e.g. {{cat.name}}.
-         * arguments[1] {boolean} Whether the binding needs to be matched exactly.
-         * arguments[2] {Element} The scope of the search.
+         * arguments[2] {boolean} Whether the binding needs to be matched exactly.
          * arguments[3] {string} The selector to use for the root app element.
          *
          * @return {Array.WebElement} The elements containing the binding.
          */
 
-        public const string FindBindings_untested = @"
-
-var binding = arguments[0];
-var exact =  arguments[1] || false;
-var using = arguments[2] || document;
-var rootSelector  = arguments[3] || 'body';
-var root = document.querySelector(rootSelector);
-
-if (angular.getTestability) {
-    return angular.getTestability(root).
-    findBindings(using, binding, exactMatch);
-}
-var bindings = using.getElementsByClassName('ng-binding');
-var matches = [];
-for (var i = 0; i < bindings.length; ++i) {
-    var dataBinding = angular.element(bindings[i]).data('$binding');
-    if (dataBinding) {
-        var bindingName = dataBinding.exp || dataBinding[0].exp || dataBinding;
-        if (exactMatch) {
-            var matcher = new RegExp('({|\\s|^|\\|)' +
-                /* See http://stackoverflow.com/q/3561711 */
-                binding.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&') +
-                '(}|\\s|$|\\|)');
-            if (matcher.test(bindingName)) {
-                matches.push(bindings[i]);
-            }
-        } else {
-            if (bindingName.indexOf(binding) != -1) {
-                matches.push(bindings[i]);
+        public const string FindBindings = @"
+var findBindings = function(binding, exactMatch, using, rootSelector) {
+    var root = document.querySelector(rootSelector || 'body');
+    using = using || document;
+    if (angular.getTestability) {
+        return angular.getTestability(root).
+        findBindings(using, binding, exactMatch);
+    }
+    var bindings = using.getElementsByClassName('ng-binding');
+    var matches = [];
+    for (var i = 0; i < bindings.length; ++i) {
+        var dataBinding = angular.element(bindings[i]).data('$binding');
+        if (dataBinding) {
+            var bindingName = dataBinding.exp || dataBinding[0].exp || dataBinding;
+            if (exactMatch) {
+                var matcher = new RegExp('({|\\s|^|\\|)' +
+                    /* See http://stackoverflow.com/q/3561711 */
+                    binding.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&') +
+                    '(}|\\s|$|\\|)');
+                if (matcher.test(bindingName)) {
+                    matches.push(bindings[i]);
+                }
+            } else {
+                if (bindingName.indexOf(binding) != -1) {
+                    matches.push(bindings[i]);
+                }
             }
         }
     }
-}
-return matches; /* Return the whole array for webdriver.findElements. */
+    return matches; /* Return the whole array for webdriver.findElements. */
+};
+
+var using = arguments[0] || document;
+var binding = arguments[1];
+var exactMatch = arguments[2] || false;
+var rootSelector = arguments[3] || 'body';
+return findBindings(binding, exactMatch, using, rootSelector);
 ";
 
 
