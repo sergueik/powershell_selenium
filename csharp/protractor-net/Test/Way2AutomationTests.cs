6d640558bb49146de9ebe18afa39c9627d5dce97
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-12-15 20:03:37 -0500

    intermediate revision - added "Delete Customer"

--- csharp/protractor-net/Test/Way2AutomationTests.cs
+++ csharp/protractor-net/Test/Way2AutomationTests.cs
@@ -24,8 +24,8 @@ namespace Protractor.Test
         private NgWebDriver ngDriver;
         private WebDriverWait wait;
         private int wait_seconds = 3;
+        private int highlight_timeout = 100;
         private Actions actions;
-        private String login_url = "http://way2automation.com/way2auto_jquery/index.php";
         private String base_url = "http://www.way2automation.com/angularjs-protractor/banking";
 
         [TestFixtureSetUp]
@@ -42,12 +42,15 @@ namespace Protractor.Test
         [SetUp]
         public void NavigateToBankingExamplePage()
         {
-        	driver.Navigate().GoToUrl(base_url);
+            driver.Navigate().GoToUrl(base_url);
             ngDriver.Url = driver.Url;
         }
-        
+
         [Test]
-        public void ShouldLogintToWay2AutomationSite(){
+        public void ShouldLogintToWay2AutomationSite()
+        {
+
+            String login_url = "http://way2automation.com/way2auto_jquery/index.php";
             const string signup_css_selector = "div#load_box.popupbox form#load_form a.fancybox[href='#login']";
             const string login_username_css_selector = "div#login.popupbox form#load_form input[name='username']";
             const string login_password_css_selector = "div#login.popupbox form#load_form input[type='password'][name='password']";
@@ -195,12 +198,65 @@ namespace Protractor.Test
         }
 
         [Test]
-        public void ShouldShowCustomersAccounts()
+        public void ShouldDeleteCustomer()
         {
-            string cust_repeater = "cust in Customers";
+            // switch to "Add Customer" screen
+            ngDriver.FindElement(NgBy.ButtonText("Bank Manager Login")).Click();
+            ngDriver.FindElement(NgBy.PartialButtonText("Add Customer")).Click();
+            // fill new Customer data 
+            ngDriver.FindElement(NgBy.Model("fName")).SendKeys("John");
+            ngDriver.FindElement(NgBy.Model("lName")).SendKeys("Doe");
+            ngDriver.FindElement(NgBy.Model("postCd")).SendKeys("11011");
+            // NOTE: there are two 'Add Customer' buttons on this form
+            NgWebElement ng_add_dustomer_button_element = ngDriver.FindElements(NgBy.PartialButtonText("Add Customer"))[1];
+            actions.MoveToElement(ng_add_dustomer_button_element.WrappedElement).Build().Perform();
+            highlight(ng_add_dustomer_button_element.WrappedElement);
+            ng_add_dustomer_button_element.WrappedElement.Submit();
+            // confirm
+            ngDriver.WrappedDriver.SwitchTo().Alert().Accept();
+            // switch to "Home" screen
+
+            ngDriver.FindElement(NgBy.ButtonText("Home")).Click();
+            // find customers
+
             ngDriver.FindElement(NgBy.ButtonText("Bank Manager Login")).Click();
             ngDriver.FindElement(NgBy.PartialButtonText("Customers")).Click();
-            ReadOnlyCollection<NgWebElement> ng_accounts = ngDriver.FindElements(NgBy.Repeater(cust_repeater));
+            ReadOnlyCollection<NgWebElement> ng_customers = ngDriver.FindElements(NgBy.Repeater("cust in Customers"));
+
+            int cnt = 0;
+            var ng_customers_enumerator = ng_customers.GetEnumerator();
+            var status = false;
+            ng_customers_enumerator.Reset();
+            while (ng_customers_enumerator.MoveNext())
+            {
+                NgWebElement ng_customer = (NgWebElement)ng_customers_enumerator.Current;
+                actions.MoveToElement(ng_customer.WrappedElement);
+                highlight(ng_customer.WrappedElement);
+                if (Regex.IsMatch(ng_customer.Text, "John Doe.*"))
+                {
+                    status = true;
+                    break;
+                }
+                cnt++;
+            }
+            Assert.IsTrue(status);
+            if (status)
+            {
+                Assert.IsTrue(ng_customers[cnt].Displayed);
+                NgWebElement ng_delete_customer_button_element = ng_customers[cnt].FindElement(NgBy.ButtonText("Delete"));
+                StringAssert.IsMatch("Delete", ng_delete_customer_button_element.Text);
+                actions.MoveToElement(ng_delete_customer_button_element.WrappedElement).Build().Perform();
+                ng_delete_customer_button_element.Click();
+            }
+        }
+
+        [Test]
+        public void ShouldShowCustomersAccounts()
+        {
+            
+            ngDriver.FindElement(NgBy.ButtonText("Bank Manager Login")).Click();
+            ngDriver.FindElement(NgBy.PartialButtonText("Customers")).Click();
+            ReadOnlyCollection<NgWebElement> ng_accounts = ngDriver.FindElements(NgBy.Repeater("cust in Customers"));
             Assert.IsTrue(ng_accounts[0].Displayed);
             StringAssert.Contains("Granger", ng_accounts[0].Text);
         }
@@ -208,6 +264,8 @@ namespace Protractor.Test
         public void highlight(IWebElement element, int px = 3, string color = "yellow")
         {
             ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].style.border='" + px + "px solid " + color + "'", element);
+            Thread.Sleep(highlight_timeout);
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].style.border=''", element);
         }
     }
 }
