e861216200b82db8124d2020aeb4524eb187ac54
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-01-30 11:45:16 -0500

    intermediate revision - re-added LocalFileTests

--- csharp/protractor-net/Test/LocalFileTests.cs
+++ csharp/protractor-net/Test/LocalFileTests.cs
@@ -13,18 +13,19 @@ namespace Protractor.Test
 {
 
     [TestFixture]
-    public class NgNavTests
+    public class LocalFileTests
     {
         private StringBuilder verificationErrors = new StringBuilder();
         private IWebDriver driver;
         private NgWebDriver ngDriver;
-        private String base_url ;
+        
         private String testpage;
         
         [TestFixtureSetUp]
         public void SetUp()
         {
             driver = new PhantomJSDriver();
+            // .net does not seem to allow running local files in the desktop browser
             // driver = new FirefoxDriver();
             driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(60));
             ngDriver = new NgWebDriver(driver);
@@ -40,13 +41,20 @@ namespace Protractor.Test
             catch (Exception) { } /* Ignore cleanup errors */
             Assert.IsEmpty(verificationErrors.ToString());
         }
+        private void GetPageContent(string testpage ){
+            String base_url = new System.Uri(Path.Combine( Directory.GetCurrentDirectory(), testpage)).AbsoluteUri;
+            ngDriver.Navigate().GoToUrl(base_url );
 
+        }
+        [Test]
+        public void ShouldEvaluate()
+        {
+       testpage = "ng_service_example.htm";
+        }
         [Test]
         public void ShouldFindRows()
         {
-        	testpage = "ng_repeat_start_and_ng_repeat_end_example.htm";
-            base_url = new System.Uri(Path.Combine( Directory.GetCurrentDirectory(), testpage)).AbsoluteUri;
-            ngDriver.Navigate().GoToUrl(base_url);
+            GetPageContent("ng_repeat_start_and_ng_repeat_end_example.htm" );
             ReadOnlyCollection<NgWebElement> elements = ngDriver.FindElements(NgBy.Repeater("definition in definitions"));
             Assert.IsTrue(elements[0].Displayed);
             StringAssert.AreEqualIgnoringCase(elements[0].Text, "Foo");
@@ -55,9 +63,7 @@ namespace Protractor.Test
         [Test]
         public void ShouldFindCells()
         {
-        	testpage = "ng_repeat_start_and_ng_repeat_end_example.htm";
-            base_url = new System.Uri(Path.Combine( Directory.GetCurrentDirectory(), testpage)).AbsoluteUri;
-            ngDriver.Navigate().GoToUrl(base_url);
+        	GetPageContent("ng_repeat_start_and_ng_repeat_end_example.htm");
             ReadOnlyCollection<NgWebElement> elements = ngDriver.FindElements(NgBy.RepeaterColumn("definition in definitions", "definition.text"));
             Assert.AreEqual(elements.Count, 2);
             StringAssert.IsMatch("Lorem ipsum", elements[0].Text);
@@ -66,9 +72,7 @@ namespace Protractor.Test
         [Test]
         public void ShouldFindTokens()
         {
-        	testpage = "ng_table1.html";
-            base_url = new System.Uri(Path.Combine( Directory.GetCurrentDirectory(), testpage)).AbsoluteUri;
-            ngDriver.Navigate().GoToUrl(base_url);
+        	GetPageContent("ng_table1.html");
             ReadOnlyCollection<NgWebElement> elements = ngDriver.FindElements(NgBy.RepeaterColumn("x in names", "Country"));
             Assert.AreNotEqual(0, elements.Count);
             StringAssert.IsMatch("Germany", elements[0].Text);
@@ -79,9 +83,7 @@ namespace Protractor.Test
         public void ShouldFindOptions()
         {
             // base_url = "http://www.java2s.com/Tutorials/AngularJSDemo/n/ng_options_with_object_example.htm";
-        	testpage = "ng_options_with_object_example.htm";
-            base_url = new System.Uri(Path.Combine( Directory.GetCurrentDirectory(), testpage)).AbsoluteUri;
-            ngDriver.Navigate().GoToUrl(base_url);
+            GetPageContent("ng_options_with_object_example.htm");
             ReadOnlyCollection<NgWebElement> elements = ngDriver.FindElements(NgBy.Options("c.name for c in colors"));
             Assert.AreEqual(5, elements.Count);
             StringAssert.IsMatch("black", elements[0].Text);
