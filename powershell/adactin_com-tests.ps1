ae518b2ac4c0a09b49a9e2257e5380badab69c35
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-03-16 18:45:01 -0400

    initial snapshot

---
+++ powershell/adactin_com-tests.ps1
@@ -0,0 +1,5 @@
+# origin: https://www.linkedin.com/groups/961927/961927-6114989515363934212
+# dummy data website (with simple functionality) that could be used for testing purposes
+# http://www.adactin.com/HotelAppBuild2/Register.php
+# harrypotter
+# password01!
