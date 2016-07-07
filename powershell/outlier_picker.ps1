73397918f3d4930b98caac160b7de8321f78f6c7
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-07-06 21:16:50 -0400

    intermediate revision -  getting longer functions

--- powershell/outlier_picker.ps1
+++ powershell/outlier_picker.ps1
@@ -65,63 +65,64 @@ $column_css_selector = 'td:nth-child(3)'
 
 $script = @"
 
-  var table_selector = 'html body div table.sortable';
-  var row_selector = 'tbody tr';
-  var column_selector = 'td:nth-child(3)';
-  // var table_selector = 'html body div table.sortable';
-  // var row_selector = 'tbody tr';
-  // var column_selector = 'td:nth-child(3)';
-  col_num = 0;
-  var tables = document.querySelectorAll(table_selector);
-  var git_hashes = {};
-  for (table_cnt = 0; table_cnt != tables.length; table_cnt++) {
-	  var table = tables[table_cnt];
-	  if (table instanceof Element) {
-		  var rows = table.querySelectorAll(row_selector);
-		  // skip first row
-		  for (row_cnt = 1; row_cnt != rows.length; row_cnt++) {
-			  var row = rows[row_cnt];
-			  if (row instanceof Element) {
-				  var cols = row.querySelectorAll(column_selector);
-				  if (cols.length > 0) {
-					  data = cols[0].innerHTML
-					  if (!git_hashes[data]) {
-						  git_hashes[data] = 0;
-					  }
-					  git_hashes[data]++;
-				  }
-			  }
-		  }
-	  }
-  }
-  var sortNumber = function(a, b) {
+	var table_selector = 'html body div table.sortable';
+	var row_selector = 'tbody tr';
+	var column_selector = 'td:nth-child(3)';
+	// var table_selector = 'html body div table.sortable';
+	// var row_selector = 'tbody tr';
+	// var column_selector = 'td:nth-child(3)';
+	col_num = 0;
+	var tables = document.querySelectorAll(table_selector);
+	var git_hashes = {};
+	for (table_cnt = 0; table_cnt != tables.length; table_cnt++) {
+		var table = tables[table_cnt];
+		if (table instanceof Element) {
+			var rows = table.querySelectorAll(row_selector);
+			// skip first row
+			for (row_cnt = 1; row_cnt != rows.length; row_cnt++) {
+				var row = rows[row_cnt];
+				if (row instanceof Element) {
+					var cols = row.querySelectorAll(column_selector);
+					if (cols.length > 0) {
+						data = cols[0].innerHTML;
+						data = data.replace(/\s+/g, '');
+						if (!git_hashes[data]) {
+							git_hashes[data] = 0;
+						}
+						git_hashes[data]++;
+					}
+				}
+			}
+		}
+	}
+	var sortNumber = function(a, b) {
 	// reverse numeric sort
-	  return b - a;
-  }
-  var removeFrequentKey = function(datahash) {
-	  var array_keys = [];
-	  var array_values = [];
-	  for (var key in datahash) {
-		  array_keys.push(key);
-		  array_values.push(0 + datahash[key]);
-	  }
-	  max_freq = array_values.sort(sortNumber)[0]
-	  for (var key in datahash) {
-		  if (datahash[key] === max_freq) {
-			  delete datahash[key]
-		  }
-	  }
-	  return datahash;
-  }
-  
-  git_hashes = removeFrequentKey(git_hashes);
-  
-  var array_keys = [];
-  for (var key in git_hashes) {
-	  array_keys.push(key);
-  }
-  
-  return array_keys.join();
+		return b - a;
+	}
+	var removeFrequentKey = function(datahash) {
+		var array_keys = [];
+		var array_values = [];
+		for (var key in datahash) {
+			array_keys.push(key);
+			array_values.push(0 + datahash[key]);
+		}
+		max_freq = array_values.sort(sortNumber)[0]
+		for (var key in datahash) {
+			if (datahash[key] === max_freq) {
+				delete datahash[key]
+			}
+		}
+		return datahash;
+	}
+	
+	git_hashes = removeFrequentKey(git_hashes);
+	
+	var array_keys = [];
+	for (var key in git_hashes) {
+		array_keys.push(key);
+	}
+	
+	return array_keys.join();
 "@
 
 $result = ([OpenQA.Selenium.IJavaScriptExecutor]$selenium).executeScript($script)
@@ -133,13 +134,15 @@ var result = [];
 var table_selector = 'html body div table.sortable';
 var row_selector = 'tbody tr';
 var hash_column_selector = 'td:nth-child(3)';
-var master_server_column_selector = 'td:nth-child(0)';
-var git_hashes_str = arguments[0];
+var master_server_column_selector = 'td:nth-child(1)';
+// var git_hashes_str = arguments[0];
+var git_hashes_str = '259c762,25bad25,2bad762,b26e5f1,bade5f1,d1bad8d,d158d8d,533acf2,533ace2,1b24bca,1b24bc2,d3c1652,d3aaa52,7538e12,7000e12';
+
 var col_num = 0;
 var git_hashes = {};
 var git_hashes_keys = git_hashes_str.split(',');
 for (var key in git_hashes_keys) {
-  git_hashes[git_hashes_keys[key]] = 1;
+	git_hashes[git_hashes_keys[key]] = 1;
 }
 var tables = document.querySelectorAll(table_selector);
 
@@ -152,19 +155,23 @@ for (table_cnt = 0; table_cnt != tables.length; table_cnt++) {
 		for (row_cnt = 1; row_cnt != rows.length; row_cnt++) {
 			var row = rows[row_cnt];
 			if (row instanceof Element) {
-				var cols = row.querySelectorAll(hash_column_selector);
-				if (cols.length > 0) {
-					data = cols[0].innerHTML;
-					data = data.replace(/\s+/g,'');
+				var hash_cols = row.querySelectorAll(hash_column_selector);
+				if (hash_cols.length > 0) {
+					data = hash_cols[0].innerHTML;
+					data = data.replace(/\s+/g, '');
 					if (git_hashes[data]) {
-					  result.push(data);
+						var master_server_cols = row.querySelectorAll(master_server_column_selector);
+						if (master_server_cols.length > 0) {
+							data = master_server_cols[0].innerHTML;
+							data = data.replace(/\s+/g, '');
+							result.push(data);
+						}
 					}
 				}
 			}
 		}
 	}
 }
-
 return result.join();
 "@
 $result = ([OpenQA.Selenium.IJavaScriptExecutor]$selenium).executeScript($script, $result)
