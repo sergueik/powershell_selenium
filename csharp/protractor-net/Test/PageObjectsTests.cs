d7ae35c0ec901ecfe6030fe1fb103e09b1318098
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-11-03 16:39:29 -0400

    intermediate revision - merged the upstream changes

--- csharp/protractor-net/Test/PageObjectsTests.cs
+++ csharp/protractor-net/Test/PageObjectsTests.cs
@@ -10,6 +10,7 @@ using OpenQA.Selenium.Support.PageObjects;
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.PhantomJS;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
 using Protractor.Extensions;
 
@@ -20,14 +21,14 @@ namespace Protractor.Test
     public class PageObjectsTests
     {
         private IWebDriver driver;
-        private String base_url = "http://angular.github.io/angular-phonecat/step-5/app/";
+        private String base_url = "http://angular.github.io/angular-phonecat/step-6/app/";
 
         [SetUp]
         public void SetUp()
         {
             // Using NuGet Package 'PhantomJS'
             // driver = new PhantomJSDriver();
-            driver = new ChromeDriver();
+            driver = new FirefoxDriver();
             driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(5));
         }
 
@@ -40,38 +41,39 @@ namespace Protractor.Test
         [Test(Description = "Should filter the phone list as user types into the search box")]
         public void ShouldFilter()
         {
-            var step5Page = new TutorialStep5Page(driver, base_url);
+            var step6Page = new TutorialStep6Page(driver, base_url);
 
-            Assert.AreEqual(3, step5Page.GetResultsCount());
+            Assert.AreEqual(3, step6Page.GetResultsCount());
 
-            step5Page.SearchFor("Motorola");
-            Assert.AreEqual(2, step5Page.GetResultsCount());
+            step6Page.SearchFor("Motorola");
+            Assert.AreEqual(2, step6Page.GetResultsCount());
 
-            step5Page.SearchFor("Nexus");
-            Assert.AreEqual(1, step5Page.GetResultsCount());
+            step6Page.SearchFor("Nexus");
+            Assert.AreEqual(1, step6Page.GetResultsCount());
         }
 
         [Test(Description = "Should be possible to control phone order via the drop down select box")]
         public void ShouldSort()
         {
-            var step5Page = new TutorialStep5Page(driver, "http://angular.github.io/angular-phonecat/step-5/app/");
-            step5Page.SearchFor("tablet");
-            Assert.AreEqual(2, step5Page.GetResultsCount());
+            var step6Page = new TutorialStep6Page(driver, 
+        	                                      "http://angular.github.io/angular-phonecat/step-6/app/");
+            step6Page.SearchFor("tablet");
+            Assert.AreEqual(2, step6Page.GetResultsCount());
 
-            step5Page.SortByAge();
-            Assert.AreEqual("Motorola XOOM™ with Wi-Fi", step5Page.GetResultsPhoneName(0));
-            Assert.AreEqual("MOTOROLA XOOM™", step5Page.GetResultsPhoneName(1));
+            step6Page.SortByAge();
+            Assert.AreEqual("Motorola XOOM™ with Wi-Fi", step6Page.GetResultsPhoneName(0));
+            Assert.AreEqual("MOTOROLA XOOM™", step6Page.GetResultsPhoneName(1));
 
-            step5Page.SortByName();
-            Assert.AreEqual("MOTOROLA XOOM™", step5Page.GetResultsPhoneName(0));
-            Assert.AreEqual("Motorola XOOM™ with Wi-Fi", step5Page.GetResultsPhoneName(1));
+            step6Page.SortByName();
+            Assert.AreEqual("MOTOROLA XOOM™", step6Page.GetResultsPhoneName(0));
+            Assert.AreEqual("Motorola XOOM™ with Wi-Fi", step6Page.GetResultsPhoneName(1));
         }
-    }
+	    }
     /*
-   * Page Object that represents the the AngularJS tutorial Step 5 page: 
-   * http://docs.angularjs.org/tutorial/step_05
+   * Page Object that represents the the AngularJS tutorial Step 6 page: 
+   * http://docs.angularjs.org/tutorial/step_06
    */
-    public class TutorialStep5Page
+    public class TutorialStep6Page
     {
         NgWebDriver ngDriver;
         [FindsBy(How = How.Custom, CustomFinderType = typeof(NgByModel), Using = "$ctrl.query")]
@@ -81,27 +83,27 @@ namespace Protractor.Test
 
         [FindsBy(How = How.Custom, CustomFinderType = typeof(NgByModel), Using = "orderProp")]
         public IWebElement SortBySelect { get; set; }
-        public TutorialStep5Page(IWebDriver driver, string url)
+        public TutorialStep6Page(IWebDriver driver, string url)
         {
             ngDriver = new NgWebDriver(driver);
             PageFactory.InitElements(ngDriver, this);
             ngDriver.Navigate().GoToUrl(url);
         }
 
-        public TutorialStep5Page SearchFor(string query)
+        public TutorialStep6Page SearchFor(string query)
         {
             QueryInput.Clear();
             QueryInput.SendKeys(query);
             return this;
         }
 
-        public TutorialStep5Page SortByName()
+        public TutorialStep6Page SortByName()
         {
             SortBySelect.FindElement(By.XPath("//option[@value='name']")).Click();
             return this;
         }
 
-        public TutorialStep5Page SortByAge()
+        public TutorialStep6Page SortByAge()
         {
             SortBySelect.FindElement(By.XPath("//option[@value='age']")).Click();
             return this;
