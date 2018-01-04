f140291d5ae825bb4cee3f81db5be949e6264222
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-01-03 20:36:36 -0500

    intermediate revision  - updated packages to let project compile

--- external/csharp/NUnit_Demo99/NUnit.Demo99/config/ActionKeywords.cs
+++ external/csharp/NUnit_Demo99/NUnit.Demo99/config/ActionKeywords.cs
@@ -12,211 +12,209 @@ using System.Threading;
 using System.Runtime.InteropServices;
 using NUnit.Demo99.utility;
 using RelevantCodes.ExtentReports;
+using System.Reflection;
 using NUnit.Demo99.executionengine;
 
 namespace NUnit.Demo99.config
 {
-    public class ActionKeywords
-    {
-        //ExcelUtils utils;
-        public static IWebDriver driver;
-        // This block of code will decide which browser type to open
-        public static void openBrowser(String obj, String data)
-        {
-            try
-            {
-                Log.getTest();
-                Log._test.Log(LogStatus.Info, "Openning Browser");
-                if (data == "Mozilla")
-                {
-                    driver = new FirefoxDriver();
-                    Log._test.Log(LogStatus.Info, "Mozilla browser started");
-                }
-                else if (data == "IE")
-                {
-                    driver = new InternetExplorerDriver();
-                    Log._test.Log(LogStatus.Info, "IE browser started");
-                }
-                else if (data == "Chrome")
-                {
-                    driver = new ChromeDriver();
-                    Log._test.Log(LogStatus.Info, "Chrome browser started");
-                }
-            }
-            catch (Exception e)
-            {
-                Log._test.Log(LogStatus.Info, "Failed to open Browser --- " + e.Message);
-                DriverScript.bResult = false;
-            }
-        }
+	class Methods
+	{
+	
+		private int num1 = 0;
+		private int num2 = 0;
 
-        public static void navigate(String obj, String data)
-        {
-            try
-            {
-                Log.getTest();
-                Log._test.Log(LogStatus.Info, "Navigating to URL: " + Constants.URL);
-                driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(15));
-                driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(15));
-                driver.Navigate().GoToUrl(Constants.URL);
-            }
-            catch (Exception e)
-            {
-                Log._test.Log(LogStatus.Info, "Failed to navigate --- " + e.Message);
-                DriverScript.bResult = false;
-            }
-        }
+		public int AddNumb(Dictionary<String,String> param)
+		{
+			String data;
+			try {
+				param.TryGetValue("1", out data);
+				num1 = Int32.Parse(data);
+				param.TryGetValue("2", out data);
+				num2 = Int32.Parse(data);
 
-        public static void click(String obj, String data)
-        {
-            try
-            {
-                Log.getTest();
-                Log._test.Log(LogStatus.Info, "Clicking on web element: " + obj);
+    	
+			} catch (Exception e) {
+				Console.Error.WriteLine("Exception :" + e);
+				// ignore
+			}
+			int res = num1 + num2;
+			return res;
+		}
+	}
+	public class ActionKeywords
+	{
+		//ExcelUtils utils;
+		public static IWebDriver driver;
+		// This block of code will decide which browser type to open
+		public static void openBrowser(String obj, String data)
+		{
+			
+			Type methodType = typeof(Methods); 
+			//Create an instance of the type
+			object methodObj = Activator.CreateInstance(methodType);
+			Dictionary<String,String> methodParam = new Dictionary<String,String>();
+			methodParam.Add("1", "1");
+			methodParam.Add("2", "2");
+			int res = (int)methodType.InvokeMember("AddNumb", BindingFlags.InvokeMethod,
+				          null, methodObj, new Object[]{ (Object)methodParam });
+			Console.Write("Result: {0} \n", res);
+			try {
+				Log.getTest();
+				Log._test.Log(LogStatus.Info, "Openning Browser");
+				if (data == "Mozilla") {
+					driver = new FirefoxDriver();
+					Log._test.Log(LogStatus.Info, "Mozilla browser started");
+				} else if (data == "IE") {
+					driver = new InternetExplorerDriver();
+					Log._test.Log(LogStatus.Info, "IE browser started");
+				} else if (data == "Chrome") {
+					driver = new ChromeDriver();
+					Log._test.Log(LogStatus.Info, "Chrome browser started");
+				}
+			} catch (Exception e) {
+				Log._test.Log(LogStatus.Info, "Failed to open Browser --- " + e.Message);
+				DriverScript.bResult = false;
+			}
+		}
 
-                // This is to fetch using the css selector of the element from the object Repository property file
-                driver.FindElement(By.CssSelector(getKey(obj, ""))).Click();
-            }
-            catch (Exception e)
-            {
-                Log._test.Log(LogStatus.Info, "Unable to click --- " + e.Message);
-                DriverScript.bResult = false;
-            }
-        }
+		public static void navigate(String obj, String data)
+		{
+			try {
+				Log.getTest();
+				Log._test.Log(LogStatus.Info, "Navigating to URL: " + Constants.URL);
+				driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(15);
+				driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+				driver.Navigate().GoToUrl(Constants.URL);
+			} catch (Exception e) {
+				Log._test.Log(LogStatus.Info, "Failed to navigate --- " + e.Message);
+				DriverScript.bResult = false;
+			}
+		}
 
-        public static string getKey(String obj, String data)
-        {
-            return config.Settings.Default.Properties[obj].DefaultValue as string;
-        }
+		public static void click(String obj, String data)
+		{
+			try {
+				Log.getTest();
+				Log._test.Log(LogStatus.Info, "Clicking on web element: " + obj);
 
-        public static void input(String obj, String data)
-        {
-            try
-            {
-                Log.getTest();
-                Log._test.Log(LogStatus.Info, "Entering the text in " + obj);
-                IWebElement we = driver.FindElement(By.CssSelector(getKey(obj, "")));
-                we.Clear();
-                we.SendKeys(data);
-            }
-            catch (Exception e)
-            {
-                Log._test.Log(LogStatus.Info, "Failed to enter text in " + obj + " --- " + e.Message);
-                DriverScript.bResult = false;
-                Console.WriteLine(e.Message);
-            }
-        }
+				// This is to fetch using the css selector of the element from the object Repository property file
+				driver.FindElement(By.CssSelector(getKey(obj, ""))).Click();
+			} catch (Exception e) {
+				Log._test.Log(LogStatus.Info, "Unable to click --- " + e.Message);
+				DriverScript.bResult = false;
+			}
+		}
 
-        public static void waitFor(String obj, String data)
-        {
-            try
-            {
-                Log.getTest();
-                Log._test.Log(LogStatus.Info, "Wait for 5 seconds");
-                Thread.Sleep(5000);
-            }
-            catch (ThreadInterruptedException e)
-            {
-                Log._test.Log(LogStatus.Info, "Unable to wait --- " + e.Message);
-                DriverScript.bResult = false;
-            }
-        }
+		public static string getKey(String obj, String data)
+		{
+			return config.Settings.Default.Properties[obj].DefaultValue as string;
+		}
 
-        public static void waitUntil(String obj, String data)
-        {
-            try
-            {
-                Log.getTest();
-                Log._test.Log(LogStatus.Info, "Wait until element " + obj + " is to be clickable");
-                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-                wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector(data)));
-            }
-            catch (Exception e)
-            {
-                Log._test.Log(LogStatus.Info, "Exception occurred in waiting --- " + e.Message);
-                DriverScript.bResult = false;
-            }
-        }
+		public static void input(String obj, String data)
+		{
+			try {
+				Log.getTest();
+				Log._test.Log(LogStatus.Info, "Entering the text in " + obj);
+				IWebElement we = driver.FindElement(By.CssSelector(getKey(obj, "")));
+				we.Clear();
+				we.SendKeys(data);
+			} catch (Exception e) {
+				Log._test.Log(LogStatus.Info, "Failed to enter text in " + obj + " --- " + e.Message);
+				DriverScript.bResult = false;
+				Console.WriteLine(e.Message);
+			}
+		}
 
-        public static void checkCheckbox(String obj, String data)
-        {
-            try
-            {
-                Log.getTest();
-                Log._test.Log(LogStatus.Info, "make sure checkbox - Same as billing address - is checked");
-                IWebElement chkbx = driver.FindElement(By.CssSelector(getKey(obj, "")));
-                if (!chkbx.Selected)
-                {
-                    chkbx.Click();
-                }
-            }
-            catch (Exception e)
-            {
-                Log._test.Log(LogStatus.Info, "Failed to check same as billing checkbox --- " + e.Message);
-                DriverScript.bResult = false;
-            }
-        }
+		public static void waitFor(String obj, String data)
+		{
+			try {
+				Log.getTest();
+				Log._test.Log(LogStatus.Info, "Wait for 5 seconds");
+				Thread.Sleep(5000);
+			} catch (ThreadInterruptedException e) {
+				Log._test.Log(LogStatus.Info, "Unable to wait --- " + e.Message);
+				DriverScript.bResult = false;
+			}
+		}
 
-        public static void select(String obj, String data)
-        {
-            try
-            {
-                Log.getTest();
-                Log._test.Log(LogStatus.Info, "select a country from checkout page");
-                SelectElement select = new SelectElement(driver.FindElement(By.CssSelector(getKey(obj, ""))));
-                select.SelectByText(data);
-            }
-            catch (Exception e)
-            {
-                Log._test.Log(LogStatus.Info, "Failed to select from drop down list --- " + e.Message);
-                DriverScript.bResult = false;
-            }
-        }
+		public static void waitUntil(String obj, String data)
+		{
+			try {
+				Log.getTest();
+				Log._test.Log(LogStatus.Info, "Wait until element " + obj + " is to be clickable");
+				WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+				wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector(data)));
+			} catch (Exception e) {
+				Log._test.Log(LogStatus.Info, "Exception occurred in waiting --- " + e.Message);
+				DriverScript.bResult = false;
+			}
+		}
 
-        public static void submitForm(String obj, String data)
-        {
-            try
-            {
-                Log.getTest();
-                Log._test.Log(LogStatus.Info, "pick the first form and submit - i.e. Click Add to Cart button");
-                driver.FindElements(By.CssSelector(data))[0].Submit();
-            }
-            catch (Exception e)
-            {
-                Log._test.Log(LogStatus.Info, "Failed to add to cart --- " + e.Message);
-                DriverScript.bResult = false;
-            }
-        }
+		public static void checkCheckbox(String obj, String data)
+		{
+			try {
+				Log.getTest();
+				Log._test.Log(LogStatus.Info, "make sure checkbox - Same as billing address - is checked");
+				IWebElement chkbx = driver.FindElement(By.CssSelector(getKey(obj, "")));
+				if (!chkbx.Selected) {
+					chkbx.Click();
+				}
+			} catch (Exception e) {
+				Log._test.Log(LogStatus.Info, "Failed to check same as billing checkbox --- " + e.Message);
+				DriverScript.bResult = false;
+			}
+		}
 
-        public static void confirmOrder(String obj, String data)
-        {
-            Log.getTest();
-            Log._test.Log(LogStatus.Info, "confirm if order was placed successfully");
-            // wait for seconds so as to load confirmation page		
-            Thread.Sleep(8000);
-            // take a screenshot anyway
-            Log.TakeScreenshot(driver, Constants.Path_TestScr);
-            Log._test.Log(LogStatus.Info, "Screenshot - " + Log._test.AddScreenCapture(Constants.Path_TestScr)); // add screenshot
-            if (!driver.PageSource.Contains(data))
-            {
-                Log._test.Log(LogStatus.Info, "Failed to place the order --- ");
-                DriverScript.bResult = false;
-            }
-        }
+		public static void select(String obj, String data)
+		{
+			try {
+				Log.getTest();
+				Log._test.Log(LogStatus.Info, "select a country from checkout page");
+				SelectElement select = new SelectElement(driver.FindElement(By.CssSelector(getKey(obj, ""))));
+				select.SelectByText(data);
+			} catch (Exception e) {
+				Log._test.Log(LogStatus.Info, "Failed to select from drop down list --- " + e.Message);
+				DriverScript.bResult = false;
+			}
+		}
 
-        public static void closeBrowser(String obj, String data)
-        {
-            try
-            {
-                Log.getTest();
-                Log._test.Log(LogStatus.Info, "Closing the browser");
-                driver.Quit();
-            }
-            catch (Exception e)
-            {
-                Log._test.Log(LogStatus.Info, "Failed to close browser --- " + e.Message);
-                DriverScript.bResult = false;
-            }
-        }
-    }
+		public static void submitForm(String obj, String data)
+		{
+			try {
+				Log.getTest();
+				Log._test.Log(LogStatus.Info, "pick the first form and submit - i.e. Click Add to Cart button");
+				driver.FindElements(By.CssSelector(data))[0].Submit();
+			} catch (Exception e) {
+				Log._test.Log(LogStatus.Info, "Failed to add to cart --- " + e.Message);
+				DriverScript.bResult = false;
+			}
+		}
+
+		public static void confirmOrder(String obj, String data)
+		{
+			Log.getTest();
+			Log._test.Log(LogStatus.Info, "confirm if order was placed successfully");
+			// wait for seconds so as to load confirmation page		
+			Thread.Sleep(8000);
+			// take a screenshot anyway
+			Log.TakeScreenshot(driver, Constants.Path_TestScr);
+			Log._test.Log(LogStatus.Info, "Screenshot - " + Log._test.AddScreenCapture(Constants.Path_TestScr)); // add screenshot
+			if (!driver.PageSource.Contains(data)) {
+				Log._test.Log(LogStatus.Info, "Failed to place the order --- ");
+				DriverScript.bResult = false;
+			}
+		}
+
+		public static void closeBrowser(String obj, String data)
+		{
+			try {
+				Log.getTest();
+				Log._test.Log(LogStatus.Info, "Closing the browser");
+				driver.Quit();
+			} catch (Exception e) {
+				Log._test.Log(LogStatus.Info, "Failed to close browser --- " + e.Message);
+				DriverScript.bResult = false;
+			}
+		}
+	}
 }
--- external/csharp/NUnit_Demo99/NUnit.Demo99/utility/Log.cs
+++ external/csharp/NUnit_Demo99/NUnit.Demo99/utility/Log.cs
@@ -9,48 +9,47 @@ using System.Drawing.Imaging;
 
 namespace NUnit.Demo99.utility
 {
-    public class Log
-    {
-        public static ExtentReports _extent = new ExtentReports(config.Constants.Path_Report, DisplayOrder.NewestFirst);
-        public static ExtentTest _test;
-        public static DriverScript ds;
-        public static void getTest()
-        {
-            if (_test == null)
-            {
-                // init extent reports instance
-                _extent.LoadConfig(config.Constants.Path_Config);
-                _test = _extent.StartTest("Practice Exercise", "This is the Practice Exercise");
-            }
-        }
+	public class Log
+	{
+		// TODO: find the proper version of the ExtentReports package
+		// public static ExtentReports _extent = new ExtentReports(config.Constants.Path_Report, DisplayOrder.NewestFirst);
+		public static ExtentReports _extent = new ExtentReports(config.Constants.Path_Report, false);
+		public static ExtentTest _test;
+		public static DriverScript ds;
+		public static void getTest()
+		{
+			if (_test == null) {
+				// init extent reports instance
+				// TODO:  'RelevantCodes.ExtentReports.ExtentReports' 2.0.1 does not contain a definition for 'LoadConfig'
+				// _extent.LoadConfig(config.Constants.Path_Config);
+				_test = _extent.StartTest("Practice Exercise", "This is the Practice Exercise");
+			}
+		}
 
-        // this is to print log for the beginning of the test case, as we usually run so many test cases as a test suite
-        public static void startTestCase(String sTestCaseName)
-        {
-            getTest();
-            _test.Log(LogStatus.Info, "|-------------------------");
-            _test.Log(LogStatus.Info, "Test ID: " + sTestCaseName);
-        }
+		// this is to print log for the beginning of the test case, as we usually run so many test cases as a test suite
+		public static void startTestCase(String sTestCaseName)
+		{
+			getTest();
+			_test.Log(LogStatus.Info, "|-------------------------");
+			_test.Log(LogStatus.Info, "Test ID: " + sTestCaseName);
+		}
 
-        //This is to print log for the ending of the test case
-        public static void endTestCase(String sTestCaseName)
-        {
-            _test.Log(LogStatus.Info, "-E---N---D-");
-            _test.Log(LogStatus.Info, "-------------------------|");
-        }
+		//This is to print log for the ending of the test case
+		public static void endTestCase(String sTestCaseName)
+		{
+			_test.Log(LogStatus.Info, "-E---N---D-");
+			_test.Log(LogStatus.Info, "-------------------------|");
+		}
 
-        public static void TakeScreenshot(IWebDriver driver, string fileName)
-        {
-            ITakesScreenshot screenshotDriver = driver as ITakesScreenshot;
-            if (screenshotDriver == null)
-            {
-                _test.Log(LogStatus.Fail, "Error in taking screenshot!");
-            }
-            else
-            {
-                Screenshot screenshot = screenshotDriver.GetScreenshot();
-                screenshot.SaveAsFile(fileName, System.Drawing.Imaging.ImageFormat.Png);
-            }
-        }
-    }
+		public static void TakeScreenshot(IWebDriver driver, string fileName)
+		{
+			ITakesScreenshot screenshotDriver = driver as ITakesScreenshot;
+			if (screenshotDriver == null) {
+				_test.Log(LogStatus.Fail, "Error in taking screenshot!");
+			} else {
+				Screenshot screenshot = screenshotDriver.GetScreenshot();
+				screenshot.SaveAsFile(fileName, ScreenshotImageFormat.Png);
+			}
+		}
+	}
 }
