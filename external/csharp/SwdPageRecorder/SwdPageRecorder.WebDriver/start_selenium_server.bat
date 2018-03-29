7e2b27fd49ad03377fb7b90d9d63d0c0a3d93771
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-03-29 15:07:09 -0400

    updated package dependencies, primarily Selenium, to 3.x

--- external/csharp/SwdPageRecorder/SwdPageRecorder.WebDriver/start_selenium_server.bat
+++ external/csharp/SwdPageRecorder/SwdPageRecorder.WebDriver/start_selenium_server.bat
@@ -5,6 +5,6 @@ set PHANTOM=-Dphantomjs.binary.path="%cd%\phantomjs.exe"
 REM to set a custom location of Firefox driver, update and uncomment the next line:
 REM set FIREFOX_LOCATION_ARGUMENT=-Dwebdriver.firefox.bin="C:\\Users\\vagrant\\Desktop\\Firefox\\firefox.exe"
 REM Provide the pathname of Selenium jar
-set SELENIUM_SERVER_EXE=selenium-server-standalone-2.45.0.jar
+set SELENIUM_SERVER_EXE=selenium-server-standalone-2.53.1.jar
 REM to prevent the argument list too long error, remove 'start "%SELENIUM_SERVER_EXE%"' from the command below
 start "%SELENIUM_SERVER_EXE%" java %PHANTOM% %CHROME% %IE% %FIREFOX_LOCATION_ARGUMENT% -jar %SELENIUM_SERVER_EXE%
