9f3cc09e43444c1f5ae57c70bd4e7aef9bca887a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-07-17 14:22:08 -0400

    intermediate revision , minor refactoring

--- powershell/download_edgedriver.ps1
+++ powershell/download_edgedriver.ps1
@@ -25,7 +25,7 @@
 # href : https://download.microsoft.com/download/C/0/7/C07EBF21-5305-4EC8-83B1-A6
 #        FCC8F93F45/MicrosoftWebDriver.exe
 # text : Release 10586
-# 
+#
 # href : https://download.microsoft.com/download/8/D/0/8D0D08CF-790D-4586-B726-C6
 #        469A9ED49C/MicrosoftWebDriver.exe
 # text : Release 10240
@@ -35,42 +35,7 @@ $locator = 'section#downloads ul.driver-downloads li.driver-download > a'
 # can not directly return the value
 # https://stackoverflow.com/questions/26021813/ie-com-automation-how-to-get-the-return-value-of-window-execscript-in-powersh
 # $script_template
-$scripts = @(
-@"
-    var selector = '${locator}';
-    var elements = document.querySelectorAll(selector);
-    var result = elements[0].innerHTML;
-    /*
-    var fso = new ActiveXObject('Scripting.FileSystemObject');
-    var out = fso.GetStandardStream(1); out.Write(result);
-    */
-    return(result);
-"@,
- ("var selector = '{0}';var elements = document.querySelectorAll(selector); var result = elements[0].innerHTML; document.body.setAttribute('PSResult' , result);" -f $locator),
- ("var selector = '{0}';var elements = document.querySelectorAll(selector); var result = elements[0].getAttribute('href'); document.body.setAttribute('PSResult' , result);" -f $locator),
- @"
-    var selector = '${locator}';
-    var elements = document.querySelectorAll(selector);
-    var result = elements[0].innerHTML;
-    document.body.setAttribute('PSResult' , result);
-"@,
-@"
-  var selector = '${locator}';
-  var elements = document.querySelectorAll(selector);
-  var result = elements[0].getAttribute('href');
-  document.body.setAttribute('PSResult' , result);
-"@
-@"
-  var selector = '${locator}';
-  var elements = document.querySelectorAll(selector);
-  var element = elements[0];
-  var result = {
-    'href': element.getAttribute('href'),
-    'text': element.innerHTML
-  };
-  document.body.setAttribute('PSResult' , JSON.stringify(result) );
-"@,
-@"
+$script = @"
   var selector = '${locator}';
   var elements = document.querySelectorAll(selector);
   var result = [];
@@ -84,7 +49,6 @@ $scripts = @(
   document.body.setAttribute('PSResult' , JSON.stringify(result) );
 "@
 
-)
 $ie = new-object -com 'internetexplorer.application'
 $ie.visible = $false
 $ie.navigate2($download_url)
@@ -101,52 +65,30 @@ $document = $ie.document
 $document = $ie.document
 $window = $document.parentWindow
 
-$scripts | foreach-object { $script = $_
-  write-debug ('Locating "{0}"' -f $locator)
-  write-debug ('Script : "{0}"' -f $script)
-  try {
-    $window.execScript($script, 'javascript')
-    $result = $document.body.getAttribute('PSResult')
-    write-output $result
-    $result  | convertfrom-json | format-list
-    <#
-    
-href : https://download.microsoft.com/download/C/0/7/C07EBF21-5305-4EC8-83B1-A6
-       FCC8F93F45/MicrosoftWebDriver.exe
-text : Release 10586
-
-href : https://download.microsoft.com/download/8/D/0/8D0D08CF-790D-4586-B726-C6
-       469A9ED49C/MicrosoftWebDriver.exe
-text : Release 10240
-...
-#>
-  } catch [Exception] {
-      write-output ( 'Exception : ' + $_.Exception.Message)
-      # Exception : Could not complete the operation due to error 80020101.
-      return
+write-debug ('Script : "{0}"' -f $script)
+try {
+  $window.execScript($script, 'javascript')
+  $result = $document.body.getAttribute('PSResult') | convertfrom-json
+  # add a release key
+  $indexed_downloads = @{}
+  $result | foreach-object {
+    $data = $_
+    add-member -InputObject $data -name release -value ($data.'text' -replace 'Release\s+', '') -membertype noteproperty
+    $indexed_downloads[$data.'release'] = $data.'href'
   }
+  # https://stackoverflow.com/questions/14741397/what-is-the-view-parameter-of-format-list
+  if ($DebugPreference -eq 'Continue') {
+    format-list -InputObject $result
+  }
+  format-list -InputObject $indexed_downloads
+} catch [Exception] {
+    write-output ('Exception : ' + $_.Exception.Message)
+    # NOTE: Exception : Could not complete the operation due to error 80020101.
+    # indicates a possible Javascript error, validate in Developer console
+    return
 }
 
-
-# section#downloads ul.driver-downloads li.driver-download > a
-# <li class="driver-download"><a class="subtitle" href="https://download.microsoft.com/download/3/2/D/32D3E464-F2EF-490F-841B-05D53C848D15/MicrosoftWebDriver.exe" aria-label="WebDriver for release number 14393">Release 14393</a>
-# <p class="driver-download__meta">Version: 3.14393 | Edge version supported: 14.14393 | <a href="https://az813057.vo.msecnd.net/eulas/webdriver-eula.pdf">License terms</a></p>
-
-# Quit and dispose IE COM
+# quit and dispose IE
 $ie.Quit()
 [System.Runtime.Interopservices.Marshal]::ReleaseComObject($ie) | out-null
 Remove-Variable ie
-<#
-$script_template = @"
-var selector = arguments[0];
-if (selector == undef)  {
- selector = '{0}' ;
-};
-var elements = document.querySelectorAll(selector);
-"@
-# Error formatting a string: Input string was not in a correct format..
-
-
-$script_template = (($script_template -split "`n" ) -join '' )
-
-#>
