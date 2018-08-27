ea2eb06d683780747bc38f845f189a192156a18a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-08-26 22:28:31 -0400

    intermediate revision - added idle time snippet, not bindled

--- utils/nosleep.ps1
+++ utils/nosleep.ps1
@@ -69,3 +69,54 @@ $f.Controls.AddRange(@($s))
 $f.ResumeLayout($false)
 # TODO: start minimized in he taskbar, indicate the app is running
 [void]$f.ShowDialog()
+<#
+# determine user input| idle state
+# origin: http://forum.oszone.net/thread-335867.html
+Add-Type @"
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace PInvoke.Win32 {
+
+  public static class UserInput {
+
+    [DllImport("user32.dll", SetLastError=false)]
+    private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct LASTINPUTINFO {
+      public uint cbSize;
+      public int dwTime;
+    }
+
+    public static DateTime LastInput {
+      get {
+        DateTime bootTime = DateTime.UtcNow.AddMilliseconds(-Environment.TickCount);
+        DateTime lastInput = bootTime.AddMilliseconds(LastInputTicks);
+        return lastInput;
+      }
+    }
+
+    public static TimeSpan IdleTime {
+      get {
+        return DateTime.UtcNow.Subtract(LastInput);
+      }
+    }
+
+    public static int LastInputTicks {
+      get {
+        LASTINPUTINFO lii = new LASTINPUTINFO();
+        lii.cbSize = (uint)Marshal.SizeOf(typeof(LASTINPUTINFO));
+        GetLastInputInfo(ref lii);
+        return lii.dwTime;
+      }
+    }
+  }
+}
+"@
+
+write-output ("Last input: {0}" -f ([PInvoke.Win32.UserInput]::LastInput))
+write-output ("Idle for: {0}" -f ([PInvoke.Win32.UserInput]::IdleTime))
+
+#>
