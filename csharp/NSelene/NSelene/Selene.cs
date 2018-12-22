468bebbea370438970cd3a6e2631d29b90757839
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-12-21 19:12:04 -0500

    intermediate revision - added a better "findByCssSelectorAndInnerText" ,unfinished

--- csharp/NSelene/NSelene/Selene.cs
+++ csharp/NSelene/NSelene/Selene.cs
@@ -24,6 +24,59 @@ namespace NSelene {
 
 		private static MatchCollection matches;
 		private static Regex regex;
+		private const String findByCssSelectorAndInnerText = @"
+ /**
+ * Find elements by css selector and inner Textual content. Alternative to xpath locator //*[contains(text(),'...'))
+ * Derived from: Protractor clientLibrary API
+ * https://github.com/angular/protractor/blob/master/lib/clientsidescripts.js#L686
+ * @param {string} cssSelector The css selector to send to document.querySelectorAll. Default is a 'body *'  
+ * @param {string} innerText exact text or a serialized RegExp to search for
+ *
+ * @return last element in the array of matching elements (the innermost matching element is taken to the caller).
+ */
+var findByCssSelectorAndInnerText = function(сssSelector, innerText) {
+  if (сssSelector == null || сssSelector == '') {
+    сssSelector = 'body *';
+  }
+  if (debug != null && debug) {
+    alert('cssSelector: ' +сssSelector);
+  }
+  if (debug != null && debug) {
+	alert('text: ' + innerText );
+  }
+  if (innerText.indexOf('__REGEXP__') === 0) {
+    var match = innerText.split('__REGEXP__')[1].match(/\/(.*)\/(.*)?/);
+    innerText = new RegExp(match[1], match[2] || '');	  
+  }
+
+  var elements = document.querySelectorAll(сssSelector);
+  var matches = [];
+  for (var i = 0; i < elements.length; ++i) {
+    var element = elements[i];
+    var elementText = element.textContent.replace(/\n/, ' ') || element.innerText.replace(/\n/, ' ') || element.getAttribute('placeholder') || '';
+    var elementMatches = innerText instanceof RegExp ?
+      innerText.test(elementText) :
+      elementText.indexOf(innerText) > -1;
+    if (elementMatches) {
+      matches.push(element);
+    }
+  }
+  var result = matches[matches.length - 1];
+  if (debug != null && debug) {
+	  if (result!= null ){
+			 alert('Result: ' + /* result.outerHTML */ result.textContent );
+	  } else {
+		  alert('nothing found');		  
+	  }
+  }
+  return result;
+};
+
+var debug = false;
+var cssSelector = arguments[0];
+var innerText = arguments[1];
+return findByCssSelectorAndInnerText(cssSelector, innerText);
+						";
 
 		public static void SetWebDriver(IWebDriver driver) {
 			PrivateConfiguration.SharedDriver.Value = driver;
@@ -32,7 +85,7 @@ namespace NSelene {
 		public static IWebDriver GetWebDriver() {
 			return PrivateConfiguration.SharedDriver.Value;
 		}
-
+		// need to learn to pass args.
 		public static object ExecuteScript(string script) {
 			return (GetWebDriver() as IJavaScriptExecutor).ExecuteScript(script);
 		}
@@ -52,7 +105,8 @@ namespace NSelene {
 						seleneElement = S(By.XPath(selectorValue));
 						break;
 					case "text":
-						seleneElement = S(By.XPath(String.Format("//*[contains(text(),'%s')]", selectorValue)));
+						seleneElement = ExecuteScript(findByCssSelectorAndInnerText) as SeleneElement;
+						// seleneElement = S(By.XPath(String.Format("//*[contains(text(),'%s')]", selectorValue)));
 						break;
 					default:
 						seleneElement = S(By.CssSelector(selectorValue));
@@ -93,7 +147,8 @@ namespace NSelene {
 						seleneElement = S(By.XPath(selectorValue));
 						break;
 					case "text":
-						seleneElement = S(By.XPath(String.Format("//*[contains(text(),'%s')]", selectorValue)));
+						seleneElement = ExecuteScript(findByCssSelectorAndInnerText) as SeleneElement;
+						// seleneElement = S(By.XPath(String.Format("//*[contains(text(),'%s')]", selectorValue)));
 						break;
 					default:
 						seleneElement = S(By.CssSelector(selector));
@@ -143,7 +198,9 @@ namespace NSelene {
 						seleneElement = S(By.XPath(selectorValue), driver);
 						break;
 					case "text":
-						seleneElement = S(By.XPath(String.Format("//*[contains(text(),'%s')]", selectorValue)), driver);
+						IWebElement elementToWrap = ( driver as IJavaScriptExecutor ).ExecuteScript(findByCssSelectorAndInnerText, new object[] { null, selectorValue}) as IWebElement;
+						seleneElement = new SeleneElement(elementToWrap, driver);
+						// seleneElement = S(By.XPath(String.Format("//*[contains(text(),'%s')]", selectorValue)), driver);
 						break;
 					default:
 						seleneElement = S(By.CssSelector(selectorValue), driver);
--- csharp/NSelene/Tests/SElementXPathTextCssTest.cs
+++ csharp/NSelene/Tests/SElementXPathTextCssTest.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-
+using System.Text.RegularExpressions;
 
 using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
@@ -46,32 +46,60 @@ namespace NSeleneTests
 
 		[Test]
 		public void SElementLocalizedTextSearch() {
-			String alienName = "абвгдежзийклмнопрстуфхцчшщъыьэюя";
-			Given.OpenedPageWithBody(String.Format("<h1>Hello {0}!</h1>", alienName));
-			Selene.S(With.Text(alienName)).Should(Have.Text(String.Format("Hello {0}!", alienName)));
+			String name = "абвгдежзийклмнопрстуфхцчшщъыьэюя";
+			Given.OpenedPageWithBody(String.Format("<h1>Hello {0}!</h1>", name));
+			Selene.S(With.Text(name)).Should(Have.Text(String.Format("Hello {0}!", name)));
 			IWebDriver driver = Selene.GetWebDriver();
-			IWebElement element = driver.FindElement(By.XPath(String.Format("//h1[contains(text(), '{0}')]", alienName)));
+			IWebElement element = driver.FindElement(By.XPath(String.Format("//h1[contains(text(), '{0}')]", name)));			
 			StringAssert.AreEqualIgnoringCase("h1" , element.TagName);
+			Selene.S(With.XPath(String.Format("//h1[contains(text(), '{0}')]", name))).Should(Have.Text(String.Format("Hello {0}!", name)));
 		}
 
 
 		[Test]
-		public void SYandexTextSearch() {
+		public void YandexTextSearch() {
 			String emptySearchResponse = "Задан пустой поисковый запрос";
 			Selene.GoToUrl("https://yandex.ru/search");
 			Selene.S(With.Text(emptySearchResponse)).Should(Have.Text(emptySearchResponse));
 		}
 
-[Test]
-		public void SCP1251TextSearch() {
-			String searchString = "Информация для заявителей"; // <br>  и исполнителей проектов
-			Selene.GoToUrl("http://www.rfbr.ru/rffi/ru/");
-			Selene.S(With.Text(searchString)).Should(Have.Text(searchString));
-		}
+		[Test]
+		public void MultilineTextSearch()
+		{
+			String url = "http://www.rfbr.ru/rffi/ru/";
+			String cssSelector = "div.grants > p";
+			// note one space short
+			String searchString = "Информация для заявителей и исполнителей проектов";
+			// note one space short
+			searchString = "Информация для заявителейи исполнителей проектов";
+			// The text of the element has a <br/> and a newline 
+			// making it difficult to impossible to write the matching expression
+			Selene.GoToUrl(url);
+			// NOTE: the exception is confirmed but not in a "junit" way
+			try{
+				// Selene.S(With.Text(searchString)).Should(Be.InDom);
+				Selene.S(String.Format("text={0}", searchString.Replace("\n","").Replace("\r","")), Selene.GetWebDriver()).Should(Be.InDom);
+				// Selene.S(With.Text(searchString)).Should(Have.Text(searchString));
+			} catch (TimeoutException e) {
+				Console.Error.WriteLine("Exception (ignored) " + e.ToString());
+			} catch (NoSuchElementException e) {
+				Console.Error.WriteLine("Exception (ignored) " + e.ToString());
+			}   
 
+			// Break down the element text into single line chunks, successfully find each
+			string elementText = (Selene.GetWebDriver()).FindElement(By.CssSelector(cssSelector)).Text;
+			elementText = Selene.S(With.Css(cssSelector)).Text;
+
+			foreach (String line in elementText.Split('\n')) {
+				searchString = line.Replace("\r", "");
+				Console.Error.WriteLine("Searching by inner Text fragment:" + searchString);
+				Selene.S(With.Text(searchString)).Should(Be.InDom);
+				Selene.S(With.Text(searchString)).Should(Have.Text(searchString));            
+			}
+		}
 		
 		[Test]
-		public void SElementTextSearch() {
+		public void ElementTextSearch() {
 			Given.OpenedPageWithBody("<h1>Hello World!</h1>");
 			IWebDriver webDriver = Selene.GetWebDriver();
 			Selene.S(NSelene.With.Text("Hello")).ShouldNot(Have.ExactText("Hello"));
