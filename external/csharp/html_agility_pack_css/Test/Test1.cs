975fdca4a02ce8d75cfd6bcbbbf8f7ca4f4ec173
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-01-24 16:18:30 -0500

    added some more extended css expressions test, mostt failing

--- external/csharp/html_agility_pack_css/Test/Test1.cs
+++ external/csharp/html_agility_pack_css/Test/Test1.cs
@@ -14,33 +14,71 @@ namespace HapCss.UnitTests {
 			doc.LoadHtml(Resource.GetString("Test1.html"));
 		}
 
-		
-		[TestCase("#myDiv", "myDiv")]
-		[TestCase("div[id=myDiv]", "myDiv")]
-		[TestCase("div[id^=myDiv]", "myDiv")]
-	//	[TestCase("div[id = 'myDiv']", "myDiv")]
-	//	[TestCase("div[id = myDiv]", "myDiv")]
-	// invalid token exception
-	// System.InvalidOperationException : Token inválido: = 
-	// needs  to be made whitespace tolerant
-	//	[TestCase("div[id *= 'myDiv']", "myDiv")]
-		public void IdSelectorMustReturnOnlyFirstElement(String cssSelector, String elementId) {
+		// special test, implemetnation detail 
+		// NOTE: using id here is not the best example, 
+		// see e.g. https://www.w3.org/TR/WCAG20-TECHS/H93.html
+		// about uniqueness of the id of the Web page DOM 
+		[Test]
+		public void IdSelectorMustReturnOnlyFirstElement() {
+			String elementId = "myDiv";
+			String cssSelector = String.Format("#{0}", elementId);
 			var elements = doc.QuerySelectorAll(cssSelector);
-
-			Assert.IsTrue(elements.Count >= 1);
+			Assert.IsTrue(elements.Count == 1);
 			Assert.IsTrue(elements[0].Id == elementId);
+			// identify the DOM node that was found, there are multiple
 			Assert.IsTrue(elements[0].Attributes["first"].Value == "1");
 		}
 
-		// NOTE: bad example, see e.g. https://www.w3.org/TR/WCAG20-TECHS/H93.html
-		// about uniqueness of the id of the Web page DOM 
-		[TestCase("*[id=myDiv]", "myDiv")]
 		[TestCase("div[id=myDiv]", "myDiv")]
+		[TestCase("*[id*=myDiv]", "myDiv")]
+		[TestCase("div[id^=myDiv]", "myDiv")]
+		[TestCase("body div[id=myDiv]", "myDiv")]
+		[TestCase("body > div[id=myDiv]", "myDiv")]
 		public void GetElementsByAttribute(String cssSelector, String elementId) {
 			var elements = doc.QuerySelectorAll(cssSelector);
-			Assert.IsTrue(elements.Distinct().Count() == 2 && elements.Count == 2);
+			Assert.IsTrue(elements.Distinct().Count() == 2);
+			Assert.IsTrue(elements.Count == 2);
 			for (int i = 0; i < elements.Count; i++)
 				Assert.IsTrue(elements[i].Id == elementId);
+		}
+
+		// NOTE: driver still has to be made whitespace tolerant
+		// https://developer.mozilla.org/en-US/docs/Web/CSS/Attribute_selectors
+		// System.InvalidOperationException : Token inválido: = 
+		[TestCase("div[id = 'myDiv']", "myDiv")]
+		[TestCase("div[id = myDiv]", "myDiv")]
+		[ExpectedException( typeof( System.InvalidOperationException ) )]
+		public void GetElementsByConditionCss(String cssSelector, String elementId) {
+			var elements = doc.QuerySelectorAll(cssSelector);
+			Assert.IsTrue(elements.Distinct().Count() == 2);
+			Assert.IsTrue(elements.Count == 2);
+			for (int i = 0; i < elements.Count; i++)
+				Assert.IsTrue(elements[i].Id == elementId);
+		}
+
+
+		// https://developer.mozilla.org/en-US/docs/Web/CSS/:nth-of-type
+		// System.NotSupportedException : Pseudo classe não suportada: nth-of-type
+		[ExpectedException( typeof( System.NotSupportedException ) )]
+		[Test]
+		public void GetElementsByPseudoClass1Css( ) {
+		String cssSelector = "div:nth-of-type(1)";
+		String elementId = "myDiv";
+			var elements = doc.QuerySelectorAll(cssSelector);
+			Assert.IsTrue(elements.Distinct().Count() == 1);
+			Assert.IsTrue(elements.Count == 1);
+			Assert.IsTrue(elements[0].Id == elementId);
+		}
+
+		// https://developer.mozilla.org/en-US/docs/Web/CSS/:nth-child
+		[Test]
+		public void GetElementsByPseudoClass2Css() {
+			String cssSelector = "body:nth-child(1)";
+			String elementId = "myDiv";
+			var elements = doc.QuerySelectorAll(cssSelector);
+			Assert.IsTrue(elements.Distinct().Count() == 1);
+			Assert.IsTrue(elements.Count == 1);
+			Assert.IsTrue(elements[0].Id == elementId);
 		}
 
 		[Test]
