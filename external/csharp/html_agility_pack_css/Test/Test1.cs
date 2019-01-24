4b82eb8214d16ac17af72494124aaa483db35eeb
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-01-24 12:03:55 -0500

    intermeidate revision -  actually switching to nunit

--- external/csharp/html_agility_pack_css/Test/Test1.cs
+++ external/csharp/html_agility_pack_css/Test/Test1.cs
@@ -1,73 +1,70 @@
 ﻿using System;
 using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 
-namespace HapCss.UnitTests
-{
-    [TestClass]
-    public class Html1
-    {
-        static HtmlAgilityPack.HtmlDocument doc = LoadHtml();
+namespace HapCss.UnitTests {
+	[TestFixture]
+	public class Html1
+	{
+		static HtmlAgilityPack.HtmlDocument doc = null;
+		
+		[TestFixtureSetUp]
+		public void SetUp() {
+			doc = LoadHtml();
+		}
 
-        [TestMethod]
-        public void IdSelectorMustReturnOnlyFirstElement()
-        {
-            var elements = doc.QuerySelectorAll("#myDiv");
+		[Test]
+		public void IdSelectorMustReturnOnlyFirstElement() {
+			var elements = doc.QuerySelectorAll("#myDiv");
 
-            Assert.IsTrue(elements.Count == 1);
-            Assert.IsTrue(elements[0].Id == "myDiv");
-            Assert.IsTrue(elements[0].Attributes["first"].Value == "1");
-        }
+			Assert.IsTrue(elements.Count == 1);
+			Assert.IsTrue(elements[0].Id == "myDiv");
+			Assert.IsTrue(elements[0].Attributes["first"].Value == "1");
+		}
 
-        [TestMethod]
-        public void GetElementsByAttribute()
-        {
-            var elements = doc.QuerySelectorAll("*[id=myDiv]");
+		[Test]
+		public void GetElementsByAttribute() {
+			var elements = doc.QuerySelectorAll("*[id=myDiv]");
 
-            Assert.IsTrue(elements.Distinct().Count() == 2 && elements.Count == 2);
-            for (int i = 0; i < elements.Count; i++)
-                Assert.IsTrue(elements[i].Id == "myDiv");
-        }
+			Assert.IsTrue(elements.Distinct().Count() == 2 && elements.Count == 2);
+			for (int i = 0; i < elements.Count; i++)
+				Assert.IsTrue(elements[i].Id == "myDiv");
+		}
 
-        [TestMethod]
-        public void GetElementsByClassName1()
-        {
-            var elements1 = doc.QuerySelectorAll(".cls-a");
-            var elements2 = doc.QuerySelectorAll(".clsb");
+		[Test]
+		public void GetElementsByClassName1() {
+			var elements1 = doc.QuerySelectorAll(".cls-a");
+			var elements2 = doc.QuerySelectorAll(".clsb");
 
-            Assert.IsTrue(elements1.Count == 1);
-            for (int i = 0; i < elements1.Count; i++)
-                Assert.IsTrue(elements1[i] == elements2[i]);
-        }
+			Assert.IsTrue(elements1.Count == 1);
+			for (int i = 0; i < elements1.Count; i++)
+				Assert.IsTrue(elements1[i] == elements2[i]);
+		}
 
-		[TestMethod]
-        public void GetElementsByClassName_MultiClasses()
-        {
-            var elements = doc.QuerySelectorAll(".cls-a, .cls-b");
+		[Test]
+		public void GetElementsByClassName_MultiClasses() {
+			var elements = doc.QuerySelectorAll(".cls-a, .cls-b");
 
-            Assert.IsTrue(elements.Count == 2);
-            Assert.IsTrue(elements[0].Id == "spanA");
-            Assert.IsTrue(elements[1].Id == "spanB");
-        }
+			Assert.IsTrue(elements.Count == 2);
+			Assert.IsTrue(elements[0].Id == "spanA");
+			Assert.IsTrue(elements[1].Id == "spanB");
+		}
 
-		[TestMethod]
-        public void GetElementsByClassName_WithUnderscore()
-        {
-            var elements = doc.QuerySelectorAll(".underscore_class");
+		[Test]
+		public void GetElementsByClassName_WithUnderscore() {
+			var elements = doc.QuerySelectorAll(".underscore_class");
 
-            Assert.IsTrue(elements.Count == 1);
-            Assert.IsTrue(elements[0].Id == "spanB");
-        }
+			Assert.IsTrue(elements.Count == 1);
+			Assert.IsTrue(elements[0].Id == "spanB");
+		}
 
+		private static HtmlAgilityPack.HtmlDocument LoadHtml()
+		{
+			var doc = new HtmlAgilityPack.HtmlDocument();
+			doc.LoadHtml(Resource.GetString("Test1.html"));
 
+			return doc;
+		}
 
-        private static HtmlAgilityPack.HtmlDocument LoadHtml()
-        {
-            var doc = new HtmlAgilityPack.HtmlDocument();
-            doc.LoadHtml(Resource.GetString("Test1.html"));
-
-            return doc;
-        }
-
-    }
+	}
 }
