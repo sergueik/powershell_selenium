d87137a38200d591eb540ce26dcb5ea9d70c7cf7
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-03-13 23:36:21 -0400

    intermeidate revision - cosmetic refactoring

--- powershell/cookie_management.ps1
+++ powershell/cookie_management.ps1
@@ -1,4 +1,4 @@
-#Copyright (c) 2014 Serguei Kouzmine
+#Copyright (c) 2014,2019 Serguei Kouzmine
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
@@ -19,101 +19,38 @@
 #THE SOFTWARE.
 
 param(
-  [string]$browser
+  [string]$hub_host = '127.0.0.1',
+  [String]$base_url = 'http://www.wikipedia.org',
+  [string]$browser,
+  [switch]$headless,
+  [switch]$grid,
+  # NOTE:  currently ol works right ith grid option - locally launched Chrome opens in a full screen mode, working but annoying
+  # NOTE: this is because  this is a replica of the table_tnery.ps1 which apatently sets some profile and maximized the browser
+  [switch]$pause
 )
+[bool]$fullstop = [bool]$PSBoundParameters['pause'].IsPresent
+write-debug ('Full Stop: {0}' -f $fullstop )
 
-function cleanup
-{
-  param(
-  [System.Management.Automation.PSReference]$selenium_ref 
-  )
-  try {
-    $selenium_ref.Value.Quit()
-  } catch [exception]{
-  # Ignore errors if unable to close the browser
-  Write-Output (($_.Exception.Message) -split "`n")[0]
-
-  }
+$MODULE_NAME = 'selenium_utils.psd1'
+Import-Module -Name ('{0}/{1}' -f '.',$MODULE_NAME)
+if ([bool]$PSBoundParameters['grid'].IsPresent) {
+  write-debug 'Running on grid'
 }
-
-
-$shared_assemblies = @(
-  "WebDriver.dll",
-  "WebDriver.Support.dll",
-  "Selenium.WebDriverBackedSelenium.dll",
-  'nunit.core.dll',
-  'nunit.framework.dll'
-)
-
-$env:SHARED_ASSEMBLIES_PATH = "c:\java\selenium\csharp\sharedassemblies"
-
-$shared_assemblies_path = $env:SHARED_ASSEMBLIES_PATH
-pushd $shared_assemblies_path
-$shared_assemblies | ForEach-Object { Unblock-File -Path $_; Add-Type -Path $_ }
-popd
-
-$verificationErrors = New-Object System.Text.StringBuilder
-$base_url = 'http://www.wikipedia.org'
-$uri = [System.Uri]('http://127.0.0.1:4444/wd/hub')
-if ($browser -ne $null -and $browser -ne '') {
-  try {
-    $connection = (New-Object Net.Sockets.TcpClient)
-    $connection.Connect("127.0.0.1",4444)
-    $connection.Close()
-  } catch {
-    Start-Process -FilePath "C:\Windows\System32\cmd.exe" -ArgumentList "start cmd.exe /c c:\java\selenium\hub.cmd"
-    Start-Process -FilePath "C:\Windows\System32\cmd.exe" -ArgumentList "start cmd.exe /c c:\java\selenium\node.cmd"
-    Start-Sleep -Seconds 10
-  }
-  Write-Host "Running on ${browser}"
-  if ($browser -match 'firefox') {
-    $capability = [OpenQA.Selenium.Remote.DesiredCapabilities]::Firefox()
-  }
-  elseif ($browser -match 'chrome') {
-    $capability = [OpenQA.Selenium.Remote.DesiredCapabilities]::Chrome()
-  }
-  elseif ($browser -match 'ie') {
-    $capability = [OpenQA.Selenium.Remote.DesiredCapabilities]::InternetExplorer()
-<#
-# The following registry key describes the state of the 'Delete Browsing history on exit' checkbox 
-
-pushd 'HKCU:'
-cd '/Software/Microsoft/Internet Explorer/Privacy'
-get-itemproperty -name 'ClearBrowsingHistoryOnExit' -path 'HKCU:/Software/Microsoft/Internet Explorer/Privacy'
-set-itemproperty -name 'ClearBrowsingHistoryOnExit' -path 'HKCU:/Software/Microsoft/Internet Explorer/Privacy' -value '1'
-popd
-
-#>
-
-  }
-  elseif ($browser -match 'safari') {
-    $capability = [OpenQA.Selenium.Remote.DesiredCapabilities]::Safari()
-  }
-  else {
-    throw ( 'Unknown browser choice: ' + $browser )
-  }
-  
-  try {
-    # TODO:
-    # OpenQA.Selenium.Remote.CapabilityType.ForSeleniumServer possibly not available for C# port
-    $capability.setCapability([OpenQA.Selenium.Remote.CapabilityType.ForSeleniumServer]::ENSURING_CLEAN_SESSION,$true)
-  } catch [exception]{
-  }
-
-  try {
-    $selenium = New-Object OpenQA.Selenium.Remote.RemoteWebDriver ($uri,$capability)
-  } catch [exception]{
-    throw
+if ([bool]$PSBoundParameters['headless'].IsPresent) {
+  write-debug 'Running headless'
+}
+if ([bool]$PSBoundParameters['grid'].IsPresent) {
+  if ([bool]$PSBoundParameters['headless'].IsPresent) {
+    $selenium = launch_selenium -browser $browser -grid -headless
+  } else {
+    $selenium = launch_selenium -browser $browser -grid
   }
 } else {
-  $phantomjs_executable_folder = 'C:\tools\phantomjs'
-  $selenium = New-Object OpenQA.Selenium.PhantomJS.PhantomJSDriver ($phantomjs_executable_folder)
-  $selenium.Capabilities.setCapability('ssl-protocol','any')
-  $selenium.Capabilities.setCapability('ignore-ssl-errors',$true)
-  $selenium.Capabilities.setCapability('takesScreenshot',$true)
-  $selenium.Capabilities.setCapability('userAgent','Mozilla/5.0 (Windows NT 6.1) AppleWebKit/534.34 (KHTML, like Gecko) PhantomJS/1.9.7 Safari/534.34')
-  $options = New-Object OpenQA.Selenium.PhantomJS.PhantomJSOptions
-  $options.AddAdditionalCapability('phantomjs.executable.path',$phantomjs_executable_folder)
+  if ([bool]$PSBoundParameters['headless'].IsPresent) {
+    $selenium = launch_selenium -browser $browser -headless
+  } else {
+    $selenium = launch_selenium -browser $browser
+  }
 }
 
 $selenium.Navigate().GoToUrl($base_url)
@@ -151,8 +88,6 @@ NOTE: Recent Files in the latter directory  are present even before the browser 
 # http://stackoverflow.com/questions/7413966/delete-cookies-in-webdriver 
 <#
 
-
-
 $target_server = '...'
 function clear_cookies{
 
@@ -169,7 +104,9 @@ try {
   // TODO Auto-generated catch block
   e.printStackTrace();
 #>
+
 <#
+
 caps.setCapability(CapabilityType.ForSeleniumServer.ENSURING_CLEAN_SESSION, true); 
 WebDriver driver = new InternetExplorerDriver(caps);
 
@@ -177,45 +114,45 @@ Once initialized, you can use:
 
 driver.manage().deleteAllCookies()
 
-
-# note this is a very very old post:
-
-# http://stackoverflow.com/questions/595228/how-can-i-delete-all-cookies-with-javascript
-# http://stackoverflow.com/questions/2144386/javascript-delete-cookie
 #>
 
-$script = @"
+# NOTE: these are very very old post, and some answers are ranked "low-quality or spam":
+# http://stackoverflow.com/questions/595228/how-can-i-delete-all-cookies-with-javascript
+# http://stackoverflow.com/questions/2144386/javascript-delete-cookie
 
-function createCookie(name,value,days) {
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime()+(days*24*60*60*1000));
-        var expires = "; expires="+date.toGMTString();
-    }
-    else var expires = "";
-    document.cookie = name+"="+value+expires+"; path=/";
+
+$script = @'
+
+function createCookie(name, value, days) {
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    var expires = "; expires=" + date.toGMTString();
+  } else var expires = "";
+  document.cookie = name + "=" + value + expires + "; path=/";
 }
 
 function readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
 }
 
 function eraseCookie(name) {
-    createCookie(name,"",-1);
+  createCookie(name, "", -1);
 }
 
 var cookies = document.cookie.split(";");
 for (var i = 0; i < cookies.length; i++) {
   eraseCookie(cookies[i].split("=")[0]);
 }
-"@
+
+'@
 
 # executeScript works fine with Chrome or Firefox 31, ie 10, but not IE 11.
 # Exception calling "ExecuteScript" with "1" argument(s): "Unable to get browser
@@ -224,5 +161,17 @@ for (var i = 0; i < cookies.length; i++) {
 [void]([OpenQA.Selenium.IJavaScriptExecutor]$selenium).executeScript($script)
 
 # Cleanup
+if ($PSBoundParameters['pause']) {
 
+  try {
+
+    [void]$host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+  } catch [exception]{}
+
+} else {
+  Start-Sleep -Millisecond 1000
+}
+
+# Cleanup
 cleanup ([ref]$selenium)
+
