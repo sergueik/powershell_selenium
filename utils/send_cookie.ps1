07409ff21955e591759ebd60f45637c339610bf5
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-03-18 23:42:19 -0400

    intermediate revision  - just links added

--- utils/send_cookie.ps1
+++ utils/send_cookie.ps1
@@ -1,7 +1,15 @@
-$downloadToPath = 'c:\temp\file.html'
-$remoteFileLocation = 'http://bandi.servizi.politicheagricole.it/taxcredit/Menu.aspx'
+$out_file = 'c:\temp\file.html'
 
-$session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+# based on: https://blog.gripdev.xyz/2015/05/27/powershell-invoke-webrequest-with-a-cookie/
+# see also: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/invoke-webrequest?view=powershell-5.1#examples
+# slightly different format: https://gist.github.com/JohnRoos/f5f40bf8bbef4020b165d4ecff142ffc
+# see also: https://www.adamtheautomator.com/invoke-webrequest-powershell/
+# http://blog.roostech.se/2017/02/invoke-restmethod-with-anti-forgery.html
+
+$url = 'http://bandi.servizi.politicheagricole.it/taxcredit/Menu.aspx'
+$url = 'http://bandi.servizi.politicheagricole.it/taxcredit/moduloTCR.aspx'
+$debug  = $true
+$session = new-object Microsoft.PowerShell.Commands.WebRequestSession
 
 $cookie_data =  convertfrom-json -inputobject ((get-content (resolve-path 'cookies.json') ) -join '')
 
@@ -17,14 +25,17 @@ $cookie_data | foreach-object {
   $cookie.Path = $cookie_item.'path'
   $cookie.HttpOnly = ($false -or $cookie_item.'httpOnly')
   $cookie.Expires  = $ExpiryDate
-  # NOTE: we do not even parsethe epory in the cookie,
+  # NOTE: we do not even parse the expiry field from the cookie,
   # luckily since it is null
-  $cookie.Secure =  ($false -or $cookie_item.'secure')
+  $cookie.Secure = ($false -or $cookie_item.'secure')
 
+  if ($debug){
+    $cookie| format-list
+  }
   $session.Cookies.Add($cookie);
 }
 
-Invoke-WebRequest $remoteFileLocation -WebSession $session -TimeoutSec 900 -OutFile $downloadToPath
+invoke-webrequest -Method POST $url -WebSession $session -TimeoutSec 900 -OutFile $out_file
 
 <#
 # {"path":"/","domain":"bandi.servizi.politicheagricole.it","name":"ASP.NET_SessionId","httpOnly":true,"expiry":null,"secure":false,"value":"xxxxxxxxxxxxxxxxxxxxxxxx"},
@@ -59,8 +70,7 @@ $cookie.Secure = $false
 $session.Cookies.Add($cookie);
 #>
 
-# response is in c:\temp\file.html
-# it looks like it i /default.aspx:
+# response looks like it i /default.aspx:
 #     Normativa
 #    Manuale utente#
 #
