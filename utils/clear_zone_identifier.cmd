385092a5258386667439bd5a5cdab806b3d1d6dd
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-04-09 20:50:08 -0400

    initial snapshot of a ntfs streams "management" hack

---
+++ utils/clear_zone_identifier.cmd
@@ -0,0 +1,8 @@
+@echo OFF
+REM  origin: http://forum.oszone.net/thread-340041.html
+REM quick removal of zone indefier stream information
+cd /D "%~1"
+chcp 65001>nul
+for /F "eol= tokens=1,*" %%a in ('dir /R /A:-D /-C /N ^| findStr.exe /c:"Zone.Identifier"') do (echo %%a %%b && echo.>"%%b")
+REM 2 selenium-server-standalone-2.53.0.jar:Zone.Identifier:$DAT
+REM 2 selenium-server-standalone-3.3.1.jar:Zone.Identifier:$DATA
