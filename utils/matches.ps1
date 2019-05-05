5e20c544f6fe27cc63eaf6efc2bad7b504712e4d
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-05-05 16:53:16 -0400

    example, improved debug messages

--- utils/matches.ps1
+++ utils/matches.ps1
@@ -7,14 +7,15 @@ public class Test {
 	public string findMatches(string text, int pos){
 		MatchCollection matches = Regex.Matches(text, @"([a-z]+)", RegexOptions.Singleline | RegexOptions.IgnoreCase);
 		if (debug) {
-			Console.Error.WriteLine(matches.Count);
-			Console.Error.WriteLine(matches[pos].Groups.Count);
+			Console.Error.WriteLine("Matches count => " + matches.Count);
+			Console.Error.WriteLine(String.Format("Groups in match {0} => {1}" , pos , matches[pos].Groups.Count));
 		}
-		return pos > matches.Count ? null : matches[pos].Groups[1].ToString();
+		return pos > matches.Count ? null : matches[pos].Groups[1].Value;
 	}
 }
 '@
 $o = new-object -TypeName 'Test'
 $string =  'a b c d e f'
 $pos = 4
+$o.debug =  $true
 write-output ('{0} => "{1}"' -f  $pos,  $o.findMatches($string, $pos))
