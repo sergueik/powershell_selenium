01881259e56caa0e0d156b5ee3971a0eb2a91530
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-09-20 20:11:16 -0400

    initial revision

---
+++ utils/configure_chrome.ps1
@@ -0,0 +1,19 @@
+# based on: http://forum.oszone.net/thread-342256.html
+param(
+  [String]$downoad = "${env:USERPROFILE}\Downloads", # no configuration default
+  [switch]$all
+)
+
+$po_filepath = "${env:LocalAppData}\Google\Chrome\User Data\Default\Preferences"
+$po = get-content -Path $po_filepath | convertfrom-json
+# $po is a System.Management.Automation.PSCustomObject
+# its fields are accessed through getters and setters
+
+# TODO: handle 'null' 
+if(-not $po.download.default_directory) {
+  Add-Member -inputObject $po.download -NotePropertyName 'default_directory' -NotePropertyValue $download
+} else {
+  $po.download.default_directory = $download
+}
+
+convertto-json -InputObject $po -Compress | set-content -Path $po_filepath
