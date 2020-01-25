3db89a1bd271cca0307d463b4c733581bc121006
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-01-25 20:24:54 +0100

    intermediate revision - commented on apparent api spec change in recent versions of chrome

--- python/print_pdf.py
+++ python/print_pdf.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # based on: https://habr.com/ru/post/459112 (in Russian)
-# NOTE: not working with recent 
+# failing with certain builds of google-chrome-stable 77
+# - a tiny blank pdf is produced
+# NOTE: works with chromium-browser 79 and google-chrome-stable 76 and 72
 import sys
 from os import getenv
 from selenium import webdriver
@@ -21,20 +23,19 @@ def print_pdf(path, chromedriver = './chromedriver', print_options = {}):
   }
   params.update(print_options)
   result = send_command_and_get_result(driver, 'Page.printToPDF', params)
+  # print( result.keys())
   driver.quit()
   return base64.b64decode(result['data'])
 
 def send_command_and_get_result(driver, cmd, params = {}):
   # https://www.python-course.eu/python3_formatted_output.php
   response = driver.command_executor._request('POST', driver.command_executor._url + '/session/{0:s}/chromium/send_command_and_get_result'.format( driver.session_id), json.dumps({'cmd': cmd, 'params': params}))
+  # NOTE: 'has_key()' is even removed from P 3.x
+  # see also: https://stackoverflow.com/questions/1323410/should-i-use-has-key-or-in-on-python-dicts
+  # NOTE: KeyError: 'status'
+  # early imlementation returns JSON with ['status', 'sessionId', 'value'] keys
+  # with recent versions of chrome response contains only has ['value']['data']
   # print( response.keys())
-  # NOTE: on bionic 18.04 ubuntu Linix with google-chrome-stable 77 or chromium-browser 79
-  # the response just contains 'value', no 'status' key leading to 
-  # KeyError: 'status'
-  # on xenial16.04 with chromium-browser 49 and google-chrome-stable 72
-  # response has 'status', u'sessionId', u'value'
-  # https://stackoverflow.com/questions/1323410/should-i-use-has-key-or-in-on-python-dicts
-  # has_key is even removed from P 3.x 
   if ('status' in response ) and response['status']:
     raise Exception(response.get('value'))
   return response.get('value')
