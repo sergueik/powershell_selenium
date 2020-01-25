1eeec85aad0608c34fd9c0798494ae80e2bd7616
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-01-25 19:36:46 +0100

    intermediate revision - misc notes on tability for recent chromium / chrome-stable

--- python/print_pdf.py
+++ python/print_pdf.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-# origin: https://habr.com/ru/post/459112 (in Russian)
-
+# based on: https://habr.com/ru/post/459112 (in Russian)
+# NOTE: not working with recent 
 import sys
 from os import getenv
 from selenium import webdriver
@@ -27,7 +27,15 @@ def print_pdf(path, chromedriver = './chromedriver', print_options = {}):
 def send_command_and_get_result(driver, cmd, params = {}):
   # https://www.python-course.eu/python3_formatted_output.php
   response = driver.command_executor._request('POST', driver.command_executor._url + '/session/{0:s}/chromium/send_command_and_get_result'.format( driver.session_id), json.dumps({'cmd': cmd, 'params': params}))
-  if response['status']:
+  # print( response.keys())
+  # NOTE: on bionic 18.04 ubuntu Linix with google-chrome-stable 77 or chromium-browser 79
+  # the response just contains 'value', no 'status' key leading to 
+  # KeyError: 'status'
+  # on xenial16.04 with chromium-browser 49 and google-chrome-stable 72
+  # response has 'status', u'sessionId', u'value'
+  # https://stackoverflow.com/questions/1323410/should-i-use-has-key-or-in-on-python-dicts
+  # has_key is even removed from P 3.x 
+  if ('status' in response ) and response['status']:
     raise Exception(response.get('value'))
   return response.get('value')
 
