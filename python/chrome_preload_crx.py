15788360c933a7d3acadcbcda4ac225f1710689f
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-01-25 23:07:54 -0500

    initial snapshot

---
+++ python/chrome_preload_crx.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# origin: http://coreygoldberg.blogspot.com/2018/09/python-using-chrome-extensions-with.html
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+if __name__ == '__main__':
+  if getenv('OS') != None :
+    homedir = getenv('HOMEDIR').replace('\\', '/')
+  else:
+    homedir = getenv('HOME')
+  extension = 'cropath.crx'# download archive
+  packed_extension_path = '{0:s}/Downloads/{1:s}'.format(homedir, extention)
+  options = Options()
+  options.add_extension(packed_extension_path)
+  # driver = webdriver.Chrome(options = options)
+
+  # develper extension project
+  unpacked_extension_path = os.path.abspath('chrome_extension_project')
+  options.add_argument('--load-extension={}'.format(unpacked_extension_path))
+  driver = webdriver.Chrome(options = options)
