d7f4f50fd0f6b942e10a4fbf2222a18832b6521a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-04-23 02:01:06 -0400

    intermediate revision - added  console logs example

---
+++ python/browser_console_logs.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from __future__ import print_function
+import time
+import sys
+import os
+import re
+from os import getenv
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
+
+# print console logs messages
+def get_console_errors(driver):
+  browserlogs = driver.get_log('driver')
+  errors = []
+  for entry in browserlogs:
+    if entry['level'] == 'SEVERE':
+      errors.append(entry)
+  return errors
+
+is_windows = getenv('OS') != None and re.compile('.*NT').match( getenv('OS'))
+homedir = getenv('USERPROFILE' if is_windows else 'HOME')
+print('user home directory path: {}'.format(homedir))
+chromedriver_path = homedir + os.sep + 'Downloads' + os.sep + ('chromedriver.exe' if is_windows else 'chromedriver')
+options = Options()
+print('chromedriver path: {}'.format(chromedriver_path))
+# enable browser logging
+disired_capabilities = DesiredCapabilities.CHROME
+disired_capabilities['loggingPrefs'] = {
+  'browser':'ALL'
+}
+driver = webdriver.Chrome(executable_path = chromedriver_path, desired_capabilities = disired_capabilities)
+
+# load the desired webpage
+driver.get('http://www.cnn.com')
+time.sleep(10)
+console_browserlog_errors = get_console_errors(driver)
+for log_entry in console_browserlog_errors:
+  print( log_entry)
+
+driver.quit()
--- python/chromepass.py
+++ python/chromepass.py
@@ -11,126 +11,126 @@ import json
 import argparse
 
 try:
-    import win32crypt  # python -m pip install pywin32
+  import win32crypt  # python -m pip install pywin32
 except:
-    pass
+  pass
 
 def args_parser():
 
-    parser = argparse.ArgumentParser(
-        description='Retrieve Google Chrome Passwords')
-    parser.add_argument('-o', '--output', choices=['csv', 'json'],
-                        help='Output passwords to [ CSV | JSON ] format.')
-    parser.add_argument('-b', '--browser', choices=['vivaldi', 'chrome', 'none'],
-                        help='Browser ("chrome" is default).')
-    parser.add_argument(
-        '-d', '--dump', help='Dump passwords to stdout. ', action='store_true')
-    browser = 'chrome'
-    args = parser.parse_args()
-    if args.browser != None:
-        browser = args.browser
-    if args.dump:
-        for data in main(browser):
-            print(data)
-    if args.output == 'csv':
-        output_csv(main(browser))
-        return
+  parser = argparse.ArgumentParser(
+    description='Retrieve Google Chrome Passwords')
+  parser.add_argument('-o', '--output', choices=['csv', 'json'],
+            help='Output passwords to [ CSV | JSON ] format.')
+  parser.add_argument('-b', '--browser', choices=['vivaldi', 'chrome', 'none'],
+            help='Browser ("chrome" is default).')
+  parser.add_argument(
+    '-d', '--dump', help='Dump passwords to stdout. ', action='store_true')
+  browser = 'chrome'
+  args = parser.parse_args()
+  if args.browser != None:
+    browser = args.browser
+  if args.dump:
+    for data in main(browser):
+      print(data)
+  if args.output == 'csv':
+    output_csv(main(browser))
+    return
 
-    if args.output == 'json':
-        output_json(main(browser))
-        return
+  if args.output == 'json':
+    output_json(main(browser))
+    return
 
-    else:
-        parser.print_help()
+  else:
+    parser.print_help()
 
 
 def main(app = None):
-    info_list = []
-    path = getpath(app)
-    try:
-        connection = sqlite3.connect(path + 'Login Data')
-        with connection:
-            cursor = connection.cursor()
-            v = cursor.execute(
-                'SELECT action_url, username_value, password_value FROM logins')
-            value = v.fetchall()
+  info_list = []
+  path = getpath(app)
+  try:
+    connection = sqlite3.connect(path + 'Login Data')
+    with connection:
+      cursor = connection.cursor()
+      v = cursor.execute(
+        'SELECT action_url, username_value, password_value FROM logins')
+      value = v.fetchall()
 
-        if (os.name == 'posix') and (sys.platform == 'darwin'):
-            print('Mac OSX not supported.')
-            sys.exit(0)
+    if (os.name == 'posix') and (sys.platform == 'darwin'):
+      print('Mac OSX not supported.')
+      sys.exit(0)
 
-        for origin_url, username, password in value:
-            if os.name == 'nt':
-                password = win32crypt.CryptUnprotectData(
-                    password, None, None, None, 0)[1]
+    for origin_url, username, password in value:
+      if os.name == 'nt':
+        password = win32crypt.CryptUnprotectData(
+          password, None, None, None, 0)[1]
 
-            if password:
-                info_list.append({
-                    'origin_url': origin_url,
-                    'username': username,
-                    'password': str(password)
-                })
+      if password:
+        info_list.append({
+          'origin_url': origin_url,
+          'username': username,
+          'password': str(password)
+        })
 
-    except sqlite3.OperationalError as e:
-        e = str(e)
-        if (e == 'database is locked'):
-            print('[!] Make sure Google Chrome is not running in the background')
-        elif (e == 'no such table: logins'):
-            print('[!] Something wrong with the database name')
-        elif (e == 'unable to open database file'):
-            print('[!] Something wrong with the database path')
-        else:
-            print(e)
-        sys.exit(0)
+  except sqlite3.OperationalError as e:
+    e = str(e)
+    if (e == 'database is locked'):
+      print('[!] Make sure Google Chrome is not running in the background')
+    elif (e == 'no such table: logins'):
+      print('[!] Something wrong with the database name')
+    elif (e == 'unable to open database file'):
+      print('[!] Something wrong with the database path')
+    else:
+      print(e)
+    sys.exit(0)
 
-    return info_list
+  return info_list
 
 
 def getpath(app = 'chrome'):
-    app_path = {'chrome': 'Google\\Chrome', 'vivaldi': 'Vivaldi', 'none': 'None' }
-    if os.name == 'nt':
-        # Windows
-        user_data_dir = os.getenv('localappdata') + \
-        '\\' + app_path.get(app) + \
-            '\\User Data\\Default\\'
-    elif os.name == 'posix':
-        user_data_dir = os.getenv('HOME')
-        if sys.platform == 'darwin':
-            # OS X
-            user_data_dir += '/Library/Application Support/Google/Chrome/Default/'
-        else:
-            # Linux
-            user_data_dir += '/.config/google-chrome/Default/'
-    if not os.path.isdir(user_data_dir):
-        print("User data directory of application {} doesn\'t exists".format(app))
-        sys.exit(0)
+  app_path = {'chrome': 'Google\\Chrome', 'vivaldi': 'Vivaldi', 'none': 'None' }
+  if os.name == 'nt':
+    # Windows
+    user_data_dir = os.getenv('localappdata') + \
+    '\\' + app_path.get(app) + \
+      '\\User Data\\Default\\'
+  elif os.name == 'posix':
+    user_data_dir = os.getenv('HOME')
+    if sys.platform == 'darwin':
+      # OS X
+      user_data_dir += '/Library/Application Support/Google/Chrome/Default/'
+    else:
+      # Linux
+      user_data_dir += '/.config/google-chrome/Default/'
+  if not os.path.isdir(user_data_dir):
+    print("User data directory of application {} doesn\'t exists".format(app))
+    sys.exit(0)
 
-    return user_data_dir
+  return user_data_dir
 
 
 def output_csv(info):
-    try:
-        with open('chromepass-passwords.csv', 'wb') as csv_file:
-            csv_file.write('origin_url,username,password \n'.encode('utf-8'))
-            for data in info:
-                csv_file.write(("%s, %s, %s \n" % (data['origin_url'], data[
-                    'username'], data['password'])).encode('utf-8'))
-        print('Data written to chromepass-passwords.csv')
-    except EnvironmentError:
-        print('EnvironmentError: cannot write data')
+  try:
+    with open('chromepass-passwords.csv', 'wb') as csv_file:
+      csv_file.write('origin_url,username,password \n'.encode('utf-8'))
+      for data in info:
+        csv_file.write(("%s, %s, %s \n" % (data['origin_url'], data[
+          'username'], data['password'])).encode('utf-8'))
+    print('Data written to chromepass-passwords.csv')
+  except EnvironmentError:
+    print('EnvironmentError: cannot write data')
 
 
 def output_json(info):
-	try:
-		with open('chromepass-passwords.json', 'w') as json_file:
-			json.dump({'password_items':info},json_file)
-			print('Data written to chromepass-passwords.json')
-	except EnvironmentError:
-		print('EnvironmentError: cannot write data')
+  try:
+    with open('chromepass-passwords.json', 'w') as json_file:
+      json.dump({'password_items':info},json_file)
+      print('Data written to chromepass-passwords.json')
+  except EnvironmentError:
+    print('EnvironmentError: cannot write data')
 
 
 
 if __name__ == '__main__':
-    args_parser()
-	
+  args_parser()
+  
 
