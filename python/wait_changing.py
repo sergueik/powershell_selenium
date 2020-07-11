04a15e2ddf609e2b9c27e490012473e20411d822
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-07-11 17:34:57 -0400

    intermediate revision - added python syntax example helper methods

--- python/wait_changing.py
+++ python/wait_changing.py
@@ -17,6 +17,42 @@ class waittest2:
     else:
       self.last_value = initial_value
     print('Last value:{}'.format(self.last_value))
+    # origin: https://stackoverflow.com/questions/17082425/running-selenium-webdriver-with-a-proxy-in-python
+    self.driver.implicitly_wait(30)
+    self.base_url = "https://www.google.ie/"
+    self.verificationErrors = []
+    self.accept_next_alert = True
+
+  def tearDown(self):
+    self.driver.quit()
+    self.assertEqual([], self.verificationErrors)
+
+  def is_element_present(self, how, what):
+    try:
+      self.driver.find_element(by=how, value=what)
+    except NoSuchElementException as e:
+      return False
+    return True
+
+  def is_alert_present(self):
+    try:
+      self.driver.switch_to_alert()
+    except NoAlertPresentException as e:
+      return False
+    return True
+
+  def close_alert_and_get_its_text(self):
+    try:
+      alert = self.driver.switch_to_alert()
+      alert_text = alert.text
+      if self.accept_next_alert:
+        alert.accept()
+      else:
+        alert.dismiss()
+      return alert_text
+    finally:
+      self.accept_next_alert = True
+
   def __call__(self, driver):
     element = driver.find_element_by_css_selector(self._locator)
     self.new_value = element.get_attribute(self._attribute)
@@ -34,7 +70,7 @@ if __name__ == '__main__':
     homedir = getenv('USERPROFILE').replace('\\', '/')
   else:
     homedir = getenv('HOME')
-  
+
   location = 'file:///{0}/{1}'.format('{0}/Downloads'.format(homedir), 'clock.html')
   driver.get(location)
 
