ee8e319f3325d2cb48d99917fe9fc9a802a006c9
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-07-12 16:13:01 +0200

    intermediate revision - added few (non-working) keyboard shortcut and logging of action and effect

--- python/tab_exercise.py
+++ python/tab_exercise.py
@@ -28,19 +28,46 @@ driver = webdriver.Chrome(chromedriver, options = options)
 url = 'http://www.wikipedia.org/'
 driver.get(url)
 handles = driver.window_handles
+num_tabs = len(handles)
 orig_handle = handles[0]
 element = driver.find_element_by_css_selector('body')
 print ('"body" element id: {}'.format(element.get_attribute('id')))
-time.sleep(3)
 
-# the following code has no effect
+# the following code has no effect on Linux
+print('open new tab via keyboard: {}'.format("Keys.CONTROL + 'T'"))
 element.send_keys(Keys.CONTROL + 'T')
-# open new tab
-driver.execute_script("window.open('" +url + "');")
-time.sleep(3)
+time.sleep(1)
 # reload handles
 handles = driver.window_handles
-print('window handles: {}'.format(handles))
+if len(handles) > num_tabs:
+  print('window handles: {}'.format(handles))
+else:
+  print('no extra tabs')
+num_tabs = len(handles)
+
+# the following code has no effect on Linux
+print('open new tab via keyboard: {}'.format("Keys.CONTROL + Keys.TAB"))
+element.send_keys(Keys.CONTROL + Keys.TAB)
+time.sleep(1)
+# reload handles
+handles = driver.window_handles
+if len(handles) > num_tabs:
+  print('window handles: {}'.format(handles))
+else:
+  print('no extra tabs')
+num_tabs = len(handles)
+
+print('open new tab through script injection')
+driver.execute_script("window.open('{}');".format(url))
+time.sleep(1)
+# reload handles
+handles = driver.window_handles
+if len(handles) > num_tabs:
+  print('window handles: {}'.format(handles))
+else:
+  print('no extra tabs')
+num_tabs = len(handles)
+# https://python-3-patterns-idioms-test.readthedocs.io/en/latest/Comprehensions.html
 new_handle = [x for x in handles if x != orig_handle][0]
 try:
   print('switching to new window handle: {}'.format(new_handle))
@@ -49,17 +76,17 @@ try:
   driver.close()
   print('switching to original window handle: {}'.format(orig_handle))
   driver.switch_to_window(orig_handle)
-except WebDriverException,e:
+except WebDriverException as e:
   print('Excepion (ignored): {}'.format(e))
   pass
-except InvalidArgumentException,e:
+except InvalidArgumentException as e:
   print('Excepion (ignored): {}'.format(e))
   pass
 time.sleep(3)
 if driver != None:
   try:
     driver.close()
-  except NoSuchWindowException,e:
+  except NoSuchWindowException as e:
     print('Excepion (ignored): {}'.format(e))
     pass
   driver.quit()
