1c62aa455f0f3ef96b0f1276272df6d1df446579
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-10-16 01:20:52 +0200

    intermediate revision - added Python parser capable of digeting of the "ultra-short hand notation" ansible-like YAML

--- python/playbook_parse.py
+++ python/playbook_parse.py
@@ -1,29 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import yaml
 import sys
 import pprint
-input_file = sys.argv[1]
-f = open(input_file)
-c = yaml.load(f, Loader=yaml.FullLoader)
-pp = pprint.PrettyPrinter(indent=2)
-# TypeError: 'module' object is not callable
-pp.pprint(c)
-tasks = c['tasks']
+import re
+
+DEBUG = False
+pp = pprint.PrettyPrinter(indent = 2)
+# TODO: captures values with a space inside single quotes  
+# except leading and trailing space in the value
+# pattern = re.compile(r"'([^=']+ [^=']+)'")
+tokenizer_expression = "'(?P<word>[^=']+ [^=']+)'" # no need for 'raw'
+
+playbook = yaml.load(open(sys.argv[1]), Loader=yaml.FullLoader)
+if DEBUG:
+  pp.pprint(playbook)
+tasks = playbook['tasks']
 for cnt in range(len(tasks)):
   task = tasks[cnt]
 
-  pp.pprint(task['package'])
+  if DEBUG:
+    pp.pprint(task['package'])
   try:
     data = task['package']
-    pp.pprint(data)
-    values = dict(item.split('=') for item in data.split('  *'))
-    pp.pprint(values['name'])
+    if DEBUG:
+      pp.pprint(data)
+    
+    pattern = re.compile(tokenizer_expression)
+    total_cnt = 0
+    new_data = data
+    # https://stackoverflow.com/questions/3345785/getting-number-of-elements-in-an-iterator-in-python
+    if DEBUG:
+      for matches in re.finditer(pattern, data):
+        total_cnt = total_cnt + 1
+        print('found: "{}"'.format( matches.group(1)))
+    else:
+      total_cnt =  sum(1 for _ in re.finditer(pattern, data))
+    for cnt in range(total_cnt):
+      matches = pattern.search( data )
+      if matches != None:
+        word = matches.group('word')
+        if ' ' in word:
+          new_word = word.replace(' ', '0x20') 
+          if DEBUG:
+            print('processing "{}"'.format(word))
+          new_data = data.replace("'{}'".format(word), "'{}'".format(new_word))
+        else:
+          new_data = data
+      if DEBUG:    
+        print('temporary data (iteration {}): "{}"'.format(cnt, new_data))
+      data = new_data
+    values = {}
+    raw_values = dict(item.split('=') for item in new_data.split(' '))
+    for k in raw_values:
+      values[k] = raw_values[k].replace('0x20', ' ').replace("'", '')
+    subkey = 'name'  
+    print('{}="{}"'.format(subkey, values[subkey]))
   except TypeError as e:
-    print(str(e))
-    # string indices must be integers
-    # because the value of the package is tokenized by ansible not yaml
+    print(str(e), file = sys.stderr)
     pass
   except ValueError as e:
-    print(str(e))
+    print(str(e), file = sys.stderr)
     # TODO: ValueError: dictionary update sequence element #0 has length 3; 2 is required
     pass
-  
+
