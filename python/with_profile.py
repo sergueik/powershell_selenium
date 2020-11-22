301552c5056499647ab0edd4379416212114f860
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-11-22 16:52:13 -0500

    cosmetic changes, logging custom profile directory usage

--- python/with_profile.py
+++ python/with_profile.py
@@ -12,31 +12,49 @@ from selenium.webdriver.chrome.options import Options
 
 is_windows = getenv('OS') != None and re.compile('.*NT').match( getenv('OS'))
 homedir = getenv('USERPROFILE' if is_windows else 'HOME')
-print('user home directory path: {}'.format(homedir))
+
+print('user home directory path: {}'.format(homedir), file = sys.stderr)
 
 chromedriver_path = homedir + os.sep + 'Downloads' + os.sep + ('chromedriver.exe' if is_windows else 'chromedriver')
+
+print('chromedriver path: {}'.format(chromedriver_path), file = sys.stderr)
+
 options = Options()
-print('chromedriver path: {}'.format(chromedriver_path))
-dir_name = None
+profile_dir_name = None
 if len(sys.argv) > 1:
-  dir_name = sys.argv[1]
-if dir_name == None:
-  dir_name = 'CustomProfile'
-user_data_dir ='{0}\\AppData\\Local\\Google\\Chrome\\User Data\\{1}'.format(getenv('USERPROFILE'), dir_name) if is_windows else '/home/{0}/.config/{1}'.format(getenv('USER'), dir_name)
-print('user data dir path: {}'.format(user_data_dir))
+  profile_dir_name = sys.argv[1]
+if profile_dir_name == None:
+  profile_dir_name = 'CustomProfile'
+user_data_dir ='{0}\\AppData\\Local\\Google\\Chrome\\User Data\\{1}'.format(getenv('USERPROFILE'), profile_dir_name) if is_windows else '/home/{0}/.config/{1}'.format(getenv('USER'), profile_dir_name)
+if os.path.isdir(user_data_dir):
+  print('Custom profile will be used: "{}"'.format(user_data_dir), file = sys.stderr)
+else:
+  print('Custom profile will be created: "{}"'.format(user_data_dir), file = sys.stderr)
+
 options.add_argument( 'user-data-dir={}'.format(user_data_dir))
 try:
   driver = webdriver.Chrome(executable_path = chromedriver_path, options = options)
+# TODO: Message: unknown error: Chrome failed to start: exited normally.
+# unknown error: DevToolsActivePort file doesn't exist
+# The process started from chrome location
+# C:\Program Files\Google\Chrome\Application\chrome.exe
+# is no longer running, so ChromeDriver is assuming that Chrome has crashed.
+# from chromedriver / chrome version mismatch
 except WebDriverException,e :
   driver = None
-  print( e)
+  print(e, file = sys.stderr)
   pass
-  # unknown error: Could not remove old devtools port file. Perhaps the given user-data-dir at ... is still attached to a running Chrome or Chromium process
+  # TODO: handle unknown error: Could not remove old devtools port file.
+  # Perhaps the given user-data-dir at ... is still
+  # attached to a running Chrome or Chromium process
 if driver != None:
   driver.get('chrome://version/')
+
   time.sleep(10)
   driver.close()
   driver.quit()
+  if not os.path.isdir(user_data_dir + ('\\' if is_windows else '/') + 'Default' ):
+    print('The profile was not created: "{}"'.format(user_data_dir), file = sys.stderr)
 
 # on vanilla Windows node
 # PATH=%PATH%;c:\Python27;%USERPROFILE%\Downloads
