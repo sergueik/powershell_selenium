d649afedcf17c1b8caf948831b0331ab56db958c
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-03-10 09:35:47 -0500

    intermediate revision - added a note about System.Array.Find method argument signature and added System.Linq.Enumerable.Where one

--- utils/fluent_wait_exercise.ps1
+++ utils/fluent_wait_exercise.ps1
@@ -13,20 +13,31 @@ $exact_find = [Boolean](
 }))
 
 write-output $exact_find
-# NOTE: 'contains' method is is not doing what it name says
+# NOTE: '-contains' method is is not doing what it name says
+# https://docs.microsoft.com/en-us/dotnet/api/system.array.findall?view=netframework-4.0
+#
 $match_find = [Boolean](
   [Array]::Find($data,[System.Predicate[String]]{
   return ($args[0] -match $text)
 }))
-write-output $match_find
+write-output ('match_find(boolean): {0}' -f $match_find)
 
+# the default is return the matching values
+$text = 'e'
+$matching_elements = (
+  [Array]::FindAll($data,[System.Predicate[String]]{
+  return ($args[0] -match $text)
+}))
+write-output ('matching_elements(array) of {0}: {1}' -f $text, ($matching_elements -join ','))
 
+$text = 'pear'
+# this is not really needed with System.Array.Find but will preactice with plan touse with Wait.Util
 $matching_element = [String](
   [Array]::Find($data,[System.Predicate[String]]<# follows the code block #>{
   if ($text -match $args[0]) { return $args[0] } else { return $null }
 }))
 
-write-output $matching_element
+write-output ('another matching_element: {0}' -f $matching_element)
 
 # NOTE: strong plain.net type
 [String[]]$data = @(
@@ -35,8 +46,12 @@ write-output $matching_element
   'pear',
   'plum'
 )
+
 # both signatures are accepted
+write-output 'select example'
 [System.Linq.Enumerable]::Select($data,[Func[String,String]] <# follows the code block #> { return $args[0].ToUpper()})
 
 [System.Linq.Enumerable]::Select($data,[System.Func[[String],[String]]] <# follows the code block #> { return $args[0].ToUpper()})
 
+write-output 'where example'
+[System.Linq.Enumerable]::Where($data,[Func[String,Bool]] <# follows the code block #> { return $args[0].ToLower() -match $text })
