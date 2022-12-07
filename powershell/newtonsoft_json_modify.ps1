8f80c6f80c8b2a96a376340381430e7c76445f02
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2022-12-07 01:58:49 +0100

    intermediate revision - attempted to add default arguments to method, but incorrectly

--- powershell/newtonsoft_json_modify.ps1
+++ powershell/newtonsoft_json_modify.ps1
@@ -69,34 +69,34 @@ namespace WebTester {
       this.rawdata = rawdata;
     }
 
-    public JObject addElement() {
+    public JObject addElement(string elementName = "friendslist" , string  newElementName = "CustomCommands") {
 
       // NOTE: cannot mix stringly typed and LINQ serialization type
-      // JObject? obj = (JObject?) test["friendslist"];
+      // JObject? obj = (JObject?) test[elementName];
       // 'Newtonsoft.Json.Linq.JObject' must be a non-nullable value type in order to use it as parameter 'T' in the generic type or method 'System.Nullable<T>'
 
-      // JObject obj = (JObject) test["friendslist"];
+      // JObject obj = (JObject) test[elementName];
       // Cannot apply indexing with [] to an expression of type 'WebTester.Test'
 
       // JObject obj = (JObject) test.friendslist;
       // Cannot convert type 'WebTester.Friendslist' to 'Newtonsoft.Json.Linq.JObject'
 
       var data = JObject.Parse(rawdata);
-      JObject obj = (JObject)data["friendslist"];
-      obj.Add(new JProperty("CustomCommands", new JObject()));
-      data["friendslist"] = obj;
+      JObject obj = (JObject)data[elementName];
+      obj.Add(new JProperty(newElementName, new JObject()));
+      data[elementName] = obj;
       return data;
     }
 
-    public JObject addElement(String elementData) {
+    public JObject addElement(string newElementData, string elementName = "friendslist" , string  newElementName = "CustomCommands" ) {
+      //optional parameters must appear after all required parameters
       var data = JObject.Parse(rawdata);
-      JObject obj = (JObject)data["friendslist"];
-      var element = JObject.Parse(elementData);
-      obj.Add(new JProperty("CustomCommands", element));
-      data["friendslist"] = obj;
+      JObject obj = (JObject)data[elementName ];
+      var element = JObject.Parse(newElementData);
+      obj.Add(new JProperty(newElementName, element));
+      data[elementName] = obj;
       return data;
     }
-
   }
 }
 '@  -ReferencedAssemblies "${shared_assemblies_path}\Newtonsoft.Json.dll","${shared_assemblies_path}\nunit.framework.dll",'System.dll','System.Data.dll','Microsoft.CSharp.dll','System.Xml.Linq.dll','System.Xml.dll'
@@ -127,10 +127,7 @@ write-output $o2.ToString()
 [
     [
         [
-            "{\r\n  \"steamid\": \"76561198031578776\",\r\n  \"relationship\": \
-"friend\",\r\n  \"friend_since\": 1519194870\r\n} {\r\n  \"steamid\": \"76561198
-040628535\",\r\n  \"relationship\": \"friend\",\r\n  \"friend_since\": 146074328
-9\r\n}"
+            "{\r\n  \"steamid\": \"76561198031578776\",\r\n  \"relationship\": \"friend\",\r\n  \"friend_since\": 1519194870\r\n} {\r\n  \"steamid\": \"76561198040628535\",\r\n  \"relationship\": \"friend\",\r\n  \"friend_since\": 1460743289\r\n}"
         ],
         [
             ""
@@ -140,7 +137,122 @@ write-output $o2.ToString()
 
 #>
 $o.rawdata = $data;
-$o3 = $o.addElement("{""name"":""value"", ""a"": [""b"",""c""]}")
+try {
+  $o3 = $o.addElement("{""name"":""value"", ""a"": [""b"",""c""]}")
+} catch [Exception] { 
+  # Exception calling "addElement" with "1" argument(s): "Object reference not set to an instance of an object."
+  write-output $_.Exception.Message
+}
+# should put all interacting classes in one virtual type definition otherwise cannnot compile (below is an example):
+# The type or namespace name 'Tester' does not exist in the namespace 'WebTester' (are you missing an assembly reference?)
+#
+<#
+
+add-type -TypeDefinition @'
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+namespace WebTester {
+  public class Tester2 {
+    public String rawdata {
+      get;
+      set;
+    }
+    public String elementdata {
+      get;
+      set;
+    }
+    public Tester2(String rawdata,String elementdata ) {
+      this.rawdata = rawdata;
+      this.elementdata  = elementdata;
+    }
+    public JObject addElement2(){
+    var o = new WebTester.Tester(rawdata);
+    return o.addElement(elementdata);
+    }
+  }
+}
+'@  -ReferencedAssemblies "${shared_assemblies_path}\Newtonsoft.Json.dll","${shared_assemblies_path}\nunit.framework.dll",'System.dll','System.Data.dll','Microsoft.CSharp.dll','System.Xml.Linq.dll','System.Xml.dll'
+
+#>
+<#
+add-type -TypeDefinition @'
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace WebTester {
+  public class Tester1 {
+    public String rawdata {
+      get;
+      set;
+    }
+    public Tester1(String rawdata) {
+      this.rawdata = rawdata;
+    }
+
+    public JObject addElement(string elementName = "friendslist" , string  newElementName = "CustomCommands") {
+
+      // NOTE: cannot mix stringly typed and LINQ serialization type
+      // JObject? obj = (JObject?) test[elementName];
+      // 'Newtonsoft.Json.Linq.JObject' must be a non-nullable value type in order to use it as parameter 'T' in the generic type or method 'System.Nullable<T>'
+
+      // JObject obj = (JObject) test[elementName];
+      // Cannot apply indexing with [] to an expression of type 'WebTester.Test'
+
+      // JObject obj = (JObject) test.friendslist;
+      // Cannot convert type 'WebTester.Friendslist' to 'Newtonsoft.Json.Linq.JObject'
+
+      var data = JObject.Parse(rawdata);
+      JObject obj = (JObject)data[elementName];
+      obj.Add(new JProperty(newElementName, new JObject()));
+      data[elementName] = obj;
+      return data;
+    }
+
+    public JObject addElement(string newElementData, string elementName = "friendslist" , string  newElementName = "CustomCommands" ) {
+      //optional parameters must appear after all required parameters
+      var data = JObject.Parse(rawdata);
+      JObject obj = (JObject)data[elementName ];
+      var element = JObject.Parse(newElementData);
+      obj.Add(new JProperty(newElementName, element));
+      data[elementName] = obj;
+      return data;
+    }
+  }
+    public class Tester2 {
+    public String rawdata {
+      get;
+      set;
+    }
+    public String elementdata {
+      get;
+      set;
+    }
+    public Tester2(String rawdata,String elementdata ) {
+      this.rawdata = rawdata;
+      this.elementdata  = elementdata;
+    }
+    public JObject addElement2(){
+    // The call is ambiguous between the following methods or properties:
+    var o = new WebTester.Tester1(rawdata);
+    return o.addElement(elementdata);
+    }
+  }
+
+}
+'@  -ReferencedAssemblies "${shared_assemblies_path}\Newtonsoft.Json.dll","${shared_assemblies_path}\nunit.framework.dll",'System.dll','System.Data.dll','Microsoft.CSharp.dll','System.Xml.Linq.dll','System.Xml.dll'
+#>
+
+
+$elementName = "friendslist" 
+$newElementName = "CustomCommands" 
+$o3 = $o.addElement("{""name"":""value"", ""a"": [""b"",""c""]}", $elementName, $newElementName)
+
 write-output $o3.ToString()
 <#
 
@@ -148,10 +260,7 @@ $o3 | convertto-json
 [
     [
         [
-            "{\r\n  \"steamid\": \"76561198031578776\",\r\n  \"relationship\": \
-"friend\",\r\n  \"friend_since\": 1519194870\r\n} {\r\n  \"steamid\": \"76561198
-040628535\",\r\n  \"relationship\": \"friend\",\r\n  \"friend_since\": 146074328
-9\r\n}"
+            "{\r\n  \"steamid\": \"76561198031578776\",\r\n  \"relationship\": \"friend\",\r\n  \"friend_since\": 1519194870\r\n} {\r\n  \"steamid\": \"76561198040628535\",\r\n  \"relationship\": \"friend\",\r\n  \"friend_since\": 1460743289\r\n}"
         ],
         [
             "\"name\": \"value\" \"a\": [\r\n  \"b\",\r\n  \"c\"\r\n]"
