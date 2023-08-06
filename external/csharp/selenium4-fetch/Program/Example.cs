9a188a8e363fbf39d656deb3b0227424a0b7192e
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-08-06 03:18:53 +0200

    intermediate revision - removed the non working code and used the skeleton project to try basic CDP / DevTools commands (only one for now)

--- external/csharp/selenium4-fetch/Program/Example.cs
+++ external/csharp/selenium4-fetch/Program/Example.cs
@@ -1,10 +1,16 @@
 // origin: https://qna.habr.com/q/1266474?e=13639006#clarification_1709400
+// see also: https://stackoverflow.com/questions/72771825/selenium-4-c-sharp-chrome-devtools
 // (in Russian)
 
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.DevTools;
 using OpenQA.Selenium.DevTools.V109;
+//
+using DevToolsSessionDomains = OpenQA.Selenium.DevTools.V109.DevToolsSessionDomains;
+using EnableCommandSettings = OpenQA.Selenium.DevTools.V109.Page.EnableCommandSettings;
+using AddScriptToEvaluateOnNewDocumentCommandSettings = OpenQA.Selenium.DevTools.V109.Page.AddScriptToEvaluateOnNewDocumentCommandSettings;
+using SetDeviceMetricsOverrideCommandSettings = OpenQA.Selenium.DevTools.V109.Emulation.SetDeviceMetricsOverrideCommandSettings;
 using System.Linq;
 using System;
 using System.Management;
@@ -16,50 +22,38 @@ using System.Text;
 class Example {
 	
 	private readonly static string driverLocation = Environment.GetEnvironmentVariable("CHROMEWEBDRIVER");
+	
 	static void Main(string[] args) {
-		System.Environment.SetEnvironmentVariable("webdriver.chrome.driver",System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase).Replace("file:\\", ""));
+		System.Environment.SetEnvironmentVariable("webdriver.chrome.driver", System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase).Replace("file:\\", ""));
 		var options = new ChromeOptions();
-		options.AddArgument("--start-maximized");
-		IWebDriver  driver = new ChromeDriver(options);
-		// NOTE: With Selenium WebDriver 4.11 the program crashes with 
-		// Unhandled Exception: OpenQA.Selenium.NoSuchDriverException: Unable to obtain chrome using Selenium Manager; 
-		// For documentation on this error, please visit: https://www.selenium.dev/documentation/webdriver/troubleshooting/errors/driver_location
-		// ---> System.TypeInitializationException: The type initializer for 'OpenQA.Selenium.SeleniumManager' threw an exception. 
-		// ---> OpenQA.Selenium.WebDriverException: Unable to locate or obtain Selenium Manager binary at ...\Program\bin\Debug\selenium-manager/windows/selenium-manager.exe
+		// options.AddArgument("--start-maximized");
+		IWebDriver driver = new ChromeDriver(options);
+		// NOTE: not using the WebDriver Service
 		// var service = ChromeDriverService.CreateDefaultService();
 		// service.DriverServicePath = driverLocation;
 		// IWebDriver driver = new ChromeDriver(service);	
 
-		var xhrUrls = new List<string>();
-		var handler = new NetworkRequestHandler();
-		handler.RequestTransformer = (request) => {
-			return request;
-		};
-		handler.RequestMatcher = httprequest => {
-			// works fine under debugger. but fails without debugger with exceptions like:
-			// System.InvalidOperationException: A command response was not received: Fetch.continueRequest
-			// System.InvalidOperationException: There is already one outstanding 'SendAsync' call for this WebSocket instance. ReceiveAsync and SendAsync can be called simultaneously, but at most one outstanding operation for each of them is allowed at the same time.
-			// see also: https://github.com/SeleniumHQ/selenium/issues/10564
-			xhrUrls.Add(httprequest.Url);
-			// the name 'Fetch' does not exist in the current context
-			// Fetch.continueRequest(httprequest);
-			return false;
-		};
+		
+		IDevTools devTools = driver as IDevTools;
+		IDevToolsSession session = devTools.GetDevToolsSession();
+		var domains = session.GetVersionSpecificDomains<DevToolsSessionDomains>();
+		domains.Page.Enable(new EnableCommandSettings());
+		domains.Page.AddScriptToEvaluateOnNewDocument(new AddScriptToEvaluateOnNewDocumentCommandSettings() {
+			Source = "Object.defineProperty(navigator, 'webdriver', { get: () => undefined })"
+		});
 
-		INetwork networkInterceptor = driver.Manage().Network;
-		networkInterceptor.AddRequestHandler(handler);
-
-		networkInterceptor.StartMonitoring();
-
+		var deviceModeSetting = new SetDeviceMetricsOverrideCommandSettings();
+		deviceModeSetting.Width = 600;
+		deviceModeSetting.Height = 1000;
+		deviceModeSetting.Mobile = true;
+		deviceModeSetting.DeviceScaleFactor = 50;
+      
+		domains.Emulation.SetDeviceMetricsOverride(deviceModeSetting);
+		// NOTE: Selenium dev recommends async / await
+		// await domains.Emulation.SetDeviceMetricsOverride(deviceModeSetting);
 		driver.Navigate().GoToUrl("https://store.epicgames.com/en-US/p/tunche");
 
 		driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
-
-		networkInterceptor.StopMonitoring();
-
-		foreach (var url in xhrUrls) {
-			Console.WriteLine(url);
-		}
 
 		driver.Quit();
 	}
--- external/csharp/selenium4-fetch/README.md
+++ external/csharp/selenium4-fetch/README.md
@@ -1,8 +1,72 @@
 ### Info
 
-this directory contains a basic example from the [forum](https://qna.habr.com/q/1266474?e=13639006#clarification_1709400)(in Russian) which was compiled against .Net Framework 4.5 to try 
+this directory contains a basic skeleton example to practice Selenum WebDriver BIDI and CDP Command examples from
+[Selenium 4 Chrome DevTools Documentation\](https://www.selenium.dev/documentation/webdriver/bidirectional/chrome_devtools/)
+and other sources
+which was compiled against .Net Framework 4.5
+
+### Note
+
+When compiled in Sharp Develop (which is discontinued) see the nuget error`
+```text
+NOTE: Selenium.WebDriver' already has a dependency defined for 'Newtonsoft.Json'.
+Exited with code: 1
+```
+the workaround is to download the package manually:
+```powershell
+$localfile = (resolve-path '.').path + '\' + 'selenium.webdriver.zip'
+$url = 'https://www.nuget.org/api/v2/package/Selenium.WebDriver/4.11.0'
+$url = 'https://www.nuget.org/api/v2/package/Selenium.WebDriver/4.8.2'
+```
+
+then
+```powershell
+ [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+ ```
+to prevent 
+
+```text
+The underlying connection was closed: An unexpected error occurred on a send.
+```
+error
+
+then
+```powershell
+$progressPreference  = 'SilentlyContinue'
+```
+to suppress the time consuming Powershell console download progress indicator
+then
+```powershell
+Invoke-WebRequest -Uri $url -OutFile $localfile
+mkdir .\packages\Selenium.WebDriver.4.8.2\lib\net45
+```
+and unzip the file `lib\net45\WebDriver.dll` from `selenium.webdriver.zip` manually into `packages\Selenium.WebDriver.4.8.2\lib\net45`
+
+### Note
+
+NOTE: With `Selenium WebDriver` __4.11__ the program crashes with excption indicating it attempts to deal with Chrome on its own:
+
+```text
+
+Unhandled Exception: OpenQA.Selenium.NoSuchDriverException: Unable to obtain chrome using Selenium Manager; 
+For documentation on this error, please visit: 
+https://www.selenium.dev/documentation/webdriver/troubleshooting/errors/driver_location
+ ---> System.TypeInitializationException: The type initializer for 'OpenQA.Selenium.SeleniumManager' threw an exception. 
+ ---> OpenQA.Selenium.WebDriverException: Unable to locate or obtain Selenium Manager binary at 
+...\Program\bin\Debug\selenium-manager/windows/selenium-manager.exe
+```
+### Usage
+
 
 
 ### See Also
 
   * https://github.com/SeleniumHQ/selenium/issues/10564
+  * [Run cdp commands on Selenium C#](https://stackoverflow.com/questions/70912939/run-cdp-commands-on-selenium-c-sharp)
+  * [evolution of CreateDevToolsSession in ChromeDriver Selenium](https://stackoverflow.com/questions/70529101/there-are-no-createdevtoolssession-in-chromedriver-selenium)
+
+### Author
+[Serguei Kouzmine](kouzmine_serguei@yahoo.com)
+
+
+
