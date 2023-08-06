4c6d48639666ce52841842ef70b9501d5883c86c
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-08-06 02:18:05 +0200

    intermediate revision - added remarks about issues with launching the example with more recent Selenium version

--- external/csharp/selenium4-fetch/Program/Example.cs
+++ external/csharp/selenium4-fetch/Program/Example.cs
@@ -14,41 +14,53 @@ using System.Data;
 using System.Text;
 
 class Example {
-    static void Main(string[] args) {
-        var options = new ChromeOptions();
-        options.AddArgument("--start-maximized");
-        IWebDriver driver = new ChromeDriver(options);
+	
+	private readonly static string driverLocation = Environment.GetEnvironmentVariable("CHROMEWEBDRIVER");
+	static void Main(string[] args) {
+		System.Environment.SetEnvironmentVariable("webdriver.chrome.driver",System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase).Replace("file:\\", ""));
+		var options = new ChromeOptions();
+		options.AddArgument("--start-maximized");
+		IWebDriver  driver = new ChromeDriver(options);
+		// NOTE: With Selenium WebDriver 4.11 the program crashes with 
+		// Unhandled Exception: OpenQA.Selenium.NoSuchDriverException: Unable to obtain chrome using Selenium Manager; 
+		// For documentation on this error, please visit: https://www.selenium.dev/documentation/webdriver/troubleshooting/errors/driver_location
+		// ---> System.TypeInitializationException: The type initializer for 'OpenQA.Selenium.SeleniumManager' threw an exception. 
+		// ---> OpenQA.Selenium.WebDriverException: Unable to locate or obtain Selenium Manager binary at ...\Program\bin\Debug\selenium-manager/windows/selenium-manager.exe
+		// var service = ChromeDriverService.CreateDefaultService();
+		// service.DriverServicePath = driverLocation;
+		// IWebDriver driver = new ChromeDriver(service);	
 
-        var xhrUrls = new List<string>();
-        var handler = new NetworkRequestHandler();
-        handler.RequestTransformer = (request) => { return request; };
-        handler.RequestMatcher = httprequest => {
-        	// works fine under debugger. but fails without debugger with exceptions like:
-        	// System.InvalidOperationException: A command response was not received: Fetch.continueRequest
+		var xhrUrls = new List<string>();
+		var handler = new NetworkRequestHandler();
+		handler.RequestTransformer = (request) => {
+			return request;
+		};
+		handler.RequestMatcher = httprequest => {
+			// works fine under debugger. but fails without debugger with exceptions like:
+			// System.InvalidOperationException: A command response was not received: Fetch.continueRequest
 			// System.InvalidOperationException: There is already one outstanding 'SendAsync' call for this WebSocket instance. ReceiveAsync and SendAsync can be called simultaneously, but at most one outstanding operation for each of them is allowed at the same time.
 			// see also: https://github.com/SeleniumHQ/selenium/issues/10564
-            xhrUrls.Add(httprequest.Url);
-            // the name 'Fetch' does not exist in the current context
-            // Fetch.continueRequest(httprequest);
-            return false;
-        };
+			xhrUrls.Add(httprequest.Url);
+			// the name 'Fetch' does not exist in the current context
+			// Fetch.continueRequest(httprequest);
+			return false;
+		};
 
-        INetwork networkInterceptor = driver.Manage().Network;
-        networkInterceptor.AddRequestHandler(handler);
+		INetwork networkInterceptor = driver.Manage().Network;
+		networkInterceptor.AddRequestHandler(handler);
 
-        networkInterceptor.StartMonitoring();
+		networkInterceptor.StartMonitoring();
 
-        driver.Navigate().GoToUrl("https://store.epicgames.com/en-US/p/tunche");
+		driver.Navigate().GoToUrl("https://store.epicgames.com/en-US/p/tunche");
 
-        driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
+		driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
 
-        networkInterceptor.StopMonitoring();
+		networkInterceptor.StopMonitoring();
 
-        foreach (var url in xhrUrls)
-        {
-            Console.WriteLine(url);
-        }
+		foreach (var url in xhrUrls) {
+			Console.WriteLine(url);
+		}
 
-        driver.Quit();
-    }
+		driver.Quit();
+	}
 }
